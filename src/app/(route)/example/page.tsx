6528f9fa8dee44eb76c644360b2c317c05c54244
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-17 00:34:01 +0900

    design(common): 공용 Tab 생성

--- src/app/(route)/example/page.tsx
+++ src/app/(route)/example/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 // 공용 컴포넌트 예시
 
+import { useState } from "react";
 import Tab from "@/app/components/common/Tab";
 
+const tabs = [
+  { id: "1", label: "Tab 1" },
+  { id: "2", label: "Tab 2" },
+  { id: "3", label: "Tab 3" },
+];
+
 const page = () => {
+  const [selected, setSelected] = useState("1");
   return (
     <div className="w-full h-[100vh] bg-gray-600 gap-4 flex-col-center">
-      <Tab />
-      <Tab />
-      <Tab />
-      <Tab />
+      <Tab
+        tabs={tabs}
+        selected={selected}
+        onValueChange={setSelected}
+        aria-label={`${tabs.find((tab) => tab.id === selected)?.label || "Tab"}`}
+      />
     </div>
   );
 };
--- src/app/components/common/Tab.tsx
+++ src/app/components/common/Tab.tsx
@@ -1,7 +1,35 @@
-const Tab = () => {
+import React from "react";
+
+type TabItem = {
+  id: string;
+  label: string;
+};
+
+interface TabProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "onClick"> {
+  tabs: TabItem[];
+  selected: string;
+  onValueChange: (id: string) => void;
+}
+
+const style = {
+  base: "p-3 cursor-pointer hover:bg-gray-100 mouse-hover",
+  selected: "border-b-2 font-bold border-blue-500",
+  unselected: "text-gray-500 border border-b-0 border-black rounded-t-lg",
+};
+
+const Tab = ({ tabs, selected, onValueChange, ...props }: TabProps) => {
   return (
-    <div>
-      <p>123</p>
+    <div className="flex">
+      {tabs.map((tab) => (
+        <div
+          key={tab.id}
+          className={`${style.base} ${selected === tab.id ? style.selected : style.unselected}`}
+          onClick={() => onValueChange(tab.id)}
+          {...props}
+        >
+          {tab.label}
+        </div>
+      ))}
     </div>
   );
 };
