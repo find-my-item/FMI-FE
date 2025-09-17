b8efaa66c260f287dda403e1bc9966ff2eba7b13
suhyeon0111 <soo4833@naver.com>
2025-09-17 22:27:57 +0900

    feat: input 공통 컴포넌트 (데이터 전송 안됨)

--- src/app/(auth)/components/AuthInput.tsx
+++ src/app/(auth)/components/AuthInput.tsx
@@ -1,48 +1,46 @@
+"use client";
+
 import React from "react";
-import { cn } from "@/utils/cn";
-import { RegisterOptions } from "react-hook-form";
+import { RegisterOptions, UseFormRegister, FieldError } from "react-hook-form";
+import { FormValue } from "../types/FormData";
 
 const InputStyle = "w-[330px] h-[40px] px-3 py-3 m-2 border rounded-[3px]";
 
-type FormValue = {
-  email: string,
-  password: string,
-  passwordConfirm: string,
-  nickname: string,
-}
-
-interface InputType {
-  id: keyof FormValue,
+interface InputProps {
+  id: keyof FormValue;
   label?: string;
   type: string;
   placeholder: string;
-  validation?: RegisterOptions;
+  validation?: RegisterOptions<FormValue>;
+  register: UseFormRegister<FormValue>;
   required?: boolean;
+  error?: FieldError;
 };
 
-const AuthInput: React.FC<InputType> = ({
+const AuthInput: React.FC<InputProps> = ({
   id,
   label,
   type = "text",
   placeholder = "",
+  register,
   required = true,
-  validation = "",
+  validation,
+  error,
 }) => {
+
   return (
     <>
-      {
-        label && <label htmlFor={id}>
-          {label}
-        </label>
-      }
+      {label && <label htmlFor={id}>{label}</label>}
       <input
+        {...register(id, validation)}
         className={InputStyle}
         type={type}
         placeholder={placeholder}
         required={required}
       />
+      {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
     </>
-  )
-}
+  );
+};
 
 export default AuthInput;
--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -1,106 +1,49 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { RegisterOptions } from "react-hook-form";
 import AuthInput from "../components/AuthInput";
-
-type FormValue = {
-  email: string,
-  password: string,
-  passwordConfirm: string,
-  nickname: string,
-}
-
-interface InputType {
-  id: keyof FormValue,
-  label: string;
-  type: string;
-  placeholder: string;
-  validation?: RegisterOptions;
-  required?: boolean;
-};
-
-export const singUpInputObject: InputType[] = [
-  {
-    id: "email",
-    label: "이메일",
-    type: "email",
-    placeholder: "이메일을 입력해주세요",
-    validation: {
-      pattern: {
-        value: /^\[^\\s@\]+@\[^\\s@\]+\\.\[^\\s@\]{2,}$/,
-        message: "이메일을 입력해주세요"
-      }
-    },
-    required: true,
-  },
-  {
-    id: "password",
-    label: "비밀번호",
-    type: "password",
-    placeholder: "비밀번호을 입력해주세요 (8~16자, 영문, 숫자, 특수문자 포함)",
-    validation: {
-      pattern: {
-        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8, 16}$/,
-        message: "비밀번호을 입력해주세요 (8~16자, 영문, 숫자, 특수문자 포함)",
-      }
-    },
-    required: true,
-  },
-  {
-    id: "passwordConfirm",
-    label: "비밀번호 재입력",
-    type: "password",
-    placeholder: "비밀번호 재입력",
-    validation: {
-      pattern: {
-        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8, 16}$/,
-        message: "비밀번호 재입력",
-      }
-    }
-    ,
-    required: true,
-  },
-  {
-    id: "nickname",
-    label: "닉네임",
-    type: "text",
-    placeholder: "닉네임을 입력해주세요",
-    required: true,
-  },
-];
+import { FormValue, signUpInputObject } from "../types/FormData";
 
 const Page = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<InputType>();
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<FormValue>({
+    mode: "onChange",
+    reValidateMode: "onChange",
+  }
+  );
 
-  const onSubmit: SubmitHandler<InputType> = (data) => {
+
+  const onSubmit: SubmitHandler<FormValue> = (data) => {
     console.log(data);
+    alert("폼 제출되었습니다.");
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {
-          singUpInputObject.map((item) => (
+        {signUpInputObject.map((item) => (
+          <div key={item.id}>
             <AuthInput
-              {...register("type", {})}
+              register={register}
               id={item.id}
               label={item.label}
               type={item.type}
               placeholder={item.placeholder}
               required={item.required}
               validation={item.validation}
+              error={errors[item.id]}
             />
-          ))
-        }
+          </div>
+        ))}
+
+        <button type="submit" className="bg-slate-400 w-50 h-30" >회원가입</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
