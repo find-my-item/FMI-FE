9935c55f0b0843f26eda8b65b6aa387913cafba4
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-17 16:58:35 +0900

    design(common): 모달 레이아웃 생성

--- src/app/(route)/example/page.tsx
+++ src/app/(route)/example/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Dropdown, Tab } from "@/app/components/common";
+import Modal from "@/app/components/common/Modal/ModalLayout";
 import { useState } from "react";
 
 const tabs = [
@@ -18,15 +19,38 @@ const options = [
 const page = () => {
   const [selectedTab, setSelectedTab] = useState("1");
   const [selectedOption, setSelectedOption] = useState("1");
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="w-full h-[100vh] bg-gray-600 gap-4 flex-col-center">
+      {/* Tab */}
       <Tab
         tabs={tabs}
         selected={selectedTab}
         onValueChange={setSelectedTab}
         aria-label={`${tabs.find((tab) => tab.id === selectedTab)?.label || "Tab"}`}
       />
+      {/* Dropdown */}
       <Dropdown options={options} onSelect={setSelectedOption} />
+      <button
+        className="px-4 py-2 border border-gray-300 rounded text-white hover:border-gray-500 hover:text-black mouse-hover"
+        onClick={() => setIsOpen(true)}
+      >
+        Open Modal
+      </button>
+
+      {/* Modal */}
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} className="p-4 flex-center">
+        <h2>Modal Title</h2>
+        <p className="h-[100px] flex-center">Modal Content</p>
+        <span>Modal Footer</span>
+        <button
+          className="mt-4 px-4 py-2 border border-gray-300 rounded hover:border-gray-500 hover:text-black mouse-hover"
+          onClick={() => setIsOpen(false)}
+        >
+          Close
+        </button>
+      </Modal>
     </div>
   );
 };
--- src/app/components/common/index.ts
+++ src/app/components/common/index.ts
@@ -1,2 +1,3 @@
 export { default as Tab } from "./Tab/Tab";
 export { default as Dropdown } from "./Dropdown/Dropdown";
+export { default as ModalLayout } from "./Modal/ModalLayout";
