5bc9b529f0ce95d454269f183429a57261808ff6
권형준 <znznun@gmail.com>
2025-09-18 16:37:20 +0900

    axios/tanstack query 공통 훅 작성, SSR/SSG/ISR 공통 훅 작성 및 Provider 수정 (#45)
    * chore: axios공통화 훅 뼈대 작업
    
    * chore: tanstack query훅 작성
    
    * chore: tanstack query provider fix
    
    * chore: SSR/SSG/ISR 대응 훅 작성
    
    * chore: axios함수 수정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,28 +1,38 @@
 "use client";
 
-import { PropsWithChildren, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react";
+import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function Providers({ children }: PropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 1000 * 60,
-            gcTime: 1000 * 60 * 10,
-            retry: 1,
-            refetchOnWindowFocus: false,
-          },
-        },
-      })
-  );
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60,
+        gcTime: 1000 * 60 * 10,
+        retry: 1,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+};
 
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (isServer) return makeQueryClient();
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+};
+
+const Providers = ({ children }: PropsWithChildren) => {
+  const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
-}
+};
+
+export default Providers;
