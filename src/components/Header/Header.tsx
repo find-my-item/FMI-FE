baeab7c8bf95077ad7221999661273986a89d918
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-22 00:32:43 +0900

    design(common): 헤더 디자인 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,25 @@
-import React from "react";
-import Link from "next/link";
+"use client";
 
-const HeaderLink = [
-  { name: "리스트", href: "/list" },
-  { name: "글쓰기", href: "/write" },
-  { name: "로그인", href: "/login" },
-  { name: "회원가입", href: "/signUp" },
-  { name: "프로필", href: "/profile" },
-  { name: "공지사항", href: "/notice" },
-  { name: "FAQ", href: "/faq" },
-  { name: "채팅", href: "/chat" },
-];
+import React, { useState } from "react";
+import Link from "next/link";
+import { HamburgerIcon } from "@/icon/Hamburger";
+import SideBar from "./SideBar";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="sticky top-0 w-full h-16 bg-gray-300 flex justify-between items-center p-4">
-      <Link href="/" aria-label="홈으로 이동">
-        Logo
-      </Link>
-      <nav aria-label="메인 내비게이션">
-        <ul className="flex gap-4">
-          {HeaderLink.map((link) => (
-            <li key={link.href}>
-              <Link href={link.href} className="mouse-hover hover:underline">
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="sticky top-0 w-full h-16 bg-gray-300 flex justify-between items-center p-4">
+        <Link href="/" aria-label="홈으로 이동">
+          Logo
+        </Link>
+        <button onClick={() => setIsOpen(!isOpen)} className="select-none">
+          <HamburgerIcon />
+        </button>
+      </header>
+      <SideBar isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   );
 };
 
--- src/components/Header/Hedaer.stories.tsx
+++ src/components/Header/Hedaer.stories.tsx
@@ -4,7 +4,6 @@ import Header from "./Header";
 const meta = {
   title: "Components/Header",
   component: Header,
-  tags: ["autodocs"],
 } satisfies Meta<typeof Header>;
 
 export default meta;
