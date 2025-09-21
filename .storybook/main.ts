db99613398476d9b5eb3832b2664a145be4a0c58
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-22 03:12:43 +0900

    chore: storybook 세팅 수정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,17 +2,24 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
-  ],
+  addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-a11y"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    (config.module!.rules as any[]).forEach((rule: any) => {
+      if (rule?.test instanceof RegExp && rule.test.test(".svg")) {
+        rule.exclude = [/\.svg$/].concat(rule.exclude || []);
+      }
+    });
+    (config.module!.rules as any[]).push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 export default config;
