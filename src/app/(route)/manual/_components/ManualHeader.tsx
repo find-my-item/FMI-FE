a272c7ea24b0ce2155e385092ba50151d5ee3442
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-23 01:56:29 +0900

    design(manual): 뒤로가기 아이콘 추가

--- src/app/(route)/manual/_components/ManualHeader.tsx
+++ src/app/(route)/manual/_components/ManualHeader.tsx
@@ -2,14 +2,15 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import Icon from "@/components/Icon/Icon";
 
 const ManualHeader = () => {
   const router = useRouter();
 
   return (
     <div className="w-full h-[60px] px-[20px] flex items-center justify-start gap-[3px]">
-      <button className="w-[30px] h-[30px]" onClick={() => router.back()}>
-        꺾쇠
+      <button className="w-[30px] h-[30px]" onClick={() => router.back()} aria-label="뒤로가기">
+        <Icon name="ArrowLeftSecond" size={30} />
       </button>
       <h1 className="text-[20px] font-semibold text-[#242424]">유실물 발생 시 매뉴얼</h1>
     </div>
--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -2,7 +2,7 @@ export { default as EyeOpen } from "@/assets/eye-open.svg";
 export { default as Menu } from "@/assets/menu.svg";
 export { default as XSecond } from "@/assets/x-second.svg";
 export { default as Logo } from "@/assets/logo.svg";
-export { default as ArrowLeft } from "@/assets/arrow-left.svg";
+export { default as ArrowLeftSecond } from "@/assets/arrow-left-second.svg";
 export { default as Book } from "@/assets/book.svg";
 export { default as ArrowDown } from "@/assets/arrow-down.svg";
 export { default as ArrowUp } from "@/assets/arrow-up.svg";
