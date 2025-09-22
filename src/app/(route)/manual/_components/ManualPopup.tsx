57f79e67a29a5d2f1d9740e390d08ae1f0c0565d
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-23 00:43:32 +0900

    design(manual): 메뉴얼 팝업 생성

---
+++ src/app/(route)/manual/_components/ManualPopup.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useModalBackdrop, useModalLockAndEsc } from "@/utils/useOverlayHandlers";
+import React from "react";
+
+interface ManualPopupProps {
+  isOpen: boolean;
+  onClose?: () => void;
+}
+
+const ManualPopup = ({ isOpen, onClose }: ManualPopupProps) => {
+  if (!isOpen) return null;
+
+  useModalLockAndEsc({ isOpen, onClose });
+  const onBackdropMouseDown = useModalBackdrop({ onClose });
+
+  return (
+    <div
+      className="fixed inset-0 z-[9999] flex items-end justify-center bg-black/30"
+      onMouseDown={onBackdropMouseDown}
+    >
+      <div className="w-full max-w-md rounded-t-2xl bg-white px-6">
+        <div className="mx-auto mt-[71px] mb-6 h-[74px] w-[74px] rounded-full bg-black" />
+
+        <div className="text-center">
+          <h1 className="mb-4 text-[20px] leading-[26px] font-semibold text-[#171717]">
+            분실물이 있나요? <br />
+            메뉴얼을 보면 더 도움이 돼요!
+          </h1>
+          <p className="text-[14px] leading-[22px] text-[#7D7D7D]">
+            분실물 발생 시 도움이 되는 정보를 <br /> 메뉴얼에서 확인해 보세요.
+          </p>
+        </div>
+
+        <div className="mt-8 mb-[69px] flex flex-col gap-3">
+          <button className="h-[48px] w-full rounded-[12px] bg-[#04AD69] text-[18px] text-white">
+            로그인
+          </button>
+          <button className="h-[64px] w-full text-[#04AD69]" onClick={onClose}>
+            다음에 볼게요
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ManualPopup;
--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -2,3 +2,5 @@ export { default as EyeOpen } from "@/assets/eye-open.svg";
 export { default as Menu } from "@/assets/menu.svg";
 export { default as XSecond } from "@/assets/x-second.svg";
 export { default as Logo } from "@/assets/logo.svg";
+export { default as ArrowLeft } from "@/assets/arrow-left.svg";
+export { default as Book } from "@/assets/book.svg";
