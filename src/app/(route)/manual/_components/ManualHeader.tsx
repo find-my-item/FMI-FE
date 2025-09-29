989239cd07b50105e1d4d331f9f60e78429a7f57
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-29 20:25:02 +0900

    design(manual): 매뉴얼 페이지 디자인 수정

--- src/app/(route)/manual/_components/ManualHeader.tsx
+++ src/app/(route)/manual/_components/ManualHeader.tsx
@@ -8,9 +8,9 @@ const ManualHeader = () => {
   const router = useRouter();
 
   return (
-    <div className="w-full h-[60px] px-[20px] flex items-center justify-start gap-[3px]">
+    <div className="w-full h-[56px] px-[20px] flex items-center justify-start gap-[3px]">
       <button className="w-[30px] h-[30px]" onClick={() => router.back()} aria-label="뒤로가기">
-        <Icon name="ArrowLeftSecond" size={30} />
+        <Icon name="ArrowLeftSmall" size={30} />
       </button>
       <h1 className="text-[20px] font-semibold text-[#242424]">유실물 발생 시 매뉴얼</h1>
     </div>
