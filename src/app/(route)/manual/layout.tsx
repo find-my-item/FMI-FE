7a53147e45a00c52feab7e1efc6e071ffb89b83c
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-30 00:47:43 +0900

    feat(header): 상세페이지 상단 헤더 디자인 수정 및 라우팅 로직 추가

--- src/app/(route)/manual/layout.tsx
+++ src/app/(route)/manual/layout.tsx
@@ -1,9 +1,8 @@
-import ManualHeader from "./_components/ManualHeader";
-
+import DetailHeader from "@/components/DetailHeader/DetailHeader";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <ManualHeader />
+      <DetailHeader title="유실물 발생 시 매뉴얼" />
       <div className="w-full h-[calc(100vh-60px)]">{children}</div>
     </>
   );
