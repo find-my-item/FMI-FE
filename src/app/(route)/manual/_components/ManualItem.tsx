d19e61fb92e48287534d3a6f2111a3401fd51359
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-29 20:25:02 +0900

    design(manual): 매뉴얼 페이지 디자인 수정

--- src/app/(route)/manual/_components/ManualItem.tsx
+++ src/app/(route)/manual/_components/ManualItem.tsx
@@ -18,13 +18,13 @@ const ManualItem = ({ title, content, href, btnText }: ManualItemProps) => {
 
   return (
     <>
-      <div className="w-full px-[20px] py-[26px] border-b border-[#E4E4E4]">
-        <button
-          aria-expanded={isOpen}
-          aria-controls="매뉴얼 아이템 패널"
-          onClick={() => setIsOpen((v) => !v)}
-          className="flex w-full items-center justify-between"
-        >
+      <div
+        aria-expanded={isOpen}
+        aria-controls="매뉴얼 아이템 패널"
+        onClick={() => setIsOpen((v) => !v)}
+        className="w-full px-[20px] py-[26px] border-b border-[#E4E4E4] cursor-pointer"
+      >
+        <button className="flex w-full items-center justify-between">
           <p className="font-semibold text-[16px] text-[#242424]">{title}</p>
           <span className={cn("transition-transform duration-200", isOpen && "rotate-180")}>
             <Icon name="ArrowDown" />
@@ -40,16 +40,17 @@ const ManualItem = ({ title, content, href, btnText }: ManualItemProps) => {
             transition={{ duration: 0.3 }}
             className="w-full bg-[#E4E4E4] overflow-hidden"
           >
-            <div className="px-[20px] py-[38px] text-sm text-[#525252]">
+            <div className="px-[20px] py-[38px] flex flex-col justify-center items-start text-sm text-[#525252]">
               <p className="mb-[26px]">{content}</p>
               {href && (
                 <Link
                   href={href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="px-[30px] py-[14px] rounded-[4px] bg-[#525252] font-semibold text-[14px] text-white"
+                  className="px-[30px] text-center flex items-center gap-2 justify-center py-[14px] w-full rounded-[4px] border border-[#DADADA] bg-white text-black font-semibold text-[14px]"
                 >
                   {btnText}
+                  <Icon name="ArrowRightSmall" size={20} />
                 </Link>
               )}
             </div>
