c1796dc986917c59bc948510861a18e58c99e50e
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-29 19:43:37 +0900

    refactor(pr): PR 코멘트 반영

--- src/app/(route)/manual/page.tsx
+++ src/app/(route)/manual/page.tsx
@@ -3,43 +3,46 @@
 import { cn } from "@/utils/cn";
 import { useState } from "react";
 import ManualItem from "./_components/ManualItem";
-import { MANUAL } from "./_constants/MANUAL";
+import { MANUAL, ManualItemType } from "./_constants/MANUAL";
 
 const manualList = [
   {
     title: "분실",
     content: "분실 매뉴얼",
+    key: "LOST",
   },
   {
     title: "습득",
     content: "습득 매뉴얼",
+    key: "FOUND",
   },
   {
     title: "도난",
     content: "도난 매뉴얼",
+    key: "STOLEN",
   },
 ];
 
 const page = () => {
-  const [selected, setSelected] = useState("분실");
+  const [selected, setSelected] = useState<keyof typeof MANUAL>("LOST");
 
   return (
     <div className="flex-col-center w-full">
       <div className="w-full px-[20px] flex border-b border-[#ADADAD]">
         {manualList.map((item) => (
           <button
-            key={item.title}
+            key={item.key}
             className={cn(
               "flex-1 h-[60px] flex-center font-semibold text-[20px] text-[#ADADAD]",
-              selected === item.title && "border-b-2 border-[#04AD69] text-[#04AD69]"
+              selected === item.key && "border-b-2 border-[#04AD69] text-[#04AD69]"
             )}
-            onClick={() => setSelected(item.title)}
+            onClick={() => setSelected(item.key as keyof typeof MANUAL)}
           >
             {item.title}
           </button>
         ))}
       </div>
-      {MANUAL.filter((item) => item.category === selected).map((item) => (
+      {MANUAL[selected].map((item: ManualItemType) => (
         <ManualItem
           key={item.title}
           title={item.title}
