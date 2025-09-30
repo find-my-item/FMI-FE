93db6d68c625dd2fcd8b85f0de624e31837cb964
suhyeon0111 <soo4833@naver.com>
2025-09-30 22:20:14 +0900

    최신화 받기 전 저장

--- src/app/(route)/(auth)/login/email/page.tsx
+++ src/app/(route)/(auth)/login/email/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+<<<<<<< HEAD
+=======
+import { useForm, FormProvider } from "react-hook-form";
+import { FormValue } from "../../_constant/FormData";
+>>>>>>> b2cb13c (최신화 받기 전 저장)
 import Button from "@/components/Button/Button";
 import { ButtonStyle, InputStyle } from "../../styles/authStyle";
 import Link from "next/link";
 import { cn } from "@/utils/cn";
 import Icon from "@/components/Icon/Icon";
+<<<<<<< HEAD
 import CheckBox from "./_components/CheckBox";
 
 const Page = () => {
@@ -19,6 +25,22 @@ const Page = () => {
       <div className="flex w-full flex-col gap-3 px-5">
         {/* 로그인 입력칸 */}
         <div className="flex w-full flex-col gap-3">
+=======
+
+const Page = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col-center md:flex-row gap-8">
+      {/* logo */}
+      <div className="flex-col-center">
+        <Icon name="Logo" size={90} title="로고" />
+        <h1 className="text-[#1EB87B] text-[49px] font-bold">찾아줘!</h1>
+      </div>
+
+      {/* button */}
+      <div className="w-full px-5 flex flex-col gap-3 text-[14px]">
+        {/* 로그인 입력칸 */}
+        <div>
+>>>>>>> b2cb13c (최신화 받기 전 저장)
           <input
             name="email"
             type="text"
@@ -32,6 +54,7 @@ const Page = () => {
             className={cn(InputStyle)}
           />
         </div>
+<<<<<<< HEAD
 
         {/* 체크박스 */}
         <div className="flex w-full gap-3 text-[14px] text-[#9D9D9D]">
@@ -65,6 +88,34 @@ const Page = () => {
           </Link>
         </div>
       </div>
+=======
+        {/* 체크박스 */}
+        <div>
+          <input name="rememberID" type="checkbox" />
+          <input name="autoLogin" type="checkbox" />
+        </div>
+      </div>
+
+      {/* 로그인 버튼 */}
+      <div>
+        <Button children="회원가입" type="submit" className={ButtonStyle} />
+        {/* divider 구분선 */}
+        <div className="flex items-center w-full h-[18px] px-5">
+          <div className="h-px flex-1 bg-[#E4E4E4]" />
+          <span className="px-3 text-[#9D9D9D] text-[12px]">로그인이 되지 않는다면?</span>
+          <div className="h-px flex-1 bg-[#E4E4E4]"></div>
+        </div>
+      </div>
+      {/* 회원확인 여부 */}
+      <div className="h-11">
+        <Link href="findPw" className="text-[#1EB87B] text-[14px] p-3">
+          비밀번호 찾기
+        </Link>
+        <Link href="/signUp" className="text-[#1EB87B] text-[14px] p-3">
+          회원가입
+        </Link>
+      </div>
+>>>>>>> b2cb13c (최신화 받기 전 저장)
     </div>
   );
 };
