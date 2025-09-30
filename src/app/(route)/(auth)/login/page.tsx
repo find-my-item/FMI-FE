e19a13bfaff0abd01628e170187fce97467aeff0
suhyeon0111 <soo4833@naver.com>
2025-09-30 16:16:19 +0900

    design(login): 텍스트 크기 수정 및 그 외 사항 수정

--- src/app/(route)/(auth)/login/page.tsx
+++ src/app/(route)/(auth)/login/page.tsx
@@ -8,15 +8,15 @@ import Icon from "@/components/Icon/Icon";
 
 const Page = () => {
   return (
-    <div className="flex-col-center flex min-h-screen w-full gap-8 md:flex-row">
+    <div className="w-full min-h-screen flex flex-col-center md:flex-row gap-8">
       {/* logo */}
       <div className="flex-col-center">
         <Icon name="Logo" size={90} title="로고" />
-        <h1 className="text-[49px] font-bold text-[#1EB87B]">찾아줘!</h1>
+        <h1 className="text-[#1EB87B] text-[49px] font-bold">찾아줘!</h1>
       </div>
 
       {/* button */}
-      <div className="flex w-full flex-col gap-3 px-5 text-[14px]">
+      <div className="w-full px-5 flex flex-col gap-3 text-[14px]">
         <Button
           children="카카로로 3초 만에 시작하기"
           type="submit"
@@ -35,13 +35,15 @@ const Page = () => {
       <div className="flex h-[18px] w-full items-center px-5">
         <div className="h-px flex-1 bg-[#E4E4E4]" />
         <span className="px-3 text-[12px] text-[#9D9D9D]">또는</span>
+
         <div className="h-px flex-1 bg-[#E4E4E4]"></div>
       </div>
 
       {/* 회원확인 여부 */}
       <div className="h-11">
-        <span className="text-[12px] font-medium text-[#9D9D9D]">아직 회원이 아니신가요?</span>
-        <Link href="/signUp" className="p-3 text-[14px] text-[#1EB87B]">
+
+        <span className="font-medium text-[#9D9D9D] text-[12px]">아직 회원이 아니신가요?</span>
+        <Link href="/signUp" className="text-[#1EB87B] text-[14px] p-3">
           회원가입
         </Link>
       </div>
