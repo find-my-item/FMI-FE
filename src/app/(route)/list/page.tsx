1a25cbcd817e6b83ae431be17d2e385932e5904e
wlrnjs <tjwlrnjs7336@icloud.com>
2025-09-30 22:04:17 +0900

    design(list): 리스트 페이지 드롭다운 추가

--- src/app/(route)/list/page.tsx
+++ src/app/(route)/list/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Tab } from "@/components";
+import { Dropdown, Tab } from "@/components";
 import { useState } from "react";
 import ListItem from "./_components/ListItem/ListItem";
+import Icon from "@/components/Icon/Icon";
 
 const list = [
   {
@@ -17,11 +18,28 @@ const list = [
 
 const page = () => {
   const [selected, setSelected] = useState("LOST");
+  const [selectedRegion, setSelectedRegion] = useState("지역선택");
+  const [selectedView, setSelectedView] = useState("조회순");
+  const [selectedCategory, setSelectedCategory] = useState("찾는중");
+
+  const dropdowns = [
+    { value: selectedRegion, setValue: setSelectedRegion, icon: "Position" },
+    { value: selectedView, setValue: setSelectedView, icon: "ArrowDown" },
+    { value: selectedCategory, setValue: setSelectedCategory, icon: "ArrowDown" },
+  ] as const;
 
   return (
     <div>
       <Tab tabs={list} selected={selected} onValueChange={setSelected} />
-      <div className="h-[67px] w-full bg-black" />
+      <div className="flex h-[67px] w-full items-center gap-2 px-5">
+        {dropdowns.map(({ value, setValue, icon }, idx) => (
+          <Dropdown key={idx} options={[]} onSelect={setValue} className="gap-[4px]">
+            {idx === 0 && <Icon name={icon} size={16} />}
+            <span className="text-[16px] font-semibold text-[#525252]">{value}</span>
+            {idx !== 0 && <Icon name="ArrowDown" size={12} />}
+          </Dropdown>
+        ))}
+      </div>
       {/* 아이템 */}
       <div className="w-full">
         {Array.from({ length: 5 }).map((_, index) => (
