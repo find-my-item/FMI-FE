85d715cc8c391582732d87657acfbae191eea431
suhyeon0111 <soo4833@naver.com>
2025-10-01 17:35:05 +0900

    design(findPw):비밀번호 찾기 페이지  퍼블리싱

--- src/app/(route)/(auth)/findPw/page.tsx
+++ src/app/(route)/(auth)/findPw/page.tsx
@@ -1,65 +1,21 @@
 "use client";
 
 import Button from "@/components/Button/Button";
-import { ButtonStyle } from "../styles/authStyle";
-import Link from "next/link";
+import Input from "@/components/Input/Input";
+import { ButtonStyle, InputStyle } from "../styles/authStyle";
 import { cn } from "@/utils/cn";
-import Icon from "@/components/Icon/Icon";
-import { FormProvider, useForm } from "react-hook-form";
-import { FormValue } from "../_constant/FormData";
 
 const Page = () => {
-  const methods = useForm<FormValue>({
-    mode: "onBlur",
-  });
-
-  const onSubmit = () => {
-    const onSubmit = (data: FormValue) => {
-      alert("폼 제출되었습니다.");
-    };
-  };
-
   return (
-    <FormProvider {...methods}>
-      <div className="w-full min-h-screen flex flex-col-center md:flex-row gap-8">
-        {/* logo */}
-        <div className="flex-col-center">
-          <Icon name="Logo" size={90} title="로고" />
-          <h1 className="text-[#1EB87B] text-[49px] font-bold">찾아줘!</h1>
-        </div>
-
-        {/* button */}
-        <div className="w-full px-5 flex flex-col gap-3 text-[14px]">
-          <Button
-            children="카카로로 3초 만에 시작하기"
-            type="submit"
-            className={cn(ButtonStyle, "bg-[#FFEA14] text-[#242424]")}
-            label="로그인 버튼"
-          />
-          <Button
-            children="이메일로 로그인"
-            type="submit"
-            className={cn(ButtonStyle, "bg-[#E4E4E4] text-[#525252]")}
-            label="로그인 버튼"
-          />
-        </div>
-
-        {/* divider 구분선 */}
-        <div className="flex items-center w-full h-[18px] px-5">
-          <div className="h-px flex-1 bg-[#E4E4E4]" />
-          <span className="px-3 text-[#9D9D9D] text-[12px]">또는</span>
-          <div className="h-px flex-1 bg-[#E4E4E4]"></div>
-        </div>
-
-        {/* 회원확인 여부 */}
-        <div className="h-11">
-          <span className="font-medium text-[#9D9D9D] text-[12px]">아직 회원이 아니신가요?</span>
-          <Link href="/signUp" className="text-[#1EB87B] text-[14px] p-3">
-            회원가입
-          </Link>
-        </div>
-      </div>
-    </FormProvider>
+    <div className="flex-col-center w-full gap-4 px-5 py-6">
+      <input
+        type="text"
+        placeholder="아이디(이메일)을 입력해 주세요."
+        className={InputStyle}
+        name="findPw"
+      />
+      <Button children="입력완료" className={cn(ButtonStyle, "text-white")} />
+    </div>
   );
 };
 
--- src/app/(route)/(auth)/styles/authStyle.ts
+++ src/app/(route)/(auth)/styles/authStyle.ts
@@ -1,3 +1,4 @@
 export const InputStyle =
-  "w-full h-[50px] px-[14px] py-[12.5px] bg-[#F5F5F5] rounded-[10px] text-[#9D9D9D] text-[14px] font-semibold";
-export const ButtonStyle = "w-full h-[50px] rounded-[10px] font-semibold";
+  "flex items-center w-full h-[50px] px-[14px] py-[12.5px] bg-[#F5F5F5] rounded-[10px] text-[#9D9D9D] text-[14px]";
+export const ButtonStyle =
+  "w-full h-[50px] rounded-[10px] bg-[#ADADAD] font-semibold text-[16px] text-white";
