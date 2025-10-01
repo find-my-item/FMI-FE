5181f705ea45fff76238e6c637c7a6e0af74a7fe
suhyeon0111 <soo4833@naver.com>
2025-10-01 15:56:10 +0900

    design(login): 로그인 퍼블리싱(체크박스 수정 전)

--- src/app/(route)/(auth)/styles/authStyle.ts
+++ src/app/(route)/(auth)/styles/authStyle.ts
@@ -1,3 +1,3 @@
 export const InputStyle =
-  "w-full h-[50px] bg-[#F5F5F5] rounded-[10px] text-[#9D9D9D] text-[14px] font-semibold";
+  "w-full h-[50px] px-[14px] py-[12.5px] bg-[#F5F5F5] rounded-[10px] text-[#9D9D9D] text-[14px] font-semibold";
 export const ButtonStyle = "w-full h-[50px] rounded-[10px] font-semibold";
