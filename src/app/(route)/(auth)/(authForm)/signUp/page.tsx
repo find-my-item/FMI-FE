558baeed173c7f0d8736487288722bbbc86b2756
suhyeon0111 <soo4833@naver.com>
2025-10-01 21:07:15 +0900

    feat(form): layout.tsx 생성, 폼 제출 이벤트 생성

--- src/app/(route)/(auth)/(authForm)/signUp/page.tsx
+++ src/app/(route)/(auth)/(authForm)/signUp/page.tsx
@@ -1,42 +1,38 @@
 "use client";
 
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm, useFormContext } from "react-hook-form";
 import { FormValue, signUpInputObject } from "../../_constant/FormData";
 import Input from "@/components/Input/Input";
 import Button from "@/components/Button/Button";
 import { InputStyle } from "../../styles/authStyle";
 
 const Page = () => {
-  const methods = useForm<FormValue>({
-    mode: "onBlur",
+  const methods = useFormContext();
+
+  const onSubmit = methods.handleSubmit((data) => {
+    alert("폼 제출되었습니다.");
   });
 
-  const onSubmit = (data: FormValue) => {
-    alert("폼 제출되었습니다.");
-  };
-
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        {signUpInputObject.map((item) => (
-          <Input
-            key={item.name}
-            name={item.name}
-            type={item.type}
-            className={InputStyle}
-            label={item.label}
-            placeholder={item.placeholder}
-            validation={item.validation}
-          />
-        ))}
-        <Button
-          children="회원가입"
-          type="submit"
-          className="w-50 h-30 bg-slate-400"
-          label="회원가입 버튼"
+    <form onSubmit={onSubmit}>
+      {signUpInputObject.map((item) => (
+        <Input
+          key={item.name}
+          name={item.name}
+          type={item.type}
+          className={InputStyle}
+          label={item.label}
+          placeholder={item.placeholder}
+          validation={item.validation}
         />
-      </form>
-    </FormProvider>
+      ))}
+      <Button
+        children="회원가입"
+        type="submit"
+        className="w-50 h-30 bg-slate-400"
+        label="회원가입 버튼"
+      />
+    </form>
   );
 };
 
