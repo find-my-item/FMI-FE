da7e4354e5e962f4ba39af33a057fa14b820ab91
suhyeon0111 <soo4833@naver.com>
2025-10-01 22:22:21 +0900

    feat(findPw): 이메일 전송 성공 시 발송완료 화면으로 변경

--- src/app/(route)/(auth)/(authForm)/findPw/page.tsx
+++ src/app/(route)/(auth)/(authForm)/findPw/page.tsx
@@ -5,8 +5,15 @@ import Button from "@/components/Button/Button";
 import Input from "@/components/Input/Input";
 import { ButtonStyle, InputStyle } from "../../styles/authStyle";
 import { cn } from "@/utils/cn";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const [isSend, setIsSend] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const router = useRouter();
+
   const {
     handleSubmit,
     formState: { isValid },
@@ -14,27 +21,41 @@ const Page = () => {
 
   const onSubmit = handleSubmit((data) => {
     alert("폼 제출되었습니다.");
+    // console.log("data>> ", data)
+    setEmail(data.email);
+    setIsSend(true);
   });
 
   return (
     <form className="flex-col-center w-full gap-4 px-5 py-6" onSubmit={onSubmit}>
-      <Input
-        className={InputStyle}
-        type="text"
-        placeholder="아이디(이메일)을 입력해 주세요."
-        name="findPw"
-        validation={{
-          required: "이메일은 필수 항목 입니다",
-          pattern: {
-            value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
-            message: "이메일 형식이 올바르지 않습니다",
-          },
-        }}
-      />
+      {isSend ? (
+        <div className="flex-col-center h-[91px] text-center text-[14px] leading-relaxed">
+          <p>
+            <span className="text-[#1EB87B]">{email}</span>으로
+            <br />
+            임시 비밀번호를 발송했습니다.
+          </p>
+        </div>
+      ) : (
+        <Input
+          className={InputStyle}
+          type="text"
+          placeholder="아이디(이메일)을 입력해 주세요."
+          name="email"
+          validation={{
+            required: "이메일은 필수 항목 입니다",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+              message: "이메일 형식이 올바르지 않습니다",
+            },
+          }}
+        />
+      )}
       <Button
-        children="입력완료"
-        type="submit"
+        children={isSend ? "로그인 화면으로 이동" : "입력완료"}
+        type={isSend ? "button" : "submit"}
         className={cn(ButtonStyle, isValid && "bg-[#1EB87B]")}
+        onClick={isSend ? () => router.push("/login") : undefined}
       />
     </form>
   );
