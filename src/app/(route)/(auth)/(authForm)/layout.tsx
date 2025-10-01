a15e3740822136e112bd8196a5443e8cd64bc60f
suhyeon0111 <soo4833@naver.com>
2025-10-01 21:07:15 +0900

    feat(form): layout.tsx 생성, 폼 제출 이벤트 생성

---
+++ src/app/(route)/(auth)/(authForm)/layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { FormProvider, useForm } from "react-hook-form";
+import { FormValue } from "../_constant/FormData";
+
+export default function AuthSubLayout({ children }: { children: React.ReactNode }) {
+  const methods = useForm<FormValue>({
+    mode: "onBlur",
+  });
+
+  return <FormProvider {...methods}>{children}</FormProvider>;
+}
