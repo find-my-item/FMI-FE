86365f6defd1f7b4be6c3abe82455333c6aa257e
suhyeon0111 <soo4833@naver.com>
2025-10-01 15:56:10 +0900

    design(login): 로그인 퍼블리싱(체크박스 수정 전)

--- src/app/(route)/(auth)/login/email/_components/CheckBox.tsx
+++ src/app/(route)/(auth)/login/email/_components/CheckBox.tsx
@@ -7,14 +7,14 @@ interface checkProps {
   name: string;
 }
 
-const CheckBox = ({ children, name, ...rest }: checkProps) => {
+const CheckBox = ({ children, name }: checkProps) => {
   return (
     <label htmlFor={name} className="flex cursor-pointer items-center">
       <input name={name} type="checkbox" className="peer sr-only" />
       <div className="relative grid h-3 w-3 place-items-center rounded bg-gray-300 peer-checked:bg-[#1EB87B] [&_svg]:opacity-0 peer-checked:[&_svg]:opacity-100">
         <Icon name="Check" size={5} title="체크됨" />
       </div>
-      <span className="ml-3 text-gray-700">{children}</span>
+      <span className="ml-3 text-[#9D9D9D]">{children}</span>
     </label>
   );
 };
--- src/app/(route)/(auth)/login/email/page.tsx
+++ src/app/(route)/(auth)/login/email/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useForm, FormProvider } from "react-hook-form";
-import { FormValue } from "../../_constant/FormData";
 import Button from "@/components/Button/Button";
 import { ButtonStyle, InputStyle } from "../../styles/authStyle";
 import Link from "next/link";
@@ -11,14 +9,15 @@ import CheckBox from "./_components/CheckBox";
 
 const Page = () => {
   return (
-    <div className="flex-col-center flex min-h-screen w-full gap-8 md:flex-row">
-      {/* logo */}
-      <div className="flex">
-        <Icon name="Logo" size={90} title="로고" />
-        <h1 className="text-[49px] font-bold text-[#1EB87B]">찾아줘!</h1>
-      </div>
+    <div className="flex-col-center flex min-h-screen w-full gap-6 md:flex-row">
+      {/* 상위 박스 (로고, 입력칸, 체크박스) */}
+      <div className="flex w-full flex-col gap-10 px-5">
+        {/* 로고 */}
+        <div className="flex-center">
+          <Icon name="Logo" size={50} title="로고" />
+          <h1 className="text-[39px] font-bold text-[#1EB87B]">찾아줘!</h1>
+        </div>
 
-      <div className="flex w-full flex-col gap-3 px-5">
         {/* 로그인 입력칸 */}
         <div className="flex w-full flex-col gap-3">
           <input
@@ -33,39 +32,38 @@ const Page = () => {
             placeholder="비밀번호를 입력해주세요."
             className={cn(InputStyle)}
           />
-        </div>
-
-        {/* 체크박스 */}
-        <div className="flex w-full gap-3 text-[14px] text-[#9D9D9D]">
-          <CheckBox children="아이디 기억하기" name="rememberID" />
-          <CheckBox children="자동 로그인" name="autoLogin" />
-        </div>
-
-        {/* 로그인 버튼 */}
-        <div>
-          <Button
-            children="로그인"
-            type="submit"
-            className={cn(ButtonStyle, "bg-[#1EB87B] text-white")}
-          />
-          {/* divider 구분선 */}
-          <div className="flex h-[18px] w-full items-center font-medium">
-            <div className="h-px flex-1 bg-[#E4E4E4]" />
-            <span className="px-3 text-[12px] text-[#9D9D9D]">로그인이 되지 않는다면?</span>
-            <div className="h-px flex-1 bg-[#E4E4E4]" />
+          {/* 체크박스 */}
+          <div className="flex w-full gap-3 text-[14px] text-[#9D9D9D]">
+            <CheckBox children="아이디 기억하기" name="rememberID" />
+            <CheckBox children="자동 로그인" name="autoLogin" />
           </div>
         </div>
+      </div>
 
-        {/* 회원확인 여부 */}
-        <div className="flex h-11 w-full justify-center">
-          <Link href="findPw" className="p-3 text-[14px] text-[#1EB87B]">
-            비밀번호 찾기
-          </Link>
-          <span className="h-4 self-center border-l border-gray-300" />
-          <Link href="/signUp" className="p-3 text-[14px] text-[#1EB87B]">
-            회원가입
-          </Link>
+      {/* 로그인 버튼 */}
+      <div className="flex-col-center w-full gap-6 px-5">
+        <Button
+          children="로그인"
+          type="submit"
+          className={cn(ButtonStyle, "bg-[#1EB87B] text-white")}
+        />
+        {/* divider 구분선 */}
+        <div className="flex h-4 w-full items-center">
+          <div className="h-px flex-1 bg-[#E4E4E4]" />
+          <span className="px-3 text-[12px] text-[#9D9D9D]">로그인이 되지 않는다면?</span>
+          <div className="h-px flex-1 bg-[#E4E4E4]"></div>
         </div>
+      </div>
+
+      {/* 회원확인 여부 */}
+      <div className="flex h-11 w-full justify-center">
+        <Link href="/findPw" className="p-3 text-[14px] text-[#9D9D9D]">
+          비밀번호 찾기
+        </Link>
+        <span className="h-4 self-center border-l border-gray-300" />
+        <Link href="/signUp" className="p-3 text-[14px] text-[#1EB87B]">
+          회원가입
+        </Link>
       </div>
     </div>
   );
