44619d421e751cdf47cc4b486f160c67e773cc9d
suhyeon0111 <soo4833@naver.com>
2025-10-03 02:23:36 +0900

    docs(CheckBox):체크박스 주석처리

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -10,10 +10,10 @@ interface checkProps {
 const CheckBox = ({ children, name, ...rest }: checkProps) => {
   return (
     <label htmlFor={name} className="flex cursor-pointer items-center">
-      <input name={name} type="checkbox" className="peer sr-only" />
-      <div className="relative grid h-3 w-3 place-items-center rounded bg-gray-300 peer-checked:bg-[#1EB87B] [&_svg]:opacity-0 peer-checked:[&_svg]:opacity-100">
+      <input name={name} type="checkbox" />
+      {/* <div className="relative grid h-3 w-3 place-items-center rounded bg-[#E4E4E4] peer-checked:bg-[#1EB87B] [&_svg]:opacity-0 peer-checked:[&_svg]:opacity-100">
         <Icon name="Check" size={5} title="체크됨" />
-      </div>
+      </div> */}
       <span className="ml-3 text-[#9D9D9D]">{children}</span>
     </label>
   );
