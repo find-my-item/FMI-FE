59a768b74d3d36068d553451d8dac739b81b84d0
suhyeon0111 <soo4833@naver.com>
2025-10-04 22:35:29 +0900

    feat(signUp): 회원가입 폼 데이터 변경

--- src/app/(route)/(auth)/_constant/FormData.ts
+++ src/app/(route)/(auth)/_constant/FormData.ts
@@ -10,11 +10,24 @@ export type FormValue = {
 export const signUpInputObject: InputType[] = [
   {
     name: "email",
-    label: "이메일",
+    label: "아이디(이메일)",
     type: "text",
-    placeholder: "이메일을 입력해주세요",
+    placeholder: "로그인에 사용할 이메일을 입력해주세요",
     validation: {
-      required: "이메일은 필수 항목 입니다",
+      required: true,
+      pattern: {
+        value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+        message: "이메일을 입력해주세요",
+      },
+    },
+  },
+  {
+    name: "emailAuth",
+    label: "이메일 인증",
+    type: "number",
+    placeholder: "인증번호를 입력해주세요",
+    validation: {
+      required: false,
       pattern: {
         value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
         message: "이메일을 입력해주세요",
@@ -25,12 +38,12 @@ export const signUpInputObject: InputType[] = [
     name: "password",
     label: "비밀번호",
     type: "password",
-    placeholder: "비밀번호을 입력해주세요. (8~16자, 영문, 숫자, 특수문자 포함)",
+    placeholder: "비밀번호을 입력해 주세요.",
     validation: {
       required: true,
       pattern: {
         value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9])[^\s]{8,16}$/,
-        message: "비밀번호을 입력해주세요. (8~16자, 영문, 숫자, 특수문자 포함)",
+        message: "8~16자리, 영문/숫자/특수문자 포함",
       },
     },
   },
@@ -38,7 +51,7 @@ export const signUpInputObject: InputType[] = [
     name: "passwordConfirm",
     label: "비밀번호 확인",
     type: "password",
-    placeholder: "비밀번호 재입력",
+    placeholder: "비밀번호 입력해 주세요.",
     validation: {
       required: true,
       validate: (value, formValue) => value === formValue || "비밀번호가 일치하지 않습니다.",
@@ -48,12 +61,12 @@ export const signUpInputObject: InputType[] = [
     name: "nickname",
     label: "닉네임",
     type: "text",
-    placeholder: "닉네임을 입력해주세요.",
+    placeholder: "닉네임을 입력해 주세요.",
     validation: {
-      required: "닉네임을 입력해주세요.",
+      required: true,
       maxLength: {
         value: 10,
-        message: "한글+영문 / 한글 + 숫자 등 모두 가능. (10자 이내로)",
+        message: "2~10자, 특수문자/금칙어 제한",
       },
     },
   },
