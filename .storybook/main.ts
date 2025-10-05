97c3b735c20fdf943b1fc97f6abd4d36538d0531
지권 <tjwlrnjs7336@icloud.com>
2025-10-05 23:35:06 +0900

    chore(storybook): 스토리북 롤백 (#89)

---
+++ .storybook/main.ts
@@ -0,0 +1,37 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@chromatic-com/storybook", "@storybook/addon-docs", "@storybook/addon-a11y"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    (config.module!.rules as any[]).forEach((rule: any) => {
+      if (rule?.test instanceof RegExp && rule.test.test(".svg")) {
+        rule.exclude = [/\.svg$/].concat(rule.exclude || []);
+      }
+    });
+    (config.module!.rules as any[]).push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    config.resolve ||= {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "next/navigation": path.resolve(__dirname, "./mock/next-navigation.ts"),
+    };
+
+    return config;
+  },
+};
+export default config;
