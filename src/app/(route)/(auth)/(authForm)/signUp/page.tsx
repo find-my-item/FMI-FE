b56762479169b4a93bc89a7a6125f4a6cb86c211
suhyeon0111 <soo4833@naver.com>
2025-10-07 01:04:25 +0900

    feat(signUp, Input): Input 유효성 검사 에러 검사 제거, signUp 유효성 검사 에러 및 규칙 보여주는 코드 추가

--- src/app/(route)/(auth)/(authForm)/signUp/page.tsx
+++ src/app/(route)/(auth)/(authForm)/signUp/page.tsx
@@ -4,9 +4,13 @@ import { useFormContext } from "react-hook-form";
 import { signUpInputObject } from "../../_constant/FormData";
 import Input from "@/components/Input/Input";
 import Button from "@/components/Button/Button";
+import { cn } from "@/utils/cn";
 
 const Page = () => {
-  const { handleSubmit } = useFormContext();
+  const {
+    handleSubmit,
+    formState: { errors, touchedFields, isSubmitted },
+  } = useFormContext();
 
   const onSubmit = handleSubmit((data) => {
     alert("폼 제출되었습니다.");
@@ -16,29 +20,44 @@ const Page = () => {
     <div className="flex-col-center flex min-h-screen w-full md:flex-row">
       <form onSubmit={onSubmit} className="flex w-full flex-col justify-between">
         <div className="w-full flex-1 gap-5 p-5">
-          {signUpInputObject.map((item) => (
-            <div className="flex w-full flex-col gap-3">
-              <div className="flex min-h-[106px] w-full flex-row items-end gap-[10px]">
-                <Input
-                  key={item.name}
-                  name={item.name}
-                  label={item.label}
-                  type={item.type}
-                  className={item.className}
-                  placeholder={item.placeholder}
-                  validation={item.validation}
-                />
-                {item.name == "emailAuth" && (
-                  <>
-                    <Button>인증번호 발송</Button>
-                  </>
+          {signUpInputObject.map((item) => {
+            const fieldError = errors[item.name]?.message as string;
+            const showError = (!!touchedFields[item.name] || isSubmitted) && !!fieldError;
+            return (
+              <div className="flex w-full flex-col gap-3">
+                <div className="flex min-h-[106px] w-full flex-row items-end gap-[10px]">
+                  <Input
+                    key={item.name}
+                    name={item.name}
+                    label={item.label}
+                    type={item.type}
+                    className={item.className}
+                    placeholder={item.placeholder}
+                    validation={item.validation}
+                  />
+                  {item.name == "emailAuth" && (
+                    <>
+                      <Button>인증번호 발송</Button>
+                    </>
+                  )}
+                  {item.name == "nickname" && <Button>중복 확인</Button>}
+                </div>
+                {item.name == "emailAuth" && <Button>인증번호 확인</Button>}
+                {/* {item.rule && <p className="text-[12px] text-[#787878]">{item.rule}</p>} */}
+                {/* <p className="mt-1 text-[12px] text-red-500">{showError && fieldError && item.rule}</p> */}
+                {(showError || item.rule) && (
+                  <p
+                    className={cn(
+                      "mt-1 text-[12px]",
+                      showError ? "text-red-500" : "text-[#787878]"
+                    )}
+                  >
+                    {showError ? fieldError : item.rule}
+                  </p>
                 )}
-                {item.name == "nickname" && <Button>중복 확인</Button>}
               </div>
-              {item.name == "emailAuth" && <Button>인증번호 확인</Button>}
-              {item.rule && <p className="text-[12px] text-[#787878]">{item.rule}</p>}
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         <hr className="fixed bottom-[102px] left-0 h-px w-full bg-[#E4E4E4]" />
--- src/app/(route)/(auth)/styles/authStyle.ts
+++ src/app/(route)/(auth)/styles/authStyle.ts
@@ -1,4 +1,4 @@
 export const InputStyle =
-  "flex items-center w-full h-[50px] px-[14px] py-[12.5px] bg-[#F5F5F5] rounded-[10px] text-[#9D9D9D] text-[14px]";
+  "flex items-center w-full h-[50px] px-[14px] py-[12.5px] bg-[#F5F5F5] rounded-[10px] text-[#9D9D9D] text-[14px] border focus:border-[#ADADAD] focus:outline-none";
 export const ButtonStyle =
   "w-full h-[50px] flex-center gap-1 rounded-[10px] bg-[#ADADAD] font-semibold text-[16px] text-white";
