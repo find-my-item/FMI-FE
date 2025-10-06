54a77e6787d839f2bfc223f560c5d0703042e26a
wlrnjs <tjwlrnjs7336@icloud.com>
2025-10-07 02:16:57 +0900

    design(detail): 상세페이지 하단 영역 분리

--- src/app/(route)/list/[id]/_components/SimilarItemsSection/SimilarItemsSection.tsx
+++ src/app/(route)/list/[id]/_components/SimilarItemsSection/SimilarItemsSection.tsx
@@ -1,27 +1,17 @@
-import Icon from "@/components/Icon/Icon";
 import React from "react";
+import SimilarItem from "../SimilarItem/SimilarItem";
 
 const SimilarItemsSection = () => {
   return (
     <section className="flex flex-col gap-[16px] px-5 py-[18px]">
       <h2 className="text-[20px] font-semibold">비슷한 분실물</h2>
 
-      <div className="flex flex-col items-start justify-center gap-3">
-        <div className="h-[126px] w-[126px] rounded-[6px] bg-[#D9D9D9]" />
-        <div className="flex flex-col gap-[3px]">
-          <span className="font-semibold">게시글 제목</span>
-          <span className="text-[14px] leading-5 text-[#5D5D5D]">노원구 · 30분 전</span>
-        </div>
-        <ul className="flex items-center gap-2">
-          <li className="flex items-center gap-1">
-            <Icon name="Eye" size={18} aria-hidden="true" />
-            <span className="text-[14px] leading-5 text-[#9D9D9D]">24</span>
-          </li>
-          <li className="flex items-center gap-1">
-            <Icon name="Star" size={18} aria-hidden="true" />
-            <span className="text-[14px] leading-5 text-[#9D9D9D]">12</span>
-          </li>
-        </ul>
+      <div className="hide-scrollbar flex snap-x snap-mandatory gap-4 overflow-x-auto scroll-smooth">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div key={index} className="snap-start">
+            <SimilarItem />
+          </div>
+        ))}
       </div>
     </section>
   );
