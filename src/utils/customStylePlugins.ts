c10f1b809703438be37a3dd227eeb4df0dfadcfa
jun <znznun@gmail.com>
2025-10-11 23:03:11 +0900

    fix: 커스텀 스타일 테일윈즈 플러그인 적용

---
+++ src/utils/customStylePlugins.ts
@@ -0,0 +1,58 @@
+import plugin from "tailwindcss/plugin";
+import type { CSSRuleObject } from "tailwindcss/types/config";
+
+/**
+ * flex-center
+ */
+export const flexCenter = plugin(function ({ addUtilities }) {
+  const utilities: Record<string, CSSRuleObject> = {
+    ".flex-center": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  };
+  addUtilities(utilities);
+});
+
+/**
+ * flex-col-center
+ */
+export const flexColCenter = plugin(function ({ addUtilities }) {
+  const utilities: Record<string, CSSRuleObject> = {
+    ".flex-col-center": {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  };
+  addUtilities(utilities);
+});
+
+/**
+ * mouse-hover
+ */
+export const mouseHover = plugin(function ({ addUtilities }) {
+  const utilities: Record<string, CSSRuleObject> = {
+    ".mouse-hover": {
+      transitionProperty: "colors",
+      transitionDuration: "100ms",
+    },
+  };
+  addUtilities(utilities);
+});
+
+/**
+ * u-ellipsis
+ */
+export const uEllipsis = plugin(function ({ addUtilities }) {
+  const utilities: Record<string, CSSRuleObject> = {
+    ".u-ellipsis": {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+    },
+  };
+  addUtilities(utilities);
+});
