e150d1f31efeb1f8b05889d7f5d5ae05463295f6
권형준 <znznun@gmail.com>
2025-10-12 00:52:44 +0900

    공지사항/고객센터 스토리북 작성 (#104)
    * fix: notice/customer querystring tab add
    
    * style: customer page 1:1 문의하기 button style fix
    
    * style: 스토리북 import 수정, 고객센터 리스트, 상세 스토리북 작성
    
    * fix: workflow file 복구
    
    * fix: clsx 사용
    
    * fix: custom style add
    
    * fix: 커스텀 스타일 테일윈즈 플러그인 적용
    
    ---------
    
    Co-authored-by: 지권 <tjwlrnjs7336@icloud.com>

--- src/components/ManualPopup/ManualPopup.stories.tsx
+++ src/components/ManualPopup/ManualPopup.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/nextjs";
 import ManualPopup from "./ManualPopup";
 
 const meta: Meta<typeof ManualPopup> = {
