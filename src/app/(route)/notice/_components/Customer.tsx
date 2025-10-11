c572eac6b60856332041d4a4b8a1c1c4a507f80f
jun <znznun@gmail.com>
2025-10-11 22:07:24 +0900

    fix: clsx 사용

--- src/app/(route)/notice/_components/Customer.tsx
+++ src/app/(route)/notice/_components/Customer.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 import CustomerList from "./CustomerList";
+import clsx from "clsx";
 
 const Customer = () => {
   return (
     <div className="w-full px-[20px]">
       <Link
         href="notice/customer/inquiry-private"
-        className="mt-[20px] flex h-[50px] w-full items-center justify-center rounded-md bg-[#1EB87B] font-[550] text-white"
+        className={clsx(
+          "mt-[20px] flex h-[50px] w-full items-center justify-center rounded-md bg-[#1EB87B] font-[550] text-white"
+        )}
       >
         1:1 문의하기
       </Link>
