ec795b587222959328f0d34dfa81dfb7b1cc85b7
suhyeon0111 <soo4833@naver.com>
2025-10-13 00:13:08 +0900

    필요없는 코드 제거

--- src/app/(route)/(auth)/(authForm)/sign-up/page.tsx
+++ src/app/(route)/(auth)/(authForm)/sign-up/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { signUpInputObject } from "../../_constant/FormData";
 import Input from "@/components/Input/Input";
 import Button from "@/components/Button/Button";
 import { cn } from "@/utils/cn";
 import { InputStyle, signUpButtonStyle } from "../../_constant/authStyle";
-import Icon from "@/components/Icon/Icon";
 
 const buttonConfig: Record<string, { text: string; className: string }> = {
   email: { text: "인증번호 발송", className: signUpButtonStyle },
