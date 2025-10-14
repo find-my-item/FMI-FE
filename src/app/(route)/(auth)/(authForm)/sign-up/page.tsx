4955ceb7128a77c34f5bc9c0e4e4bc7cbff0b09a
suhyeon0111 <soo4833@naver.com>
2025-10-15 01:44:16 +0900

    design: 약관 동의 페이지 퍼블리싱

--- src/app/(route)/(auth)/(authForm)/sign-up/page.tsx
+++ src/app/(route)/(auth)/(authForm)/sign-up/page.tsx
@@ -8,7 +8,7 @@ import SignUpField from "./_components/SignUpField";
 type Step = "form" | "term"; // 회원가입 단계 타입 정의
 
 const Page = () => {
-  const [step, setStep] = useState<Step>("form"); // 회원가입
+  const [step, setStep] = useState<Step>("term"); // 회원가입
   const { handleSubmit, trigger } = useFormContext();
 
   const onSubmit = handleSubmit((data) => {
@@ -23,7 +23,7 @@ const Page = () => {
 
   return (
     <div className="flex min-h-screen w-full flex-col-center">
-      <form onSubmit={onSubmit} className="flex w-full flex-1 justify-between flex-col-center">
+      <form onSubmit={onSubmit} className="flex w-full flex-1 flex-col justify-between">
         {step === "form" && <SignUpField onNext={onNext} />}
         {step === "term" && <AllAgree />}
       </form>
--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -5,15 +5,17 @@ import Icon from "@/components/Icon/Icon";
 interface checkProps {
   children: string;
   name: string;
+  boxSize?: string;
+  className?: string;
 }
 
 const CheckBox = ({ children, name, ...rest }: checkProps) => {
   return (
     <label htmlFor={name} className="flex cursor-pointer items-center">
-      <input name={name} type="checkbox" />
-      {/* <div className="relative grid h-3 w-3 place-items-center rounded bg-[#E4E4E4] peer-checked:bg-[#1EB87B] [&_svg]:opacity-0 peer-checked:[&_svg]:opacity-100">
+      <input name={name} type="checkbox" className="peer sr-only" />
+      <div className="relative h-6 w-6 rounded bg-[#E4E4E4] flex-center peer-checked:bg-[#1EB87B] [&_svg]:opacity-0 peer-checked:[&_svg]:opacity-100">
         <Icon name="Check" size={5} title="체크됨" />
-      </div> */}
+      </div>
       <span className="ml-3 text-[#9D9D9D]">{children}</span>
     </label>
   );
