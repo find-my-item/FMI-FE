0e283b3a0cecc428b064764bdff65f4135d8195e
wlrnjs <wlrnjs5824@gmail.com>
2025-10-24 00:12:22 +0900

    design(status): 토스트 아이콘 변경 및 코드 수정

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -26,3 +26,4 @@ export { default as Share } from "@/assets/share.svg";
 export { default as DetailMenu } from "@/assets/detail-menu.svg";
 export { default as Success } from "@/assets/success.svg";
 export { default as Warning } from "@/assets/warning.svg";
+export { default as Error } from "@/assets/error.svg";
--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -12,7 +12,7 @@ function getBgByType(type: ToastType): string {
     case "success":
       return "bg-[#46C691]";
     case "error":
-      return "bg-[#FF5C5C]";
+      return "bg-[#FF4242]";
     case "warning":
       return "bg-[#FFC642]";
     default:
@@ -20,13 +20,12 @@ function getBgByType(type: ToastType): string {
   }
 }
 
-// TODO(지권): Error 아이콘 없음 및 피그마 아이콘 이슈
 function getIconByType(type: ToastType): IconName {
   switch (type) {
     case "success":
       return "Success";
-    // case "error":
-    //   return "Error";
+    case "error":
+      return "Error";
     case "warning":
       return "Warning";
     default:
@@ -34,11 +33,24 @@ function getIconByType(type: ToastType): IconName {
   }
 }
 
+function getIconBySize(type: ToastType): number {
+  switch (type) {
+    case "success":
+      return 16;
+    case "error":
+      return 20;
+    case "warning":
+      return 10;
+    default:
+      return 16;
+  }
+}
+
 const Toast = ({ message = "Text", type }: ToastProps) => {
   return (
-    <div className="glass-card min-w-[260px] gap-3 rounded-lg bg-[#5D5D5D]/70 px-5 py-3 text-white shadow-md flex-center">
+    <div className="glass-card w-[300px] gap-3 rounded-lg bg-[#5D5D5D]/70 px-5 py-3 text-white shadow-md flex-center">
       <div className={cn(getBgByType(type), "h-5 w-5 rounded-full flex-center")}>
-        <Icon name={getIconByType(type)} />
+        <Icon name={getIconByType(type)} size={getIconBySize(type)} />
       </div>
       {message}
     </div>
