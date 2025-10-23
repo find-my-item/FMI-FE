d2c85c3128ad627210a3766a50361303cf8b20de
suhyeon0111 <soo4833@naver.com>
2025-10-24 01:38:25 +0900

    fix(icon): 아이콘 중앙 배치 코드 변경

---
+++ src/components/CheckBox/CheckBox.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import CheckBox from "./CheckBox";
+
+const meta: Meta<typeof CheckBox> = {
+  title: "공통 컴포넌트/CheckBox",
+  component: CheckBox,
+  tags: ["autodocs"],
+  args: {
+    id: "agree",
+    label: "전체 약관 동의",
+    state: false,
+  },
+  argTypes: {
+    id: { control: "text" },
+    label: { control: "text" },
+  },
+  parameters: {
+    layout: "centered",
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof CheckBox>;
+
+export const Default: Story = {};
+
+export const CustomChecked: Story = {
+  args: {
+    id: "allTerms",
+    label: "전체 동의",
+    state: false,
+    boxSize: "w-[18px] h-[18px]",
+    textStyle: "text-[12px]",
+    iconSize: "h-[6px]",
+  },
+};
