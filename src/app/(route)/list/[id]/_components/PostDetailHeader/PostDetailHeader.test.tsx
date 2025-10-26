cfe6602f7847c5fca3fdc908b12a51fc62fb7d60
wlrnjs <wlrnjs5824@gmail.com>
2025-10-26 17:40:15 +0900

    refactor: 테스트 코드 수정

--- src/app/(route)/list/[id]/_components/PostDetailHeader/PostDetailHeader.test.tsx
+++ src/app/(route)/list/[id]/_components/PostDetailHeader/PostDetailHeader.test.tsx
@@ -2,24 +2,24 @@ import { render, screen } from "@testing-library/react";
 import PostDetailHeader from "./PostDetailHeader";
 
 describe("상세페이지 상단 헤더", () => {
-  it("상세페이지 헤더가 렌더링되어야 한다.", () => {
+  it("헤더가 렌더링되어야 한다.", () => {
     render(<PostDetailHeader />);
 
     const postDetailHeaderElement = screen.getByLabelText("상세페이지 유저 정보");
     expect(postDetailHeaderElement).toBeInTheDocument();
   });
 
-  it("상세페이지 닉네임이 렌더링되어야 한다.", () => {
+  it("닉네임이 렌더링되어야 한다.", () => {
     render(<PostDetailHeader />);
 
     const postDetailHeaderElement = screen.getByText("글자확인용임시닉네임");
     expect(postDetailHeaderElement).toBeInTheDocument();
   });
 
-  it("상세페이지 채팅하러가기 버튼이 렌더링되어야 한다.", () => {
+  it("채팅하러가기 버튼이 렌더링되어야 한다.", () => {
     render(<PostDetailHeader />);
 
-    const postDetailHeaderElement = screen.getByRole("button", { name: "채팅하러 가기" });
+    const postDetailHeaderElement = screen.getByRole("link", { name: "채팅하러 가기" });
     expect(postDetailHeaderElement).toBeInTheDocument();
   });
 });
--- src/app/(route)/list/[id]/_components/SimilarItem/SimilarItem.test.tsx
+++ src/app/(route)/list/[id]/_components/SimilarItem/SimilarItem.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import SimilarItem from "./SimilarItem";
 
 describe("비슷한 분실물 아이템", () => {
+  // TODO(지권): 이미지 태그 변경 후 테스트 코드 추가
   // it("비슷한 분실물 아이템 이미지가 렌더링되어야 한다.", () => {
   //   render(<SimilarItem />);
 
@@ -9,21 +10,21 @@ describe("비슷한 분실물 아이템", () => {
   //   expect(similarItemElement).toBeInTheDocument();
   // });
 
-  it("비슷한 분실물 아이템 제목이 렌더링되어야 한다.", () => {
+  it("제목이 렌더링되어야 한다.", () => {
     render(<SimilarItem />);
 
     const similarItemElement = screen.getByText("게시글 제목");
     expect(similarItemElement).toBeInTheDocument();
   });
 
-  it("비슷한 분실물 아이템 위치가 렌더링되어야 한다.", () => {
+  it("위치가 렌더링되어야 한다.", () => {
     render(<SimilarItem />);
 
     const similarItemElement = screen.getByText("노원구 · 30분 전");
     expect(similarItemElement).toBeInTheDocument();
   });
 
-  it("비슷한 분실물 아이템 조회수와 좋아요가 렌더링되어야 한다.", () => {
+  it("위치와 시간이 렌더링되어야 한다.", () => {
     render(<SimilarItem />);
 
     const similarItemElement = screen.getByText("노원구 · 30분 전");
