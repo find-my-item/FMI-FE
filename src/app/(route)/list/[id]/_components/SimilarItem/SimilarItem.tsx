835ca65544dc43e787832cf17fa286d958d3d58b
지권 <tjwlrnjs7336@icloud.com>
2025-10-27 13:41:21 +0900

    상세페이지 디자인 수정 및 테스트 코드 추가 (#138)
    * design(header): 상세페이지 헤더 수정
    
    * design(footer): 상세페이지 하단 디자인 수정
    
    * design(commentForm): 댓글 전송 폼 디자인 수정
    
    * test(detail): 상세페이지 테스트 코드 추가
    
    * refactor: 테스트 코드 수정

--- src/app/(route)/list/[id]/_components/SimilarItem/SimilarItem.tsx
+++ src/app/(route)/list/[id]/_components/SimilarItem/SimilarItem.tsx
@@ -1,22 +1,36 @@
-import Icon from "@/components/Icon/Icon";
+import Icon, { IconName } from "@/components/Icon/Icon";
 
 const SimilarItem = () => {
+  const IconList = [
+    {
+      name: "Eye",
+      value: 23,
+      ariaLabel: "조회수",
+    },
+    {
+      name: "Star",
+      value: 12,
+      ariaLabel: "즐겨찾기",
+    },
+  ];
+
   return (
     <div className="flex flex-col items-start justify-center gap-3">
+      {/* TODO(지권): 추후 이미지 컴포넌트 변경 */}
       <div className="h-[126px] w-[126px] rounded-[6px] bg-[#D9D9D9]" />
+
       <div className="flex flex-col gap-[3px]">
-        <span className="font-semibold">게시글 제목</span>
-        <span className="text-[14px] leading-5 text-[#5D5D5D]">노원구 · 30분 전</span>
+        <span className="font-semibold text-[#363636]">게시글 제목</span>
+        <span className="text-[14px] leading-5 text-[#787878]">노원구 · 30분 전</span>
       </div>
+
       <ul className="flex items-center gap-2">
-        <li className="flex items-center gap-1">
-          <Icon name="Eye" size={18} aria-hidden="true" />
-          <span className="text-[14px] leading-5 text-[#9D9D9D]">24</span>
-        </li>
-        <li className="flex items-center gap-1">
-          <Icon name="Star" size={18} aria-hidden="true" />
-          <span className="text-[14px] leading-5 text-[#9D9D9D]">12</span>
-        </li>
+        {IconList.map((icon, index) => (
+          <li key={index} className="flex items-center gap-1" aria-label={`${icon.ariaLabel}`}>
+            <Icon name={icon.name as IconName} size={18} aria-hidden="true" />
+            <span className="text-[14px] leading-5 text-[#9D9D9D]">{icon.value}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
