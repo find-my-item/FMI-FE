3ff2a6a869d627c4fef8e359539af3ad11c7fbde
jun <znznun@gmail.com>
2025-10-28 20:32:39 +0900

    index.ts 중복 import 수정

--- src/app/(route)/notice/_components/index.ts
+++ src/app/(route)/notice/_components/index.ts
@@ -1,7 +1,7 @@
 export { default as CommentList } from "./CommentList/CommentList";
 export { default as Customer } from "./Customer/Customer";
 export { default as CustomerItem } from "./CustomerItem/CustomerItem";
-export { default as CustomerList } from "./CommentList/CommentList";
+export { default as CustomerList } from "./CustomerList/CustomerList";
 export { default as NoticeChip } from "./NoticeChip/NoticeChip";
 export { default as NoticeDetailHeader } from "./NoticeDetailHeader/NoticeDetailHeader";
 export { default as NoticeView } from "./NoticeView/NoticeView";
