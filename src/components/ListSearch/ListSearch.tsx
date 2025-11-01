f4890742ce9bafa2ff6f5469eb5042ce25b301b1
jun <znznun@gmail.com>
2025-11-02 01:52:41 +0900

    feat: 쿼리스트링 커스텀 훅 적용

--- src/components/ListSearch/ListSearch.tsx
+++ src/components/ListSearch/ListSearch.tsx
@@ -1,12 +1,14 @@
-"use client";
+interface ListSearch {
+  placeholder: string;
+}
 
-const ListSearch = () => {
+const ListSearch = ({ placeholder }: ListSearch) => {
   return (
     <div>
       <form className="px-[20px] py-[10px]">
         <input
           type="text"
-          placeholder="제목, 내용을 입력해 주세요."
+          placeholder={placeholder}
           className="min-h-[40px] w-full rounded-full px-[20px] py-[8px] text-body1-regular text-neutral-normal-placeholder bg-fill-neutral-subtle-default"
         />
       </form>
