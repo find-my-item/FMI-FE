76652a44cc7fc5bdea5019520c5b34f4c8643f26
wlrnjs <wlrnjs5824@gmail.com>
2025-11-02 04:55:29 +0900

    design(detail): 상세페이지 토큰 적용

--- src/app/(route)/list/[id]/_components/SimilarItem/SimilarItem.tsx
+++ src/app/(route)/list/[id]/_components/SimilarItem/SimilarItem.tsx
@@ -18,18 +18,20 @@ const SimilarItem = () => {
   return (
     <div className="flex flex-col items-start justify-center gap-3">
       {/* TODO(지권): 추후 이미지 컴포넌트 변경 */}
-      <div className="h-[126px] w-[126px] rounded-[6px] bg-[#D9D9D9]" />
+      <div className="h-[126px] w-[126px] rounded-[6px] bg-flatGray-100" />
 
       <div className="flex flex-col gap-[3px]">
-        <span className="font-semibold text-[#363636]">게시글 제목</span>
-        <span className="text-[14px] leading-5 text-[#787878]">노원구 · 30분 전</span>
+        <span className="font-semibold text-layout-header-default">게시글 제목</span>
+        <span className="text-[14px] leading-5 text-layout-body-default">노원구 · 30분 전</span>
       </div>
 
       <ul className="flex items-center gap-2">
         {IconList.map((icon, index) => (
           <li key={index} className="flex items-center gap-1" aria-label={`${icon.ariaLabel}`}>
             <Icon name={icon.name as IconName} size={18} aria-hidden="true" />
-            <span className="text-[14px] leading-5 text-[#9D9D9D]">{formatNumber(icon.value)}</span>
+            <span className="text-caption1-regular text-neutral-normal-placeholder">
+              {formatNumber(icon.value)}
+            </span>
           </li>
         ))}
       </ul>
