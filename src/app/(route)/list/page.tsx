2b54bea16af6e237ed70eb100584e605627f533f
jun <znznun@gmail.com>
2025-11-02 01:52:41 +0900

    feat: 쿼리스트링 커스텀 훅 적용

--- src/app/(route)/list/page.tsx
+++ src/app/(route)/list/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { Dropdown, Tab } from "@/components";
+import { Dropdown, Filter, Tab } from "@/components";
 import { useState } from "react";
 import ListItem from "./_components/ListItem/ListItem";
 import Icon from "@/components/Icon/Icon";
 import { DetailHeader } from "@/components/index";
 import { TABS } from "./_constants/TABS";
 import ListSearch from "@/components/ListSearch/ListSearch";
-
-type SearchMode = "default" | "region" | "post";
+import useSearchUpdateQueryString from "@/hooks/useSearchUpdateQueryString";
+import { SEARCH_HEADER_TITLE } from "./_constants/SEARCH_HEADER_TITLE";
 
 const page = () => {
   const [selected, setSelected] = useState("LOST");
   const [selectedRegion, setSelectedRegion] = useState("지역선택");
   const [selectedView, setSelectedView] = useState("조회순");
   const [selectedCategory, setSelectedCategory] = useState("찾는중");
-  const [searchMode, setSearchMode] = useState<SearchMode>("default");
+  const { searchMode, searchUpdateQuery } = useSearchUpdateQueryString();
 
   const dropdowns = [
     { value: selectedRegion, setValue: setSelectedRegion, icon: "Position" },
@@ -23,30 +23,34 @@ const page = () => {
     { value: selectedCategory, setValue: setSelectedCategory, icon: "ArrowDown" },
   ] as const;
 
-  const headerTitle = {
-    default: "게시글",
-    region: "지연 선택",
-    post: "게시글 검색",
-  };
-
   return (
     <>
-      <DetailHeader title={headerTitle[searchMode]}>
-        <DetailHeader.Search ariaLabel="게시글 검색" onClick={() => setSearchMode("post")} />
+      <DetailHeader title={SEARCH_HEADER_TITLE[searchMode]}>
+        <DetailHeader.Search
+          ariaLabel="게시글 검색"
+          onClick={() => searchUpdateQuery("search", "post")}
+        />
       </DetailHeader>
 
-      {searchMode === "default" ? (
+      {searchMode === "default" && (
         <>
           <Tab tabs={TABS} selected={selected} onValueChange={setSelected} />
 
           <div className="flex h-[67px] w-full items-center gap-2 px-5">
-            {dropdowns.map(({ value, setValue, icon }, idx) => (
+            <Filter
+              ariaLabel="지역 선택 필터 버튼"
+              children={"지역 선택"}
+              onSelected={false}
+              icon={{ name: "Location", size: 16 }}
+              onClick={() => searchUpdateQuery("search", "region")}
+            />
+            {/* {dropdowns.map(({ value, setValue, icon }, idx) => (
               <Dropdown key={idx} options={[]} onSelect={setValue} className="gap-[4px]">
                 {idx === 0 && <Icon name={icon} size={16} />}
                 <span className="text-[16px] font-semibold text-[#525252]">{value}</span>
                 {idx !== 0 && <Icon name="ArrowDown" size={12} />}
               </Dropdown>
-            ))}
+            ))} */}
           </div>
 
           {/* 아이템 */}
@@ -63,8 +67,13 @@ const page = () => {
             ))}
           </div>
         </>
-      ) : (
-        <ListSearch />
+      )}
+      {searchMode !== "default" && (
+        <ListSearch
+          placeholder={
+            searchMode === "post" ? "제목, 내용을 입력해 주세요." : "시/군/구를 입력해 주세요."
+          }
+        />
       )}
     </>
   );
