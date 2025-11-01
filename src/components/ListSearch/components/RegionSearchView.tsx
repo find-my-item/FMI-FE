796e5c47aca685eb7dce4cda639add8e8f30c86b
jun <znznun@gmail.com>
2025-11-02 04:32:09 +0900

    fix: RegionSearchView Button 공통 컴포넌트 적용/Button 컴포넌트 ignoreBase prop추가

--- src/components/ListSearch/components/RegionSearchView.tsx
+++ src/components/ListSearch/components/RegionSearchView.tsx
@@ -1,16 +1,19 @@
+import Button from "@/components/Button/Button";
+
 const RegionSearchView = () => {
   const regions = [{ value: "서울시 광진구" }, { value: "경기 광명시" }];
 
   return (
     <section>
       {regions.map((i) => (
-        <button
+        <Button
           key={i.value}
-          aria-label={`지역 선택 ${i.value}`}
-          className="min-h-[60px] w-full border-b border-neutral-normal-default px-[20px] py-[20px] text-left text-body2-medium text-neutral-strong-default"
+          ariaLabel={`지역 선택 ${i.value}`}
+          variant="regionSearchList"
+          ignoreBase
         >
           {i.value}
-        </button>
+        </Button>
       ))}
     </section>
   );
