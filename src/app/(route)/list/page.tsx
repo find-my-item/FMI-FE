89084cbf9cc822ba0073127c91604837db053e69
jun <znznun@gmail.com>
2025-11-02 03:26:33 +0900

    delete: 불필요 주석 제거

--- src/app/(route)/list/page.tsx
+++ src/app/(route)/list/page.tsx
@@ -3,7 +3,6 @@ import { Suspense } from "react";
 
 const page = () => {
   return (
-    // 형준: useSearchParams 훅 사용을 위해 별도 컴포넌트 분리 후, Suspense 적용
     <Suspense fallback="">
       <ListView />
     </Suspense>
