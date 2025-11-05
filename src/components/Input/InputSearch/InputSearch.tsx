2e7c20acf8e65c1db56791f2ff9b49d3db1d8d0b
suhyeon0111 <soo4833@naver.com>
2025-11-05 13:26:49 +0900

    feat(InputSearch): isRHF, onChange방식 둘 중 하나 선택 로직 추가

--- src/components/Input/InputSearch/InputSearch.tsx
+++ src/components/Input/InputSearch/InputSearch.tsx
@@ -3,40 +3,50 @@
 import { InputHTMLAttributes } from "react";
 import Icon from "../../Icon/Icon";
 import { useState } from "react";
-import { UseFormRegisterReturn } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 import DeleteButton from "../_internal/DeleteButton/DeleteButton";
 
 interface InputSearchProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label?: string;
   className?: string;
+  mode: "RHF" | "onChange";
+  // onEnter?: (value: string) => void;
 }
 
-interface RfhMode {
-  mode: "RFH";
-  register: UseFormRegisterReturn;
-}
+const InputSearch = ({ name, mode, label, ...props }: InputSearchProps) => {
+  const { register, watch, setValue } = useFormContext();
+  const rhfValue = watch(name) || "";
 
-const InputSearch = ({ ...props }: InputSearchProps) => {
-  const [value, setValue] = useState("");
+  const [innerValue, setInnerValue] = useState("");
+
+  const value = mode === "onChange" ? innerValue : undefined;
+  const onChange =
+    mode === "onChange"
+      ? props.onChange
+      : (e: React.ChangeEvent<HTMLInputElement>) => setInnerValue(e.target.value);
+
+  const onChangeDelete = () => {
+    mode === "RHF" ? setValue(name, "") : setInnerValue("");
+  };
 
   return (
     <div className="relative flex w-full flex-row gap-2">
       <Icon name="Search" size={16} className="absolute left-5 top-1/2 -translate-y-1/2" />
 
       <input
+        {...(mode === "RHF" ? register(name) : {})}
         {...props}
         className="h-11 min-w-0 flex-1 rounded-[24px] border px-10 text-body1-regular text-neutral-normal-placeholder bg-fill-neutral-subtle-default hover:text-neutral-normal-hover focus:border-black focus:text-neutral-normal-focused"
-        placeholder="검색어 키워드를 입력해 주세요."
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={onChange}
       />
 
       {/* 삭제 버튼 */}
       <DeleteButton
-        value={value}
+        value={mode === "RHF" ? rhfValue : innerValue}
         className="right-5 top-1/2 -translate-y-1/2"
-        onDelete={() => setValue("")}
+        onDelete={onChangeDelete}
       />
     </div>
   );
