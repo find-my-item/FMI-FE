ad2bdbfd66830190de8ca8c4f8ec4ff60e13fc85
suhyeon0111 <soo4833@naver.com>
2025-11-06 00:53:50 +0900

    test(InputChat): storybook 추가

--- src/components/Input/InputChat/InputChat.stories.tsx
+++ src/components/Input/InputChat/InputChat.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/nextjs";
 import { useForm, FormProvider } from "react-hook-form";
-import InputField from "./InputField";
+import InputChat from "./InputChat";
 
-const meta: Meta<typeof InputField> = {
-  title: "공통 컴포넌트/InputField",
-  component: InputField,
+const meta: Meta<typeof InputChat> = {
+  title: "공통 컴포넌트/InputChat",
+  component: InputChat,
   tags: ["autodocs"],
   decorators: [
     (Story) => {
@@ -26,21 +26,13 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    label: "테스트",
     name: "테스트",
-    validation: { maxLength: { value: 20, message: "최대길이는 20자입니다." } },
-    placeholder: "20자 이내로 입력해주세요.",
-    rule: "caption",
   },
 };
 
 export const disabled: Story = {
   args: {
-    label: "테스트",
     name: "테스트",
-    validation: { required: true, maxLength: { value: 20, message: "최대길이는 20자입니다." } },
-    placeholder: "20자 이내로 입력해주세요.",
-    rule: "caption",
     disabled: true,
   },
 };
