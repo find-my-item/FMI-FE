431cf431bafb36c8bf25077350e42e2ab041f36b
suhyeon0111 <soo4833@naver.com>
2025-11-06 00:23:16 +0900

    test(InputText): storybook 추가

--- src/components/Input/InputText/InputText.stories.tsx
+++ src/components/Input/InputText/InputText.stories.tsx
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/nextjs";
 import { useForm, FormProvider } from "react-hook-form";
-import Input from "./InputText";
+import InputText from "./InputText";
 
-const meta: Meta<typeof Input> = {
-  title: "공통 컴포넌트/Input",
-  component: Input,
+const meta: Meta<typeof InputText> = {
+  title: "공통 컴포넌트/InputText",
+  component: InputText,
   tags: ["autodocs"],
   argTypes: {
     placeholder: { control: "text" },
-    required: { control: "boolean" },
+    validation: { control: "object" },
   },
   decorators: [
     (Story, context) => {
@@ -16,7 +16,7 @@ const meta: Meta<typeof Input> = {
 
       return (
         <FormProvider {...methods}>
-          <form onSubmit={(e) => e.preventDefault()}>
+          <form onSubmit={(e) => e.preventDefault()} className="w-[480px]">
             <Story />
           </form>
         </FormProvider>
@@ -30,18 +30,21 @@ type Story = StoryObj<typeof meta>;
 
 export const Text: Story = {
   args: {
+    label: "아이디(이메일)",
     name: "email",
+    validation: { required: true, maxLength: { value: 10, message: "최대길이는 10자입니다." } },
     type: "text",
     placeholder: "이메일을 입력해주세요",
-    className: "px-3 py-3 w-[480px] h-[46px]",
+    rule: "이메일은 필수 항목입니다.",
   },
 };
 
 export const Password: Story = {
   args: {
+    label: "비밀번호",
     name: "password",
+    validation: { required: false },
     type: "password",
     placeholder: "비밀번호를 입력하세요",
-    className: "px-3 py-3 w-[480px] h-[46px]",
   },
 };
