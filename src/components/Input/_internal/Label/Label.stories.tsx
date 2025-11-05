9a08636575abf93856f2366a378cc0e2afab94d0
suhyeon0111 <soo4833@naver.com>
2025-11-06 02:21:54 +0900

    test(Label): storybook 코드 추가

---
+++ src/components/Input/_internal/Label/Label.stories.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Meta, StoryObj } from "@storybook/nextjs";
+import Label from "./Label";
+
+const meta: Meta<typeof Label> = {
+  title: "공통 컴포넌트/Input/Internal/Label",
+  component: Label,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "이메일(아이디)",
+    name: "email",
+    required: true,
+  },
+};
+
+export const noRequired: Story = {
+  args: {
+    label: "이메일(아이디)",
+    name: "email",
+    required: false,
+  },
+};
