165beb8000d9f303d95c660abe5f28447f6e453a
jun <znznun@gmail.com>
2025-11-07 02:24:58 +0900

    feat(chat): chat/components 배럴패턴

---
+++ src/app/(route)/chat/_components/index.ts
@@ -0,0 +1,3 @@
+export { default as ChatItem } from "./ChatItem/ChatItem";
+export { default as DefaultList } from "./DefaultList/DefaultList";
+export { default as ListView } from "./ListView/ListView";
