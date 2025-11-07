4a28ab2ab16b9918f5731148687ce2c227b490b7
wlrnjs <wlrnjs5824@gmail.com>
2025-11-07 20:25:40 +0900

    test(Modal): 테스트 코드 추가

--- src/components/Modal/ModalLayout.stories.tsx
+++ src/components/Modal/ModalLayout.stories.tsx
@@ -36,10 +36,10 @@ export const WithContent: Story = {
     children: (
       <>
         <h2>Modal Title</h2>
-        <p className="flex-center h-[100px]">Modal Content</p>
+        <p className="h-[100px] flex-center">Modal Content</p>
         <span>Modal Footer</span>
         <button
-          className="mouse-hover mt-4 rounded border border-gray-300 px-4 py-2 hover:border-gray-500 hover:text-black"
+          className="mt-4 rounded border border-gray-300 px-4 py-2 mouse-hover hover:border-gray-500 hover:text-black"
           onClick={() => alert("Closed")}
         >
           Close
