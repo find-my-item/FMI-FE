6f952ecb08f95e166b9343d8f86956595cccee09
jun <znznun@gmail.com>
2025-11-07 13:19:58 +0900

    feat(chatRoom): ChatChip 상수 파일 분리

---
+++ src/app/(route)/chat/[roomId]/_components/ChatChip/CHAT_CHIP_MODE.ts
@@ -0,0 +1,4 @@
+export const CHAT_CHIP_MODE = {
+  find: { style: "text-accent-foundItem bg-fill-accent-fountItem", text: "습득물" },
+  lost: { style: "text-accent-lostItem bg-fill-accent-lostItem", text: "분실물" },
+};
