6cbf5e28091961d63f0cee6ac8ba95ddf57d0a09
wlrnjs <wlrnjs5824@gmail.com>
2025-11-07 18:44:46 +0900

    test(Badge): 테스트 코드 추가

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Badge from "./Badge";
+
+describe("Badge", () => {
+  it("variant가 new일 때 라벨이 NEW로 렌더링됩니다", () => {
+    render(<Badge variant="new" />);
+    expect(screen.getByText("N")).toBeInTheDocument();
+  });
+
+  it("variant가 hot일 때 라벨이 HOT로 렌더링됩니다", () => {
+    render(<Badge variant="hot" />);
+    expect(screen.getByText("H")).toBeInTheDocument();
+  });
+
+  it("variant가 new일 때 배경색이 #1EB87B로 렌더링됩니다", () => {
+    render(<Badge variant="new" />);
+    expect(screen.getByText("N")).toHaveClass("bg-[#1EB87B]");
+  });
+
+  it("variant가 hot일 때 배경색이 FF4242로 렌더링됩니다", () => {
+    render(<Badge variant="hot" />);
+    expect(screen.getByText("H")).toHaveClass("bg-[#FF4242]");
+  });
+});
