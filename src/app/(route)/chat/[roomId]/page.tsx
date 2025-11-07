1be873e400b719ff539609d5adb7c2f2d0ead9b1
jun <znznun@gmail.com>
2025-11-07 14:20:42 +0900

    feat(chatRoom): InputChat구현

--- src/app/(route)/chat/[roomId]/page.tsx
+++ src/app/(route)/chat/[roomId]/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import EmptyChatRoom from "./_components/EmptyChatRoom/EmptyChatRoom";
+import { InputChat } from "@/components";
+import { FormProvider, useForm } from "react-hook-form";
 
 const ChatRoom = () => {
+  const methods = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+  });
+
   return (
     <>
       <EmptyChatRoom />
+      <FormProvider {...methods}>
+        <div className="px-[16px] pb-[24px] pt-[12px]">
+          <InputChat name="chatRoom" />
+        </div>
+      </FormProvider>
     </>
   );
 };
