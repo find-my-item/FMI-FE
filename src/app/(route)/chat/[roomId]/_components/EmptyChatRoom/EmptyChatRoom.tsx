a1c694b7f80dd544527a60acc9c1217d007e38dd
jun <znznun@gmail.com>
2025-11-07 16:14:58 +0900

    feat(chatRoom): empty컴포넌트 상수 분리

--- src/app/(route)/chat/[roomId]/_components/EmptyChatRoom/EmptyChatRoom.tsx
+++ src/app/(route)/chat/[roomId]/_components/EmptyChatRoom/EmptyChatRoom.tsx
@@ -1,18 +1,7 @@
 import { Icon } from "@/components";
-import { Props } from "@/components/Icon/Icon";
+import { EMPTY_MODE_STYLE } from "./EMPTY_MODE_STYLE";
 
 const EmptyChatRoom = ({ postMode }: { postMode: "find" | "lost" }) => {
-  const EMPTY_MODE_STYLE = {
-    lost: {
-      iconName: "ChatLost" as Props["name"],
-      helpText: ["이 분실물을 주우셨나요?", "주인에게 먼저 연락해보세요."],
-    },
-    find: {
-      iconName: "ChatFind" as Props["name"],
-      helpText: ["내 물건이 맞는지 확인해보세요.", "습득자에게 메시지를 보내세요."],
-    },
-  };
-
   return (
     <section className="flex-1 bg-flatGray-25 flex-center">
       <h1 className="sr-only">빈 채팅 안내 화면</h1>
