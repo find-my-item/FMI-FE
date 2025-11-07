241da669564a0f6d84a0081fb453d0ac45827519
jun <znznun@gmail.com>
2025-11-07 13:18:14 +0900

    feat(chatRoom): ChatRoomHeader/ChatChip 퍼블리싱

---
+++ src/app/(route)/chat/[roomId]/page.tsx
@@ -0,0 +1,11 @@
+import ChatRoomHeader from "./_components/ChatRoomHeader/ChatRoomHeader";
+
+const ChatRoom = () => {
+  return (
+    <div>
+      <ChatRoomHeader />
+    </div>
+  );
+};
+
+export default ChatRoom;
