2245b4517bc8e801e098294f753fc2a64dae30c6
suhyeon0111 <soo4833@naver.com>
2025-11-07 15:15:02 +0900

    docs(InputField): TS Doc 추가

--- src/components/Input/InputField/InputField.tsx
+++ src/components/Input/InputField/InputField.tsx
@@ -20,44 +20,24 @@ import { useFormInput } from "../_internal/_hooks/useFormInput";
  *
  * @param items -  서술형 텍스트를 작성할 수 있는 input 공통 컴포넌트 props입니다.
  *  - 'name': 입력 필드의 id 및 register함수 사용을 위한 name
- *  - `validation`: 입력 필드의 유효성 검사를 위한 RegisterOption
- *  - 'disabled': 입력필드, 버튼을 disabled처리
  *  - 'label': 라벨의 텍스트
- *  - 'children': 버튼 텍스트 (동시에 버튼 결합 형태를 사용할 것을 의미합니다.)
- *  - 'eyeShow': 비밀번호 타입을 사용할 때 비밀번호 숨기기/보이기 기능을 추가
- *  - 'btnOnClick': 버튼이 눌렸을때 발생할 이벤트를 넣을 수 있는 옵션
- *  - 'isSuccess': 성공 caption을 보여주기 위한 옵션
- *  - 'successMessage': caption에 나타날 성공 메시지
- *  - 'rule':
+ *  - 'rule': capation에 나타날 입력 필드의 규칙
+ *  - `validation`: 입력 필드의 유효성 검사를 위한 RegisterOption
  *
  *
- * @example 입력필드만 사용
+ * @example
  * ```tsx
  * <FormProvider {...methods}>
  *   <form onSubmit={methods.handleSubmit(onSubmit)}>
- *     <InputText
+ *     <InputField
  *       name="test"
- *       validation={{maxLength: {value: 10, message: "10자 이내로 입력해주세요."}}}
+ *       validation={{maxLength: {value: 30, message: "30자 이내로 입력해주세요."}}}
  *       label="테스트"
- *       rule="2~10자 이내로 입력해주세요."
+ *       rule="2~30자 이내로 입력해주세요."
  *     />
  *   </form>
  * </FormProvider>
  * ```
- *
- * ```tsx 버튼 결합 형태
- * <FormProvider {...methods}>
- *   <form onSubmit={methods.handleSubmit(onSubmit)}>
- *     <InputText
- *       name="test"
- *       validation={{maxLength: {value: 10, message: "10자 이내로 입력해주세요."}}}
- *       label="테스트"
- *       rule="2~10자 이내로 입력해주세요."
- *       btnOnClick={(v) => console.log(v)}
- *     > 버튼이름
- *    </InputText>
- *   </form>
- * </FormProvider>
  *
  *
  */
