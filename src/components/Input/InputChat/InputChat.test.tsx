e293e7b7b661bb7c620387374aae86a917fe9317
jun <znznun@gmail.com>
2025-11-10 16:28:28 +0900

    feat(chatInput): InputChat.test 주석 제거

--- src/components/Input/InputChat/InputChat.test.tsx
+++ src/components/Input/InputChat/InputChat.test.tsx
@@ -53,7 +53,6 @@ const renderComponent = (
 };
 
 describe("InputChat 컴포넌트", () => {
-  // 테스트 1
   it("필수 요소들이 올바르게 렌더링 되는지 확인", () => {
     renderComponent();
 
@@ -66,7 +65,6 @@ describe("InputChat 컴포넌트", () => {
     expect(screen.getByText("Send")).toBeInTheDocument();
   });
 
-  // 테스트 2
   it("입력창에 텍스트를 입력할 수 있는지 확인", async () => {
     const { user } = renderComponent();
     const input = screen.getByPlaceholderText<HTMLTextAreaElement>("메시지 보내기");
@@ -75,7 +73,6 @@ describe("InputChat 컴포넌트", () => {
     expect(input.value).toBe("안녕하세요");
   });
 
-  // 테스트 3
   it("텍스트 입력 시 입력창에 'text-neutral-strong-focused' 클래스가 적용되는지 확인", async () => {
     const { user } = renderComponent();
     const input = screen.getByPlaceholderText("메시지 보내기");
@@ -86,7 +83,6 @@ describe("InputChat 컴포넌트", () => {
     expect(input).toHaveClass("text-neutral-strong-focused");
   });
 
-  // 테스트 4
   it("전송 버튼이 비어있는 입력값일 때 비활성화되는지 확인", () => {
     renderComponent();
     const sendButton = screen.getByLabelText("전송 버튼");
@@ -94,7 +90,6 @@ describe("InputChat 컴포넌트", () => {
     expect(sendButton).toBeDisabled();
   });
 
-  // 테스트 5
   it("전송 버튼이 입력값이 있을 때 활성화되는지 확인", async () => {
     const { user } = renderComponent();
     const input = screen.getByPlaceholderText("메시지 보내기");
@@ -104,7 +99,6 @@ describe("InputChat 컴포넌트", () => {
     expect(sendButton).not.toBeDisabled();
   });
 
-  // 테스트 6
   it("disabled prop이 true일 때 입력창이 비활성화되는지 확인", () => {
     renderComponent({ disabled: true });
 
@@ -112,36 +106,10 @@ describe("InputChat 컴포넌트", () => {
     expect(input).toBeDisabled();
   });
 
-  // 테스트 7
   it("disabled prop이 true일 때 전송 버튼이 비활성화되는지 확인", () => {
     renderComponent({ disabled: true });
 
     const sendButton = screen.getByLabelText("전송 버튼");
     expect(sendButton).toBeDisabled();
   });
-
-  // // 테스트 8
-  // it("disabled prop이 true일 때 이미지 첨부 input이 비활성화되는지 확인", () => {
-  //   renderComponent({ disabled: true });
-
-  //   const fileInput = screen.getByLabelText("이미지 첨부").nextElementSibling as HTMLInputElement;
-  //   expect(fileInput).toBeDisabled();
-  // });
-
-  // // 테스트 9
-  // it("이미지가 있을 때 InputChatImageSection이 렌더링되는지 확인", () => {
-  //   const mockFile = new File([""], "test.png", { type: "image/png" });
-  //   renderComponent({}, [mockFile]);
-
-  //   expect(screen.getByTestId("image-section")).toBeInTheDocument();
-  //   expect(screen.queryByPlaceholderText("메시지 보내기")).not.toBeInTheDocument();
-  // });
-
-  // // 테스트 10
-  // it("이미지가 없을 때 일반 입력창이 렌더링되는지 확인", () => {
-  //   renderComponent({}, []);
-
-  //   expect(screen.queryByTestId("image-section")).not.toBeInTheDocument();
-  //   expect(screen.getByPlaceholderText("메시지 보내기")).toBeInTheDocument();
-  // });
 });
