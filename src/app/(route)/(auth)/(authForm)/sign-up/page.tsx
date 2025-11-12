fd513ca9249542c3766002d56e08823b71312e2b
suhyeon0111 <soo4833@naver.com>
2025-11-13 01:33:10 +0900

    refactor(sign-up): signUpContainer컴포넌트에서 컴포넌트 전환 담당

--- src/app/(route)/(auth)/(authForm)/sign-up/page.tsx
+++ src/app/(route)/(auth)/(authForm)/sign-up/page.tsx
@@ -5,6 +5,8 @@ import { AllAgree, DetailAgree, SignUpField } from "./_components";
 import { useSignUpFlow } from "./_hooks/useSignUpFlow";
 import { FormProvider, useForm } from "react-hook-form";
 import { FormValue } from "../types/FormValue";
+import { useRouter } from "next/navigation";
+import SignUpContainer from "./_components/SignUpContainer/SignUpContainer";
 
 const Page = () => {
   const methods = useForm<FormValue>({
@@ -12,27 +14,18 @@ const Page = () => {
     shouldUnregister: false, // 입력 값 유지
   });
 
-  const {
-    step,
-    termDetail,
-    onSubmit,
-    onNext,
-    openTermDetail,
-    onAgreeTerm,
-    completeTerms,
-    setStep,
-  } = useSignUpFlow();
+  const router = useRouter();
+
+  const handleFinalSubmit = (data: FormValue) => {
+    console.log("최종 제출 데이터:", data);
+    alert("폼 제출되었습니다.");
+    router.push("/email-login");
+  };
 
   return (
     <div className="flex min-h-screen w-full flex-col-center">
       <FormProvider {...methods}>
-        <form onSubmit={onSubmit} className="flex w-full flex-1 flex-col justify-between">
-          {step === "form" && <SignUpField onNext={onNext} />}
-          {step === "term" && (
-            <AllAgree onOpenDetail={openTermDetail} onComplete={() => completeTerms} />
-          )}
-          {step === "termDetail" && <DetailAgree termKey={termDetail} onAgree={onAgreeTerm} />}
-        </form>
+        <SignUpContainer onFinalSubmit={handleFinalSubmit} />
       </FormProvider>
     </div>
   );
