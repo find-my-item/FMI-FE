23752cd186485e96e463e57118c36e087957e97d
suhyeon0111 <soo4833@naver.com>
2025-11-20 17:27:32 +0900

    fix(sign-up): 약관동의 페이지와 상세 약관 페이지가 겹쳐 보이는 현상 해결

--- src/app/(route)/(auth)/(authForm)/sign-up/_components/SignUpContainer/SignUpContainer.tsx
+++ src/app/(route)/(auth)/(authForm)/sign-up/_components/SignUpContainer/SignUpContainer.tsx
@@ -11,14 +11,16 @@ interface SignUpContainerProps {
 }
 
 const SignUpContainer = ({ onFinalSubmit }: SignUpContainerProps) => {
-  const { step, onSubmit, onNext, openTermDetail, onAgreeTerm, completeTerms } =
+  const { step, onSubmit, onNext, openTermDetail, onAgreeTerm, completeTerms, termName } =
     useSignUpFlow(onFinalSubmit);
 
   return (
     <form onSubmit={onSubmit} className="flex w-full flex-1 flex-col justify-between">
       {step === "1" && <SignUpField onNext={() => onNext(2)} />}
-      {step === "2" && <AllAgree onOpenDetail={openTermDetail} onComplete={completeTerms} />}
-      {step === "3" && <DetailAgree onAgree={() => onAgreeTerm(2)} />}
+      {step === "2" && !termName && (
+        <AllAgree onOpenDetail={openTermDetail} onComplete={completeTerms} />
+      )}
+      {termName && <DetailAgree termName={termName} onAgree={() => onAgreeTerm(2)} />}
     </form>
   );
 };
