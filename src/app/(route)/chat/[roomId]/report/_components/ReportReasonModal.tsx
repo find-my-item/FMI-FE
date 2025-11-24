da28d0b08b2f8e275f29eac2334fa02fab46b216
jun <znznun@gmail.com>
2025-11-24 19:38:19 +0900

    refactor(ReportReasonModal): 컴포넌트, 타입, 상수 분리

---
+++ src/app/(route)/chat/[roomId]/report/_components/ReportReasonModal.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { FormEvent, useEffect, useState } from "react";
+import { ReportReason } from "../_types/ReportReason";
+import { createPortal } from "react-dom";
+import { REPORT_REASONS } from "../_constants/REPORT_REASONS";
+import { Button } from "@/components";
+
+const ReportReasonModal = ({
+  isOpen,
+  onClose,
+  selectedReportReason,
+  setSelectedReportReason,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  selectedReportReason: ReportReason | null;
+  setSelectedReportReason: (reason: ReportReason) => void;
+}) => {
+  const [tempSelectedReportReason, setTempSelectedReportReason] = useState<ReportReason | null>(
+    selectedReportReason ?? null
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      setTempSelectedReportReason(selectedReportReason ?? null);
+    }
+  }, [selectedReportReason, isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+
+  const handleSelectReportReason = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (tempSelectedReportReason) {
+      setSelectedReportReason(tempSelectedReportReason);
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
+      onClick={onClose}
+    >
+      <form
+        className="animate-modal-slide-up absolute bottom-0 flex h-[671px] w-full max-w-[390px] flex-col rounded-t-[20px] bg-white px-[20px] py-[40px]"
+        onClick={(e) => e.stopPropagation()}
+        onSubmit={handleSelectReportReason}
+      >
+        <h1 className="mb-[32px] text-center text-h2-medium text-layout-header-default">
+          신고 사유 선택
+        </h1>
+        <fieldset className="flex flex-col gap-[2px]">
+          {REPORT_REASONS.map((reason) => (
+            <div
+              key={reason.value}
+              className="w-full space-x-[12px] py-[18px] text-h3-medium text-neutral-normal-default"
+            >
+              <input
+                type="radio"
+                name={"reportReason"}
+                id={reason.value}
+                className="cursor-pointer"
+                value={reason.value}
+                onChange={() => setTempSelectedReportReason(reason)}
+                checked={tempSelectedReportReason?.value === reason.value}
+              />
+              <label htmlFor={reason.value} className="cursor-pointer">
+                {reason.label}
+              </label>
+            </div>
+          ))}
+        </fieldset>
+        <div className="mt-auto">
+          <Button className="w-full" disabled={tempSelectedReportReason === null} type="submit">
+            선택하기
+          </Button>
+        </div>
+      </form>
+    </div>,
+    document.body
+  );
+};
+
+export default ReportReasonModal;
--- src/app/(route)/chat/[roomId]/report/page.tsx
+++ src/app/(route)/chat/[roomId]/report/page.tsx
@@ -4,104 +4,10 @@ import { Button, DetailHeader, Icon, InputField, RequiredText } from "@/componen
 import { useToast } from "@/context/ToastContext";
 import { cn } from "@/utils";
 import { useRouter } from "next/navigation";
-import { FormEvent, useEffect, useState } from "react";
-import { createPortal } from "react-dom";
+import { useState } from "react";
 import { useForm, FormProvider } from "react-hook-form";
-
-const REPORT_REASONS = [
-  { value: "1", label: "실제 분실물/습득물이 아닌 내용이에요." },
-  { value: "2", label: "동일한 내용이 여러 번 올라왔어요." },
-  { value: "3", label: "분실물과 무관한 홍보성 게시글이에요." },
-  { value: "4", label: "채팅 또는 댓글에 모욕적 표현이 있어요." },
-  { value: "5", label: "물건 반환을 빌미로 금전 요구가 있어요." },
-  { value: "6", label: "실제 주인이 아닌 사람이 소유를 주장해요." },
-  { value: "7", label: "위 항목 외의 다른 문제를 신고해요." },
-];
-type ReportReason = (typeof REPORT_REASONS)[number];
-
-const ReportReasonModal = ({
-  isOpen,
-  onClose,
-  selectedReportReason,
-  setSelectedReportReason,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-  selectedReportReason: ReportReason | null;
-  setSelectedReportReason: (reason: ReportReason) => void;
-}) => {
-  const [tempSelectedReportReason, setTempSelectedReportReason] = useState<ReportReason | null>(
-    selectedReportReason ?? null
-  );
-
-  useEffect(() => {
-    if (isOpen) {
-      setTempSelectedReportReason(selectedReportReason ?? null);
-    }
-  }, [selectedReportReason, isOpen]);
-
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-  }, [isOpen]);
-
-  if (!isOpen) return null;
-
-  const handleSelectReportReason = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (tempSelectedReportReason) {
-      setSelectedReportReason(tempSelectedReportReason);
-      onClose();
-    }
-  };
-
-  return createPortal(
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
-      onClick={onClose}
-    >
-      <form
-        className="animate-modal-slide-up absolute bottom-0 flex h-[671px] w-full max-w-[390px] flex-col rounded-t-[20px] bg-white px-[20px] py-[40px]"
-        onClick={(e) => e.stopPropagation()}
-        onSubmit={handleSelectReportReason}
-      >
-        <h1 className="mb-[32px] text-center text-h2-medium text-layout-header-default">
-          신고 사유 선택
-        </h1>
-        <fieldset className="flex flex-col gap-[2px]">
-          {REPORT_REASONS.map((reason) => (
-            <div
-              key={reason.value}
-              className="w-full space-x-[12px] py-[18px] text-h3-medium text-neutral-normal-default"
-            >
-              <input
-                type="radio"
-                name={"reportReason"}
-                id={reason.value}
-                className="cursor-pointer"
-                value={reason.value}
-                onChange={() => setTempSelectedReportReason(reason)}
-                checked={tempSelectedReportReason?.value === reason.value}
-              />
-              <label htmlFor={reason.value} className="cursor-pointer">
-                {reason.label}
-              </label>
-            </div>
-          ))}
-        </fieldset>
-        <div className="mt-auto">
-          <Button className="w-full" disabled={tempSelectedReportReason === null} type="submit">
-            선택하기
-          </Button>
-        </div>
-      </form>
-    </div>,
-    document.body
-  );
-};
+import { ReportReason } from "./_types/ReportReason";
+import ReportReasonModal from "./_components/ReportReasonModal";
 
 type ReportFormValues = {
   report: string;
