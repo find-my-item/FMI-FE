9f0cb5ab4db7453071228922ce24c067124ab864
suhyeon0111 <soo4833@naver.com>
2025-11-25 23:45:12 +0900

    feat(toast): 모든경우의 수  토스트 추가

--- src/app/(route)/(auth)/(authForm)/sign-up/_components/SignUpField/SignUpField.tsx
+++ src/app/(route)/(auth)/(authForm)/sign-up/_components/SignUpField/SignUpField.tsx
@@ -3,21 +3,55 @@
 import { SIGNUP_INPUT_CONFIG } from "../../_constant/SIGNUP_INPUT_CONFIG";
 import SignUpItem from "../SignUpItem/SignUpItem";
 import useAppMutation from "@/api/query/useAppMutation";
-import { Button, InputText, DetailHeader } from "@/components";
+import { Button, InputText, DetailHeader, Toast } from "@/components";
 import { useFormContext } from "react-hook-form";
+import { useState } from "react";
+import { useToast } from "@/context/ToastContext";
 
 const SignUpField = ({ onNext }: { onNext: () => void }) => {
   const {
     formState: { isSubmitting, isValid },
   } = useFormContext();
 
+  const { addToast } = useToast();
+
+  // const { mutate, data, error, isPending } = useAppMutation<
+  //   { email: string },
+  //   { isSuccess: boolean; code: string; message: string; result: string }
+  // >("auth", "auth/email/send-code", "post", {
+  //   onSuccess: (data) => {
+  //     console.log("data>>> ", data);
+  //     return (<Toast message="인증번호가 발송되었습니다." type="success" />);
+  //   },
+  //   onError: (error) => {
+  //     console.log("error>> ", error);
+  //   },
+  // });
+
+  const [showToast, setShowToast] = useState<{
+    show: boolean;
+    type: "success" | "error" | "warning";
+    message: string;
+  }>({ show: false, type: "success", message: "" });
+
   const HandlerToClick = (name: string) => {
     if (name === "email") {
       // TODO(수현): email 중복 확인 및 인증번호 발송 api
+      // mutate({ email: name });
+      addToast("인증번호가 발송되었습니다.", "success");
+      addToast("이미 존재하는 이메일입니다.", "warning");
+      addToast("다시 시도해 주세요.", "error");
     } else if (name === "emailAuth") {
       // TODO(수현): email 인증번호 확인 api
+      addToast("인증되었습니다.", "success");
+      addToast("인증번호가 일치하지 않습니다.", "warning");
+      addToast("다시 시도해 주세요.", "error");
     } else if (name === "nickname") {
       // TODO(수현): 닉네임 중복 확인 api
+      addToast("사용할 수 있는 닉네임입니다.", "success");
+      addToast("중복된 닉네임입니다.", "warning");
+      addToast("닉네임에 금칙어가 포함되어 있습니다.", "warning");
+      addToast("다시 시도해 주세요.", "error");
     }
   };
 
@@ -27,6 +61,7 @@ const SignUpField = ({ onNext }: { onNext: () => void }) => {
     <>
       <DetailHeader title="회원가입" />
       <div className="flex w-full flex-col gap-5 p-4">
+        {showToast.show && <Toast message={showToast.message} type={showToast.type} />}
         {SIGNUP_INPUT_CONFIG.map((item) => (
           <div key={item.name} className="h-[96px]">
             {/* TODO(수현): props 줄이기  */}
