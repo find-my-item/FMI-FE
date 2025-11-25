bd6746d0f7833cafdd1f14f96c4dd1ec90e913b4
jun <znznun@gmail.com>
2025-11-25 19:20:27 +0900

    feat(alert): alertCategory state추가

--- src/app/(route)/alert/_components/AlertCategory/AlertCategory.tsx
+++ src/app/(route)/alert/_components/AlertCategory/AlertCategory.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { Filter } from "@/components";
+import { useState } from "react";
 
 const AlertCategory = () => {
+  const [selected, setSelected] = useState("all");
   const ALERT_CATEGORIES = [
     { key: "all", label: "전체" },
     { key: "categoryKeyword", label: "카테고리 키워드" },
@@ -11,7 +15,12 @@ const AlertCategory = () => {
   return (
     <div className="mx-auto flex gap-[8px] py-[14px]">
       {ALERT_CATEGORIES.map((category) => (
-        <Filter key={category.key} ariaLabel={category.label} onSelected={false}>
+        <Filter
+          key={category.key}
+          ariaLabel={category.label}
+          onSelected={selected === category.key}
+          onClick={() => setSelected(category.key)}
+        >
           {category.label}
         </Filter>
       ))}
