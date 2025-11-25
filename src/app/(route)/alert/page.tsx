2c9e3e9fc748bf7bccbde19d72e32f545f8b1a43
jun <znznun@gmail.com>
2025-11-25 18:55:27 +0900

    style(alert): 설정 아이콘 Icon, DetailHeader추가

---
+++ src/app/(route)/alert/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { DetailHeader } from "@/components";
+
+const Alert = () => {
+  return (
+    <>
+      <DetailHeader title="알림">
+        <DetailHeader.Setting />
+      </DetailHeader>
+    </>
+  );
+};
+
+export default Alert;
