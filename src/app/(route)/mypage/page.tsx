db1c35dbf5cf1fcb0c7cf0e3c85345b92fdc6215
suhyeon0111 <soo4833@naver.com>
2025-12-02 01:23:46 +0900

    refactor(page): div 제거

--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -17,37 +17,28 @@ const page = () => {
             <p className="text-body2-regular text-layout-body-default">asdf@gmail.com</p>
           </div>
         </div>
-        {/* 버튼 */}
         <Button variant="outlined"> 프로필 수정 </Button>
       </div>
 
-      <div className="flex h-[96px] w-full items-center justify-center">
-        <div className="flex-row-center flex h-[84px] w-[350px] items-center justify-center">
-          {/* 공지사항/고객센터/채팅목록 */}
-          {TAP_CONFIG.map((item, index) => (
-            <>
-              <div
-                key={index}
-                className="flex h-[84px] w-[82px] flex-col items-center justify-center gap-2 py-4"
-              >
-                <Icon name={item.iconName} size={24} />
-                <p className="text-body2-medium text-neutral-strong-default">{item.id}</p>
-              </div>
-              {item.id !== "채팅목록" && (
-                <div className="flex w-[52px] items-center justify-center">
-                  <div className="h-[46px] border border-divider-default_3" />
-                </div>
-              )}
-            </>
-          ))}
-        </div>
+      <div className="flex h-[96px] w-full items-center justify-center gap-[26px] px-5 py-[6px]">
+        {TAP_CONFIG.map((item, index) => (
+          <>
+            <div
+              key={index}
+              className="flex h-[84px] w-[82px] flex-col items-center justify-center gap-2 py-4"
+            >
+              <Icon name={item.iconName} size={24} />
+              <p className="text-body2-medium text-neutral-strong-default">{item.id}</p>
+            </div>
+            {item.id !== "채팅목록" && <hr className="h-[46px] border border-divider-default_3" />}
+          </>
+        ))}
+        {/* </div> */}
       </div>
 
-      {/* <div className="w-full px-5 py-6"> */}
       {MENU_LIST.map((item) => (
         <MenuSection key={item} menu={item} />
       ))}
-      {/* </div> */}
     </div>
   );
 };
