fd07c0c97b666b51771e05ce178277729d288e7b
suhyeon0111 <soo4833@naver.com>
2025-12-02 01:07:46 +0900

    fix(type): TAP_CONFIG 타입 에러 수정

--- src/app/(route)/mypage/_constants/TAP_CONFIG.ts
+++ src/app/(route)/mypage/_constants/TAP_CONFIG.ts
@@ -1,4 +1,6 @@
-export const TAP_CONFIG = [
+import { IconName } from "@/components/Icon/Icon";
+
+export const TAP_CONFIG: { id: string; iconName: IconName }[] = [
   { id: "공지사항", iconName: "AnnotationAlert" },
   { id: "고객센터", iconName: "HeadPhone" },
   { id: "채팅목록", iconName: "MessageTyping" },
