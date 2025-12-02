f7cf280427dddb7d8fb4f88faa4b295a7a08529c
suhyeon0111 <soo4833@naver.com>
2025-12-02 14:14:13 +0900

    refactor(tapItem): p -> span으로 변경

--- src/app/(route)/mypage/_components/MyPageTapItem/MyPageTapItem.tsx
+++ src/app/(route)/mypage/_components/MyPageTapItem/MyPageTapItem.tsx
@@ -12,7 +12,7 @@ const MyPageTapItem = ({ tapName, iconName }: MyPageTapItemProps) => {
     <>
       <div className="w-[82px] gap-2 py-4 flex-col-center">
         <Icon name={iconName} size={24} />
-        <p className="text-body2-medium text-neutral-strong-default">{tapName}</p>
+        <span className="text-body2-medium text-neutral-strong-default">{tapName}</span>
       </div>
       {tapName !== "채팅목록" && <hr className="h-[46px] border border-divider-default_3" />}
     </>
