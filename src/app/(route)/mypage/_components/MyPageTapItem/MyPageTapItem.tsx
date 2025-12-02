b93599be87fcdec9eb5093fb3e4b0acf7385d60c
suhyeon0111 <soo4833@naver.com>
2025-12-02 14:18:25 +0900

    refactor(tapItem): width, height 고정 값 -> padding으로 관리

--- src/app/(route)/mypage/_components/MyPageTapItem/MyPageTapItem.tsx
+++ src/app/(route)/mypage/_components/MyPageTapItem/MyPageTapItem.tsx
@@ -10,9 +10,11 @@ interface MyPageTapItemProps {
 const MyPageTapItem = ({ tapName, iconName }: MyPageTapItemProps) => {
   return (
     <>
-      <div className="w-[82px] gap-2 py-4 flex-col-center">
+      <div className="gap-2 px-4 py-4 flex-col-center">
         <Icon name={iconName} size={24} />
-        <span className="text-body2-medium text-neutral-strong-default">{tapName}</span>
+        <span className="whitespace-nowrap text-body2-medium text-neutral-strong-default">
+          {tapName}
+        </span>
       </div>
       {tapName !== "채팅목록" && <hr className="h-[46px] border border-divider-default_3" />}
     </>
