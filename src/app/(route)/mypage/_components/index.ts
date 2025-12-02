98d1a5e03495ef78bbb4b7fdc9061b211af3c4ba
suhyeon0111 <soo4833@naver.com>
2025-12-02 14:53:18 +0900

    docs(index): index 추가

--- src/app/(route)/mypage/_components/index.ts
+++ src/app/(route)/mypage/_components/index.ts
@@ -1,1 +1,4 @@
-export { default as MenuSection } from "./MyPageMenuSection/MyPageMenuSection";
+export { default as MyPageMenuItem } from "./MyPageMenuItem/MyPageMenuItem";
+export { default as MyPageMenuSection } from "./MyPageMenuSection/MyPageMenuSection";
+export { default as MyPageProfile } from "./MyPageProfile/MyPageProfile";
+export { default as MyPageTapItem } from "./MyPageTapItem/MyPageTapItem";
