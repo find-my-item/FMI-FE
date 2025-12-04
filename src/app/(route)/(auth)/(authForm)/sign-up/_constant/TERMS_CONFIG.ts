f13c7dfdd8f8e85c5002bee4c8b6552f0affbc6f
suhyeon0111 <soo4833@naver.com>
2025-12-04 17:56:51 +0900

    chore(constant): 동의 3종 상수 수정

--- src/app/(route)/(auth)/(authForm)/sign-up/_constant/TERMS_CONFIG.ts
+++ src/app/(route)/(auth)/(authForm)/sign-up/_constant/TERMS_CONFIG.ts
@@ -1,17 +1,9 @@
-import { RegisterOptions } from "react-hook-form";
-
-type TermsType = {
-  name: "termsOfService" | "privacyPolicy" | "marketingConsent";
-  label: string;
-  validation: RegisterOptions;
-};
-
-export const TERMS_CONFIG: TermsType[] = [
-  { name: "termsOfService", label: "서비스 이용약관 (필수)", validation: { required: true } },
+export const TERMS_CONFIG = [
+  { name: "termsOfServiceAgreed", label: "서비스 이용약관 (필수)", validation: { required: true } },
   {
-    name: "privacyPolicy",
+    name: "privacyPolicyAgreed",
     label: "개인정보 수집 및 이용 동의 (필수)",
     validation: { required: true },
   },
   { name: "marketingConsent", label: "마케팅 수신 동의 (선택)", validation: { required: false } },
-];
+] as const;
