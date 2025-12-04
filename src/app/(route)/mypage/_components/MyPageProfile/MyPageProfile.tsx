4ef83228de61042b7d9db6859c553645439332ae
suhyeon0111 <soo4833@naver.com>
2025-12-05 01:46:07 +0900

    refactor(Profile): 버튼 포함

--- src/app/(route)/mypage/_components/MyPageProfile/MyPageProfile.tsx
+++ src/app/(route)/mypage/_components/MyPageProfile/MyPageProfile.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components";
+
 interface ProfileProps {
   userName: string;
   email: string;
@@ -5,12 +7,16 @@ interface ProfileProps {
 
 const MyPageProfile = ({ userName, email }: ProfileProps) => {
   return (
-    <div className="flex w-[188px] items-center gap-6">
-      <div className="size-[60px] rounded-full bg-slate-100" />
-      <div className="flex flex-col gap-1">
-        <span className="text-body1-semibold">{userName}</span>
-        <span className="text-body2-regular text-layout-body-default">{email}</span>
+    <div className="flex w-full items-center justify-between px-5 py-[30px]">
+      <div className="flex w-[188px] items-center gap-6">
+        {/* TODO(수현): 프로필 추후 img로 변경 */}
+        <div className="size-[60px] rounded-full bg-slate-100" />
+        <div className="flex flex-col gap-1">
+          <span className="text-body1-semibold">{userName}</span>
+          <span className="text-body2-regular text-layout-body-default">{email}</span>
+        </div>
       </div>
+      <Button variant="outlined"> 프로필 수정 </Button>
     </div>
   );
 };
--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,19 +1,15 @@
-import { Button, Icon, Tab } from "@/components";
 import MenuSection from "./_components/MyPageMenuSection/MyPageMenuSection";
 import { MypageMenuType } from "./_types/MypageMenuType";
 import { TAP_CONFIG } from "./_constants/TAP_CONFIG";
-import Profile from "./_components/MyPageProfile/MyPageProfile";
 import MyPageTapItem from "./_components/MyPageTapItem/MyPageTapItem";
+import MyPageProfile from "./_components/MyPageProfile/MyPageProfile";
 
 const MENU_LIST: MypageMenuType[] = ["내 활동", "알림", "신고/문의", "계정 설정"];
 
 const page = () => {
   return (
     <div className="flex w-full flex-col">
-      <div className="flex w-full items-center justify-between px-5 py-[30px]">
-        <Profile userName="사용자 닉네임" email="abc@gamil.com" />
-        <Button variant="outlined"> 프로필 수정 </Button>
-      </div>
+      <MyPageProfile email="abc@gmail.com" userName="사용자 닉네임" />
 
       <div className="w-full gap-[26px] px-5 py-[6px] flex-center">
         {TAP_CONFIG.map((item, index) => (
