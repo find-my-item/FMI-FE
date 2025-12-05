69dd4ab4e69b862eecdb0427fa0e934d22cb14ab
suhyeon0111 <soo4833@naver.com>
2025-12-06 01:32:53 +0900

    refactor(menuSection): 구조 변경

--- src/app/(route)/mypage/_components/MyPageMenuSection/MyPageMenuSection.tsx
+++ src/app/(route)/mypage/_components/MyPageMenuSection/MyPageMenuSection.tsx
@@ -1,87 +1,42 @@
 import { Icon } from "@/components";
 import { cn } from "@/utils";
-import { MYPAGE_MENU_LIST } from "../../_constants/MYPAGE_MENU_LIST";
 import Link from "next/link";
 
-type menuType = "내 활동" | "알림" | "신고/문의" | "계정 설정";
+interface MyPageMenuSectionProps {
+  menu: {
+    title: string;
+    pages: readonly {
+      pageName: string;
+      pageLink: string;
+    }[];
+  };
+}
 
-const MyPageMenuITem = (menu: menuType) => {
-  const MenuList = MYPAGE_MENU_LIST[menu];
+const MyPageMenuSection = ({ menu }: MyPageMenuSectionProps) => {
   return (
     <>
-      {MenuList.map((item) => (
-        <Link
-          href={`/${item.pageName}`}
-          className="flex h-11 w-full justify-between py-[10px] text-body1-semibold text-neutral-strong-default"
-        >
-          {item.pageName}
-          <Icon name="ArrowRightSmall" size={24} />
-        </Link>
-      ))}
+      <div className="w-full px-5 py-6">
+        <div className="flex h-10 items-center text-body2-regular text-layout-body-default">
+          {menu.title}
+        </div>
+        {menu.pages.map((item) => (
+          <Link
+            href={item.pageLink}
+            className="flex h-11 w-full justify-between py-[10px] text-body1-semibold text-neutral-strong-default"
+          >
+            {item.pageName}
+            <Icon name="ArrowRightSmall" size={24} />
+          </Link>
+        ))}
+      </div>
+      <hr
+        className={cn(
+          "mx-5 max-w-full border border-divider-default_3",
+          menu.title === "계정 설정" && "opacity-0"
+        )}
+      />
     </>
   );
 };
 
-const MyPageMenuSection = () => {
-  // const MenuList = MYPAGE_MENU_LIST[menu];
-
-  return (
-    <div>
-      {MYPAGE_MENU_LIST.map((item) => (
-        <>
-          <div className="w-full px-5 py-6">
-            <div className="flex h-10 items-center text-body2-regular text-layout-body-default">
-              {item}
-            </div>
-            {/* {MenuList.map((item) => (
-              <Link
-                href={`/${item.pageName}`}
-                className="flex h-11 w-full justify-between py-[10px] text-body1-semibold text-neutral-strong-default"
-              >
-                {item.pageName}
-                <Icon name="ArrowRightSmall" size={24} />
-              </Link>
-            ))} */}
-            <MyPageMenuITem menu={item} />
-          </div>
-          <hr
-            className={cn(
-              "mx-5 max-w-full border border-divider-default_3",
-              item === "계정 설정" && "opacity-0"
-            )}
-          />
-        </>
-      ))}
-    </div>
-  );
-};
-
 export default MyPageMenuSection;
-
-// {
-//   MENU_LIST.map((menu) => (
-//     <>
-//                     <div className="w-full px-5 py-6">
-//                       <div className="flex h-10 items-center text-body2-regular text-layout-body-default">
-//                         {menu}
-//                       </div>
-
-//                       {MenuList.map((item) => (
-//                         <Link
-//                           href={`/${item.pageName}`}
-//                           className="flex h-11 w-full justify-between py-[10px] text-body1-semibold text-neutral-strong-default"
-//                         >
-//                           {item.pageName}
-//                           <Icon name="ArrowRightSmall" size={24} />
-//                         </Link>
-//                       ))}
-//                     </div>
-//                     <hr
-//                       className={cn(
-//                         "mx-5 max-w-full border border-divider-default_3",
-//                         item === "계정 설정" && "opacity-0"
-//                       )}
-//                     />
-//     </>
-//   ))
-// }
--- src/app/(route)/mypage/page.tsx
+++ src/app/(route)/mypage/page.tsx
@@ -1,17 +1,18 @@
 import MyPageProfile from "./_components/MyPageProfile/MyPageProfile";
 import MyPageIconNav from "./_components/MyPageIconNav/MyPageIconNav";
 import MyPageMenuSection from "./_components/MyPageMenuSection/MyPageMenuSection";
+import { MYPAGE_MENU_LIST } from "./_constants/MYPAGE_MENU_LIST";
 
 const page = () => {
   return (
     <div className="flex w-full flex-col">
       <MyPageProfile email="abc@gmail.com" userName="사용자 닉네임" />
-      <MyPageIconNav />
-      <MyPageMenuSection />
 
-      {/* {MENU_LIST.map((item) => (
-        <MenuSection key={item} menu={item} />
-      ))} */}
+      <MyPageIconNav />
+
+      {MYPAGE_MENU_LIST.map((item) => (
+        <MyPageMenuSection menu={item} />
+      ))}
     </div>
   );
 };
