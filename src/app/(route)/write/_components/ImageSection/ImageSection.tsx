53f90827b48ce5005b1a2484ea7089b4ae8fb944
wlrnjs <wlrnjs5824@gmail.com>
2025-12-12 02:51:43 +0900

    feat(write): 이미지 추가 및 미리보기 기능 구현

--- src/app/(route)/write/_components/ImageSection/ImageSection.tsx
+++ src/app/(route)/write/_components/ImageSection/ImageSection.tsx
@@ -1,11 +1,29 @@
-import { useRef } from "react";
+"use client";
+
+import { useRef, useState } from "react";
 import { Icon } from "@/components";
+import { ImagePreviewList } from "../_internal";
 
 const ImageSection = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [images, setImages] = useState<string[]>([]);
+  const [imgTotalCount, setImgTotalCount] = useState(0);
 
-  const handleImgUpload = () => {
+  const handleSelectImage = () => {
     fileInputRef.current?.click();
+  };
+
+  const handleImageUpload = () => {
+    const files = fileInputRef.current?.files;
+    if (!files) return;
+
+    const fileArray = Array.from(files);
+    const validFiles = fileArray.filter((file) => file.type.startsWith("image/"));
+
+    const previewUrls = validFiles.map((file) => URL.createObjectURL(file));
+
+    setImages((prev) => [...prev, ...previewUrls]);
+    setImgTotalCount((prev) => prev + previewUrls.length);
   };
 
   return (
@@ -15,15 +33,26 @@ const ImageSection = () => {
         accept="image/png, image/jpeg, image/jpg"
         className="hidden"
         ref={fileInputRef}
+        onChange={handleImageUpload}
       />
-      <button
-        aria-label="이미지 업로드"
-        onClick={handleImgUpload}
-        className="size-[104px] cursor-pointer rounded-[6px] bg-flatGray-25 flex-col-center"
-      >
-        <Icon name="Camera" size={32} />
-        <span className="text-caption1-regular text-flatGray-400">(0/5)</span>
-      </button>
+
+      <div className="hide-scrollbar flex flex-nowrap items-center gap-4 overflow-x-scroll">
+        <button
+          type="button"
+          aria-label="이미지 업로드"
+          onClick={handleSelectImage}
+          className="size-[104px] shrink-0 cursor-pointer rounded-[6px] bg-flatGray-25 flex-col-center"
+        >
+          <Icon name="Camera" size={32} />
+          <span className="text-caption1-regular text-flatGray-400">({imgTotalCount}/5)</span>
+        </button>
+        <ImagePreviewList
+          images={images}
+          setImages={setImages}
+          setImgTotalCount={setImgTotalCount}
+        />
+      </div>
+
       <span className="text-caption1-regular text-neutral-normal-placeholder">
         최대 10MB, 총 5장의 이미지를 첨부할 수 있습니다. (jpg, jpeg, png)
       </span>
---
+++ src/app/(route)/write/_components/_internal/ImagePreviewList/ImagePreviewList.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import { Icon } from "@/components";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+  images: string[];
+  setImages: Dispatch<SetStateAction<string[]>>;
+  setImgTotalCount: Dispatch<SetStateAction<number>>;
+}
+
+const ImagePreviewList = ({ images, setImages, setImgTotalCount }: Props) => {
+  return (
+    <>
+      {images.map((image, index) => (
+        <div key={index} className="relative shrink-0">
+          <Image
+            src={image}
+            alt=""
+            width={104}
+            height={104}
+            className="size-[104px] rounded-[10px] object-contain"
+          />
+          <button
+            type="button"
+            aria-label="이미지 삭제"
+            onClick={() => {
+              setImages((prev) => prev.filter((_, idx) => idx !== index));
+              setImgTotalCount((prev) => prev - 1);
+            }}
+            className="absolute right-1.5 top-1.5 rounded-full border border-divider-default bg-[#5D5D5D] p-[5px]"
+          >
+            <Icon name="Delete" size={10} />
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ImagePreviewList;
