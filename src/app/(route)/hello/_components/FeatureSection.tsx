318cdc5b60faa1d6f9e74bc66bc23b7cd2c6d1c5
wlrnjs <wlrnjs5824@gmail.com>
2025-12-18 20:43:03 +0900

    refactor(service): 컴포넌트 분리

--- src/app/(route)/hello/_components/FeatureSection.tsx
+++ src/app/(route)/hello/_components/FeatureSection.tsx
@@ -1,14 +1,22 @@
+import { cn } from "@/utils";
 import Image from "next/image";
 
-interface FeatureSectionProps {
+interface FeatureSectionContent {
   imgUrl: string;
   title: string;
   description: string;
 }
 
-const FeatureSection = ({ imgUrl, title, description }: FeatureSectionProps) => {
+interface FeatureSectionProps {
+  content: FeatureSectionContent;
+  variant?: boolean;
+}
+
+const FeatureSection = ({ content, variant = false }: FeatureSectionProps) => {
+  const { imgUrl, title, description } = content;
+
   return (
-    <section className="mx-10 my-[30px] flex-col-center">
+    <section className={cn("px-10 py-[30px] flex-col-center", variant && "bg-[#EFFFF9]")}>
       <Image
         src={imgUrl}
         alt=""
