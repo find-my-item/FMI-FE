f37a0c90d5adff8b2085df765a25382a97711b34
wlrnjs <wlrnjs5824@gmail.com>
2025-12-20 00:01:23 +0900

    feat(service): 서비스 소개 페이지 hero 섹션 애니메이션 추가

--- src/app/(route)/hello/_components/ImageSlot/HeroSection.tsx
+++ src/app/(route)/hello/_components/ImageSlot/HeroSection.tsx
@@ -24,23 +24,23 @@ const HeroSectionImage = ({ src, alt = "", width, height }: HeroSectionImage) =>
 const HeroSection = () => {
   return (
     <div className="relative min-h-[225px] w-full flex-center">
-      <div className="absolute right-[250px]">
+      <div className="animate-fade-in-float absolute right-[250px]">
         <HeroSectionImage src="/hello/hero/service-hero-wallet.svg" width={120} height={57} />
       </div>
 
-      <div className="absolute bottom-0">
+      <div className="absolute inset-x-0 bottom-0 flex justify-center">
         <div className="relative flex-center">
-          <div className="absolute bottom-[50px]">
+          <div className="animate-float-hero-y absolute bottom-[50px]">
             <HeroSectionImage src="/hello/hero/service-hero-location.svg" width={94} height={131} />
           </div>
           <HeroSectionImage src="/hello/hero/service-hero-bottom.svg" width={190} height={72} />
         </div>
       </div>
 
-      <div className="absolute left-[220px] top-[10px]">
+      <div className="animate-fade-in-float absolute left-[220px] top-[20px]">
         <HeroSectionImage src="/hello/hero/service-hero-phone.svg" width={80} height={57} />
       </div>
-      <div className="absolute left-[250px] top-[130px]">
+      <div className="animate-fade-in-float absolute left-[250px] top-[130px]">
         <HeroSectionImage src="/hello/hero/service-hero-bag.svg" width={80} height={57} />
       </div>
     </div>
