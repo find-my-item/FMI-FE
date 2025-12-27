66931fe35928d9ea783c364480011288247ada1f
wlrnjs <wlrnjs5824@gmail.com>
2025-12-28 03:36:52 +0900

    docs(list): TSDoc 추가

--- src/utils/getItemCategoryLabel/getItemCategoryLabel.ts
+++ src/utils/getItemCategoryLabel/getItemCategoryLabel.ts
@@ -1,5 +1,13 @@
 import { CategoryType } from "@/types/CategoryType";
 
+/**
+ * @author jikwon
+ *
+ * 카테고리 타입을 카테고리 라벨로 변환하는 유틸리티 함수
+ *
+ * @param category - 카테고리 타입
+ * @returns 카테고리에 해당하는 라벨 문자열
+ */
 export const getItemCategoryLabel = (category: CategoryType): string => {
   const CATEGORY_LABEL: Record<CategoryType, string> = {
     ELECTRONIC: "전자기기",
