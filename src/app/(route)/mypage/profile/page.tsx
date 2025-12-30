3c0453a5ee7cc5927bd6ecf3e40350c7cfff4a9a
suhyeon0111 <soo4833@naver.com>
2025-12-30 14:16:12 +0900

    design(mypage-profile): 프로필 이미지 퍼블리싱

--- src/app/(route)/mypage/profile/page.tsx
+++ src/app/(route)/mypage/profile/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DetailHeader, InputText } from "@/components";
+import { DetailHeader, InputText, Button, Icon } from "@/components";
 import { MYPAGE_PROFILE_INPUT } from "./_constants/MYPAGE_PROFILE_INPUT";
 import { FormProvider, useForm } from "react-hook-form";
 import { Suspense } from "react";
@@ -22,24 +22,35 @@ const page = () => {
 
   return (
     <Suspense fallback="">
-      <div className="flex min-h-screen w-full flex-col">
-        <DetailHeader title="프로필 설정" />
-        <FormProvider {...methods}>
-          <form onSubmit={onSubmit}>
-            <div className="flex justify-center py-[30px]">
-              <div className="h-[80px] w-[80px] bg-slate-100" />
+      {/* <div className="flex min-h-screen w-full flex-col"> */}
+      <DetailHeader title="프로필 설정" />
+      <FormProvider {...methods}>
+        <form onSubmit={onSubmit} className="flex min-h-screen w-full flex-col">
+          <div className="flex justify-center py-[30px]">
+            <div className="relative h-[80px] w-[80px]">
+              <Icon name="UserProfile" size={80} />
+              <button className="absolute left-[52px] top-[52px] h-[28px] w-[28px] rounded-full bg-[#f5f5f5] flex-center">
+                <Icon name="CameraBorder" size={16} />
+              </button>
             </div>
+          </div>
 
-            <div className="flex w-full flex-col gap-5 p-5">
-              {MYPAGE_PROFILE_INPUT.map((item) => (
-                <InputText key={item.name} {...item}>
-                  {item.children}
-                </InputText>
-              ))}
-            </div>
-          </form>
-        </FormProvider>
-      </div>
+          <div className="flex w-full flex-col gap-5 p-5">
+            {MYPAGE_PROFILE_INPUT.map((item) => (
+              <InputText key={item.name} {...item}>
+                {item.children}
+              </InputText>
+            ))}
+          </div>
+
+          <div className="sticky bottom-0 mt-auto h-[88px] w-full max-w-[390px] border-t border-divider-default bg-white px-4 py-3">
+            <Button type="button" variant="auth" ariaLabel="회원가입 폼 버튼">
+              설정 완료
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
+      {/* </div> */}
     </Suspense>
   );
 };
