3c2464af4b1c09becd3c3d87f5d5bb044777635c
suhyeon0111 <soo4833@naver.com>
2026-01-01 00:33:15 +0900

    chore(mypage-profile): 버튼 aria-label 추가

--- src/app/(route)/mypage/profile/_components/MypageProfileForm/MypageProfileForm.tsx
+++ src/app/(route)/mypage/profile/_components/MypageProfileForm/MypageProfileForm.tsx
@@ -10,7 +10,11 @@ const MypageProfileForm = () => {
       <div className="flex justify-center py-[30px]">
         <div className="relative h-[80px] w-[80px]">
           <Icon name="UserProfile" size={80} />
-          <button className="absolute left-[52px] top-[52px] h-[28px] w-[28px] rounded-full bg-[#f5f5f5] flex-center">
+          {/* TODO(수현): 디자인 토큰 변경 요청 해놓은 상태로 등록 시 추후 변경 */}
+          <button
+            className="absolute left-[52px] top-[52px] h-[28px] w-[28px] rounded-full bg-[#f5f5f5] flex-center"
+            aria-label="프로필 이미지 변경 버튼"
+          >
             <Icon name="CameraBorder" size={16} />
           </button>
         </div>
