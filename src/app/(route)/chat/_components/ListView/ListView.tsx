0dfeb990a2fae6c31b554437aa6f79d6824cd931
jun <znznun@gmail.com>
2025-12-31 21:30:22 +0900

    feat(chat): API 수정

--- src/app/(route)/chat/_components/ListView/ListView.tsx
+++ src/app/(route)/chat/_components/ListView/ListView.tsx
@@ -4,33 +4,55 @@ import { DetailHeader, ListSearch } from "@/components";
 import { useSearchUpdateQueryString } from "@/hooks";
 import DefaultList from "../DefaultList/DefaultList";
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-const ACCESS_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ6bnpudW5AZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJ1c2VySWQiOjMsImlzcyI6ImZtaSIsImlhdCI6MTc2NjA2NzQ1OCwiZXhwIjoxNzY2MDY4MzU4fQ.OM1s8f--NBoQRf89wc6PJrbkxbD3oQ0jMOBj0TuuRYw";
-
-const fetchChatList = async () => {
-  const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}:8080/users/me/chats?size=10&sort=LATEST`,
+const postLogin = async () => {
+  const response = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}:8080/auth/login`,
     {
-      headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-      },
+      email: "test@example.com",
+      password: "Test1234!",
+    },
+    {
+      withCredentials: true,
     }
   );
 
   return response;
 };
-const useFetchChatList = () => {
+const usePostLogin = () => {
+  return useMutation({
+    mutationFn: postLogin,
+  });
+};
+
+const fetchChatList = async () => {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}:8080/users/me/chats?size=10&sort=LATEST`,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response;
+};
+const useFetchChatList = (enabled: boolean) => {
   return useQuery({
     queryKey: ["chatList"],
     queryFn: fetchChatList,
+    enabled,
   });
 };
 
 const ListView = () => {
+  const { mutate: postLogin, isSuccess: isLoginSuccess } = usePostLogin();
   const { searchMode, searchUpdateQuery } = useSearchUpdateQueryString();
-  const { data } = useFetchChatList();
+  const { data } = useFetchChatList(isLoginSuccess);
+
+  useEffect(() => {
+    postLogin();
+  }, []);
 
   return (
     <div className="w-full">
