97c0d2d8c9d2e8d868543a95df0ba48eeb91919b
wlrnjs <wlrnjs5824@gmail.com>
2026-01-02 07:50:09 +0900

    feat(list-detail): 상세페이지 공유하기 UI 추가

---
+++ src/app/(route)/list/[id]/_components/PostShare/PostShare.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { Button, PopupLayout } from "@/components";
+import { SHARE } from "./SHARE";
+
+interface PostShareProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const PostShare = ({ isOpen, onClose }: PostShareProps) => {
+  return (
+    <PopupLayout isOpen={isOpen} onClose={onClose} className="min-h-[305px] space-y-12 px-5 py-10">
+      <section className="gap-5 flex-col-center">
+        <h2 className="text-h3-semibold text-layout-header-default">게시글 공유하기</h2>
+        <div className="w-full gap-[18px] flex-center">
+          {SHARE.map((item) => (
+            <ShareOptionButton
+              key={item.name}
+              src={item.src}
+              name={item.name}
+              onClick={item.onClick}
+            />
+          ))}
+        </div>
+      </section>
+
+      <Button aria-label="닫기" onClick={onClose} variant="outlined" className="min-h-11 w-full">
+        닫기
+      </Button>
+    </PopupLayout>
+  );
+};
+
+export default PostShare;
+
+const ShareOptionButton = ({
+  src,
+  name,
+  onClick,
+}: {
+  src: string;
+  name: string;
+  onClick: () => void;
+}) => {
+  return (
+    <button aria-label={name} type="button" className="gap-2 flex-col-center" onClick={onClick}>
+      <Image src={src} alt="" width={60} height={60} className="rounded-full" />
+      <span className="text-body2-semibold text-neutral-normal-default">{name}</span>
+    </button>
+  );
+};
---
+++ src/app/(route)/list/[id]/_components/PostShare/SHARE.ts
@@ -0,0 +1,5 @@
+export const SHARE = [
+  { src: "/share/share-kakao.svg", name: "카카오톡", onClick: () => {} },
+  { src: "/share/share-native.svg", name: "공유하기", onClick: () => {} },
+  { src: "/share/share-copy.svg", name: "링크 복사", onClick: () => {} },
+];
