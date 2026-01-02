01dccdd4696ac1a0185fc8cac97dce4a9a974a1e
jun <znznun@gmail.com>
2026-01-03 06:01:50 +0900

    style(chat): chat layout h-base 적용

--- src/app/(route)/chat/layout.tsx
+++ src/app/(route)/chat/layout.tsx
@@ -1,5 +1,5 @@
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="flex h-[calc(100dvh-4px)] w-full flex-col">{children}</main>;
+  return <main className="flex w-full flex-col h-base">{children}</main>;
 };
 
 export default layout;
