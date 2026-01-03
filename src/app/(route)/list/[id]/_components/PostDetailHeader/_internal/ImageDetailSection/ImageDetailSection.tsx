7ca88fffba35e4ba110283600b53c5e3d5f4eb68
wlrnjs <wlrnjs5824@gmail.com>
2026-01-04 05:06:58 +0900

    feat(list-detail): 이미지 상세페이지 추가

--- src/app/(route)/list/[id]/_components/PostDetailHeader/_internal/ImageDetailSection/ImageDetailSection.tsx
+++
@@ -1,9 +0,0 @@
-const ImageDetailSection = () => {
-  return (
-    <div>
-      <p>이미지 상세 정보</p>
-    </div>
-  );
-};
-
-export default ImageDetailSection;
--- src/app/(route)/list/[id]/_components/PostDetailHeader/_internal/ImageSection/ImageSection.tsx
+++ src/app/(route)/list/[id]/_components/PostDetailHeader/_internal/ImageSection/ImageSection.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { useRef, useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import { ImageViewerModal } from "@/components";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -13,22 +15,57 @@ interface ImageSectionProps {
 }
 
 const ImageSection = ({ imageUrls }: ImageSectionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const isDraggingRef = useRef(false);
+
+  const handleOpenIfTap = () => {
+    if (isDraggingRef.current) return;
+    setIsOpen(true);
+  };
+
   return (
-    <Swiper modules={[Pagination]} pagination={{ clickable: true }}>
-      {imageUrls.map((url, index) => (
-        <SwiperSlide key={index}>
-          <Image
-            src={url}
-            alt={`게시글 이미지 ${index + 1}`}
-            width={390}
-            height={260}
-            draggable={false}
-            priority
-            className="h-[260px] w-[390px] object-cover"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <div className="relative cursor-pointer select-none">
+        <Swiper
+          modules={[Pagination]}
+          pagination={{ clickable: true }}
+          onTouchMove={() => {
+            isDraggingRef.current = true;
+          }}
+          onTouchEnd={() => {
+            window.setTimeout(() => {
+              isDraggingRef.current = false;
+            }, 0);
+          }}
+          onSliderMove={() => {
+            isDraggingRef.current = true;
+          }}
+          onClick={handleOpenIfTap}
+        >
+          {imageUrls.map((url, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                src={url}
+                alt={`게시글 이미지 ${index + 1}`}
+                width={390}
+                height={260}
+                draggable={false}
+                priority
+                className="h-[260px] w-[390px] object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      <ImageViewerModal
+        images={imageUrls}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        initialIndex={0}
+      />
+    </>
   );
 };
 
