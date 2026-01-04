597613b285e6d0dfbe7a5b5bdae919aa5c336763
suhyeon0111 <soo4833@naver.com>
2026-01-04 18:15:33 +0900

    refactor(footerButton): 공용 컴포넌트에 추가

--- src/components/domain/AuthFooterButton/AuthFooterButton.tsx
+++ src/components/domain/AuthFooterButton/AuthFooterButton.tsx
@@ -1,14 +1,15 @@
 import Button from "@/components/common/Buttons/Button/Button";
+import { ButtonHTMLAttributes } from "react";
 
-interface AuthFooterButton {
+interface AuthFooterButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   name: string;
   ariaLabel: string;
 }
 
-const AuthFooterButton = ({ name, ariaLabel }: AuthFooterButton) => {
+const AuthFooterButton = ({ name, ariaLabel, ...props }: AuthFooterButton) => {
   return (
     <div className="sticky bottom-0 mt-auto h-[88px] w-full border-t border-divider-default bg-white px-4 py-3">
-      <Button type="submit" variant="auth" ariaLabel={ariaLabel}>
+      <Button type="submit" variant="auth" ariaLabel={ariaLabel} {...props}>
         {name}
       </Button>
     </div>
