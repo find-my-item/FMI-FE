7d9151a097df35b5eb7f1b21f8108b7904d6a86b
wlrnjs <wlrnjs5824@gmail.com>
2026-01-04 22:18:43 +0900

    refactor(user): 타인 페이지 콘텐츠 영역 리팩토링

--- src/app/(route)/user/[id]/_components/TabContents/TabContents.tsx
+++ src/app/(route)/user/[id]/_components/TabContents/TabContents.tsx
@@ -5,45 +5,51 @@ import ListItem from "@/app/(route)/list/_components/ListItem/ListItem";
 import { USER_TABS } from "../../_types/USER_TABS";
 import { CommentItem } from "../_internal";
 
-const TabContents = ({ selectedTab }: { selectedTab: (typeof USER_TABS)[number]["key"] }) => {
+interface TabContentsProps {
+  selectedTab: (typeof USER_TABS)[number]["key"];
+}
+
+const TabContents = ({ selectedTab }: TabContentsProps) => {
   return (
-    <>
-      <section>
-        <ul>
-          {selectedTab === "post" && (
-            <li>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <ListItem post={MOCK_POST_ITEM} linkState="list" key={index} />
-              ))}
-            </li>
-          )}
+    <section aria-label="탭 콘텐츠">
+      <ul>
+        {selectedTab === "post" && (
+          <>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <li key={index}>
+                <ListItem post={MOCK_POST_ITEM} linkState="list" />
+              </li>
+            ))}
+          </>
+        )}
 
-          {selectedTab === "comment" && (
-            <>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CommentItem
-                  data={{
-                    postId: 1,
-                    likes: 5,
-                    comment: "여기에 댓글 내용이 표기됩니다",
-                    date: "2025.11.02",
-                  }}
-                  key={index}
-                />
-              ))}
-            </>
-          )}
+        {selectedTab === "comment" && (
+          <>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CommentItem
+                data={{
+                  postId: 1,
+                  likes: 5,
+                  comment: "여기에 댓글 내용이 표기됩니다",
+                  date: "2025.11.02",
+                }}
+                key={index}
+              />
+            ))}
+          </>
+        )}
 
-          {selectedTab === "favorite" && (
-            <>
-              {Array.from({ length: 5 }).map((_, index) => (
-                <ListItem post={MOCK_POST_ITEM} linkState="list" key={index} />
-              ))}
-            </>
-          )}
-        </ul>
-      </section>
-    </>
+        {selectedTab === "favorite" && (
+          <>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <li key={index}>
+                <ListItem post={MOCK_POST_ITEM} linkState="list" />
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
+    </section>
   );
 };
 
