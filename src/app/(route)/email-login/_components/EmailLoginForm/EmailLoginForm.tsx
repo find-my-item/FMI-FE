db60a65eeea794c26c1d8127ad9205d12d8dc016
suhyeon0111 <soo4833@naver.com>
2026-01-04 21:26:38 +0900

    refactor(email-login): 시멘틱 태그로 변경

--- src/app/(route)/email-login/_components/EmailLoginForm/EmailLoginForm.tsx
+++ src/app/(route)/email-login/_components/EmailLoginForm/EmailLoginForm.tsx
@@ -15,7 +15,7 @@ const Page = () => {
   return (
     <form onSubmit={onSubmitLogin} className="flex w-full flex-col gap-12">
       {/* 로그인 입력칸 */}
-      <div className="flex w-full flex-col gap-3">
+      <fieldset className="flex w-full flex-col gap-3">
         {EMAIL_LOGIN_CONFIG.map((item) => (
           <InputText
             key={item.name}
@@ -28,32 +28,30 @@ const Page = () => {
           />
         ))}
         {/* 체크박스 */}
-        <div className="flex w-full">
-          <CheckBox
-            {...register("rememberId", { required: false })}
-            id="rememberId"
-            label="아이디 기억하기"
-            boxSize="w-[18px] h-[18px]"
-            textStyle="text-caption1-semibold text-neutral-normal-default ml-2"
-            iconSize="h-[6px]"
-            state={!!checkBoxValues}
-          />
-        </div>
-      </div>
+        <CheckBox
+          {...register("rememberId", { required: false })}
+          id="rememberId"
+          label="아이디 기억하기"
+          boxSize="w-[18px] h-[18px]"
+          textStyle="text-caption1-semibold text-neutral-normal-default ml-2"
+          iconSize="h-[6px]"
+          state={!!checkBoxValues}
+        />
+      </fieldset>
 
       {/* 로그인 버튼 */}
       <div className="w-full gap-6 flex-col-center">
-        <Button type="submit" ariaLabel="로그인 버튼" variant="auth">
+        <Button type="submit" variant="auth">
           로그인
         </Button>
         {/* divider 구분선 */}
-        <div className="flex h-4 w-full items-center">
-          <hr className="h-px flex-1 bg-flatGray-100" aria-hidden />
-          <span className="px-3 text-caption1-medium text-layout-body-default" aria-hidden>
+        <nav className="flex h-4 w-full items-center">
+          <hr className="h-px flex-1 border border-divider-default" aria-hidden={true} />
+          <span className="px-3 text-caption1-medium text-layout-body-default">
             로그인이 되지 않는다면?
           </span>
-          <hr className="h-px flex-1 bg-flatGray-100" />
-        </div>
+          <hr className="h-px flex-1 border border-divider-default" aria-hidden={true} />
+        </nav>
       </div>
     </form>
   );
