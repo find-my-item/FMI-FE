66930f95dc7da616f3394056af2d218147f0ac01
jun <znznun@gmail.com>
2026-01-05 16:56:18 +0900

    refactor(chat): 중첩 sr-only 삭제

--- src/app/(route)/chat/[roomId]/_components/EmptyChatRoom/EmptyChatRoom.tsx
+++ src/app/(route)/chat/[roomId]/_components/EmptyChatRoom/EmptyChatRoom.tsx
@@ -4,7 +4,6 @@ import { EMPTY_MODE_STYLE } from "./EMPTY_MODE_STYLE";
 const EmptyChatRoom = ({ postMode }: { postMode: "find" | "lost" }) => {
   return (
     <section className="flex-1 bg-flatGray-25 flex-center">
-      <h1 className="sr-only">빈 채팅 안내 화면</h1>
       <div className="gap-2 flex-col-center">
         <Icon name={EMPTY_MODE_STYLE[postMode].iconName} size={80} />
         <div className="select-none text-center text-body2-medium text-layout-body-default">
