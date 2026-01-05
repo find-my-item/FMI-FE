d88d9c321bbe8073299df1a6a277ef2053880614
jun <znznun@gmail.com>
2026-01-05 20:21:30 +0900

    test(ChatItem): 스토리북 mock 파일 분리

--- src/app/(route)/chat/_components/ChatItem/ChatItem.stories.tsx
+++ src/app/(route)/chat/_components/ChatItem/ChatItem.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from "@storybook/nextjs";
 import ChatItem from "./ChatItem";
+import { MOCK_CHAT_ITEM } from "@/mock/MOCK_CHAT_ITEM";
+import { ChatRoom } from "@/api/fetch/chat/types/ChatListType";
 
 const meta: Meta<typeof ChatItem> = {
   title: "페이지/채팅 페이지/ChatItem",
@@ -28,29 +30,6 @@ type Story = StoryObj<typeof ChatItem>;
 
 export const Default: Story = {
   render: () => {
-    return (
-      <ChatItem
-        chatRoom={{
-          roomId: 1,
-          contactUser: {
-            userId: 1,
-            nickname: "사용자 닉네임",
-            profileImageUrl: "profile.jpg",
-          },
-          postInfo: {
-            postId: 1,
-            postType: "LOST",
-            title: "테스트 게시글",
-            address: "서울시 강남구 신사동",
-            thumbnailUrl: "test-thumbnail.jpg",
-          },
-          messageType: "TEXT",
-          lastMessage:
-            "안녕하세요! 혹시 올리신 검정색 카드 지갑, 명동에서 습득하신 지갑이실까요? 혹시나 해서",
-          lastMessageSentAt: "2026-01-01T10:00:00.000Z",
-          unreadCount: 1,
-        }}
-      />
-    );
+    return <ChatItem chatRoom={MOCK_CHAT_ITEM} />;
   },
 };
