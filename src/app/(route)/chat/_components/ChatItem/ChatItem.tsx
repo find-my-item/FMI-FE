b3140d54c2a9ed4ee8570e5f9ccb0fa1e812d78f
jun <znznun@gmail.com>
2026-01-05 19:52:41 +0900

    fix(chatItem): 태그 수정, 임시 대체 이미지

--- src/app/(route)/chat/_components/ChatItem/ChatItem.tsx
+++ src/app/(route)/chat/_components/ChatItem/ChatItem.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { ChatRoom } from "@/api/fetch/auth/types/ChatListType";
 import { Icon } from "@/components";
 import { formatDate } from "@/utils";
+import { MOCK_IMAGES } from "./MOCK_IMAGES";
 
 interface ChatItemProps {
   chatRoom: ChatRoom;
@@ -13,17 +14,18 @@ const ChatItem = ({ chatRoom }: ChatItemProps) => {
 
   return (
     <Link
-      href={`chat/${roomId}`}
-      className="flex min-h-[113px] w-full items-center gap-[12px] border-b border-divider-default px-[16px] py-[24px] duration-150 hover:bg-flatGray-25"
+      href={`/chat/${roomId}`}
+      aria-label={`${roomId} 채팅방 링크`}
+      className="flex min-h-[113px] w-full items-center gap-[12px] border-b border-divider-default px-4 py-6 transition-colors hover:bg-flatGray-25"
     >
-      <section className="relative h-[58px] w-[58px] shrink-0">
+      <div className="relative h-[58px] w-[58px] shrink-0">
         {contactUser?.profileImageUrl ? (
           <Image
             alt="유저 프로필 이미지"
             src={contactUser?.profileImageUrl}
             width={26}
             height={26}
-            className="absolute left-0 top-0 z-10 rounded-full border-[1.5px] border-white"
+            className="absolute left-0 top-0 z-10 rounded-full border-[1.5px] border-white object-cover"
           />
         ) : (
           <Icon
@@ -32,17 +34,17 @@ const ChatItem = ({ chatRoom }: ChatItemProps) => {
             className="absolute left-0 top-0 z-10 rounded-full border-[1.5px] border-white"
           />
         )}
-
+        {/* TODO(형준): 대체 이미지 수정 예정 */}
         <Image
           alt="게시글 썸네일 이미지"
-          src={postInfo?.thumbnailUrl || ""}
+          src={postInfo?.thumbnailUrl || MOCK_IMAGES[0]}
           width={50}
           height={50}
           className="absolute bottom-0 right-0 h-[50px] w-[50px] rounded object-cover"
         />
-      </section>
+      </div>
 
-      <section className="min-w-0 space-y-[2px]">
+      <div className="min-w-0 space-y-[2px]">
         <div className="flex items-center justify-between">
           <span className="text-h3-semibold text-layout-header-default">
             {contactUser?.nickname || "닉네임을 불러오지 못했습니다."}
@@ -60,7 +62,7 @@ const ChatItem = ({ chatRoom }: ChatItemProps) => {
         <p className="truncate text-body2-medium text-layout-header-default">
           {lastMessage || "메시지를 불러오지 못했습니다."}
         </p>
-      </section>
+      </div>
     </Link>
   );
 };
