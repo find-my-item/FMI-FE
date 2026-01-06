1b9ef74e62b0cfc22229f09b4c1584554fd730ab
wlrnjs <wlrnjs5824@gmail.com>
2026-01-06 17:49:13 +0900

    fix(list): useMemo 제거

--- src/app/(route)/list/_hooks/useListParams/useListParams.ts
+++ src/app/(route)/list/_hooks/useListParams/useListParams.ts
@@ -1,6 +1,5 @@
 "use client";
 
-import { useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import {
   CategoryFilterValue,
@@ -11,14 +10,11 @@ import {
 export const useListParams = () => {
   const searchParams = useSearchParams();
 
-  return useMemo(
-    () => ({
-      type: searchParams.get("type"),
-      status: searchParams.get("status") as StatusFilterValue,
-      category: searchParams.get("category") as CategoryFilterValue,
-      sort: searchParams.get("sort") as SortFilterValue,
-      region: searchParams.get("region"),
-    }),
-    [searchParams]
-  );
+  return () => ({
+    type: searchParams.get("type"),
+    status: searchParams.get("status") as StatusFilterValue,
+    category: searchParams.get("category") as CategoryFilterValue,
+    sort: searchParams.get("sort") as SortFilterValue,
+    region: searchParams.get("region"),
+  });
 };
