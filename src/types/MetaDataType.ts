486b5ed414fbf6b4268d9cc4e1be3f43c3c0e384
wlrnjs <wlrnjs5824@gmail.com>
2026-01-08 00:01:06 +0900

    docs(types): type 폴더 내 파일 TSDoc 개선

--- src/types/MetaDataType.ts
+++ src/types/MetaDataType.ts
@@ -1,3 +1,16 @@
+/**
+ * @author jikwon
+ *
+ * 메타데이터 타입 정의
+ *
+ * - 이 타입은 메타데이터를 표시합니다.
+ *
+ * @description MetaDataType
+ * - title: 메타데이터 제목
+ * - summary: 메타데이터 요약
+ * - thumbnailUrl: 메타데이터 썸네일 URL
+ * - link: 메타데이터 링크
+ */
 export type MetaDataType = {
   title: string;
   summary: string;
