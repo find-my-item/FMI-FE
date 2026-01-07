24f926ee467f24e9bceaf2aa754c77c9e91ab0ad
wlrnjs <wlrnjs5824@gmail.com>
2026-01-07 21:15:12 +0900

    feat(write): 지도 드래그 기능 추가

--- src/app/(route)/write/location/_components/_internal/KakaoMap/KakaoMap.tsx
+++ src/app/(route)/write/location/_components/_internal/KakaoMap/KakaoMap.tsx
@@ -2,6 +2,7 @@
 
 import { Radius } from "@/types";
 import { getMapLevelByRadius } from "@/utils";
+import { useState } from "react";
 import { Circle, Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
 
 interface WriteKakaoMap {
@@ -16,25 +17,43 @@ const WriteKakaoMap = ({ lat, lng, radius }: WriteKakaoMap) => {
     libraries: ["services"],
   });
 
+  const [center, setCenter] = useState<{ lat: number; lng: number }>({
+    lat,
+    lng,
+  });
+
   if (loading) return null;
   if (error) return <div>로드 실패</div>;
+
+  const handleMapDragEnd = (map: kakao.maps.Map) => {
+    const latlng = map.getCenter();
+
+    setCenter({
+      lat: latlng.getLat(),
+      lng: latlng.getLng(),
+    });
+  };
 
   const level = getMapLevelByRadius(radius);
 
   return (
     <>
-      <Map center={{ lat, lng }} level={level} style={{ width: "100%", height: "100%" }}>
+      <Map
+        center={center}
+        level={level}
+        style={{ width: "100%", height: "100%" }}
+        onDragEnd={handleMapDragEnd}
+      >
         <MapMarker
-          position={{ lat, lng }}
+          position={center}
           image={{
             src: "/kakao-map/marker.svg",
             size: { width: 26, height: 37 },
             options: { offset: { x: 13, y: 20 } },
           }}
         />
-
         <Circle
-          center={{ lat, lng }}
+          center={center}
           radius={radius}
           strokeColor="#1EB87B"
           strokeWeight={1}
