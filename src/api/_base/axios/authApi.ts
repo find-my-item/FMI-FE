da2b3ba72f0f47eeee533a4f010d42898d2abfed
suhyeon0111 <soo4833@naver.com>
2026-01-14 02:20:34 +0900

    feat(middleware): 401에러, 403에러 상황일때 토큰 재발급 api 작동

--- src/api/_base/axios/authApi.ts
+++ src/api/_base/axios/authApi.ts
@@ -13,20 +13,25 @@ authApi.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
+    // 서버환경에서 실행했을때 에러라는 것을 알려줌
     if (typeof window === "undefined") {
       return Promise.reject(error);
     }
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (
+      (error.response?.status === 401 || error.response?.status === 403) &&
+      !originalRequest._retry
+    ) {
       originalRequest._retry = true;
 
       try {
         await authApi.post("/auth/refresh");
-        console.log("재발급 중>> ");
+
         return authApi(originalRequest);
       } catch (refreshError) {
+        alert("다시 로그인 해주세요.");
         window.location.href = "/login";
-        console.log("토큰 재발급 실패>> ");
+
         return Promise.reject(refreshError);
       }
     }
