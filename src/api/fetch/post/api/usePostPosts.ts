ee2197a962d640aca7928441df8a26cd5fe3c584
wlrnjs <wlrnjs5824@gmail.com>
2026-01-15 00:42:36 +0900

    feat(write): 게시글 작성 후 상세페이지 이동

--- src/api/fetch/post/api/usePostPosts.ts
+++ src/api/fetch/post/api/usePostPosts.ts
@@ -1,18 +1,24 @@
 import useAppMutation from "@/api/_base/query/useAppMutation";
 import { PostPostsWriteRequestBody, PostPostsWriteResponse } from "../types/PostWriteType";
+import { useToast } from "@/context/ToastContext";
+import { useRouter } from "next/navigation";
 
 export const usePostPosts = () => {
+  const { addToast } = useToast();
+  const router = useRouter();
+
   return useAppMutation<PostPostsWriteRequestBody, PostPostsWriteResponse>(
     "auth",
     "/posts",
-    "post"
+    "post",
+    {
+      onSuccess: (data) => {
+        addToast("게시글이 등록되었습니다.", "success");
+        router.replace(`/list/${data.result.postId}`);
+      },
+      onError: () => {
+        addToast("게시글 등록에 실패했습니다.", "error");
+      },
+    }
   );
 };
-
-// useAppMutation<PostPostsWriteRequestBody, PostPostsWriteResponse>("auth", "/posts", "post"),
-// {
-//   onSuccess: () => {
-//     addToast("게시글이 등록되었습니다.", "success");
-//     router.push("/write/post");
-//   },
-// }
