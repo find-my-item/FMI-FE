26b52a14d2e65dbef8c9db732db03e93dd4dcbd9
wlrnjs <wlrnjs5824@gmail.com>
2026-01-15 21:22:41 +0900

    refactor(write): 불필요한 client 제거

--- src/app/(route)/write/post/layout.tsx
+++ src/app/(route)/write/post/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { ReactNode } from "react";
 import { PostWriteFormProvider } from "./location/_components";
 
--- src/app/(route)/write/post/location/_components/LocationRangeSection/LocationRangeSection.tsx
+++ src/app/(route)/write/post/location/_components/LocationRangeSection/LocationRangeSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 import { Radius } from "@/types";
 import { BottomSheet, KakaoMap } from "../_internal";
