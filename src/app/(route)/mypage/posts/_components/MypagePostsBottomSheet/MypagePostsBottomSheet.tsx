a9731e04496b65f606de41e5423a2e154107e6e4
suhyeon0111 <soo4833@naver.com>
2026-01-16 01:39:03 +0900

    refactor(mypage-posts): 날짜 계산 로직 useMakeDate로 분리

--- src/app/(route)/mypage/posts/_components/MypagePostsBottomSheet/MypagePostsBottomSheet.tsx
+++ src/app/(route)/mypage/posts/_components/MypagePostsBottomSheet/MypagePostsBottomSheet.tsx
@@ -10,6 +10,7 @@ import { Button, Filter } from "@/components/common";
 import { PopupLayout } from "@/components/domain";
 import { MYPAGE_POSTS_SHEET_FILTER } from "../../_constants/MYPAGE_POSTS_SHEET_FILTER";
 import { FilterModeType } from "../../_types/FilterModeType";
+import useMakeDate from "../../_hooks/useMakeDate";
 
 const DateWheel = ({
   dateArray,
@@ -77,40 +78,7 @@ interface MypagePostsBottomSheetProps {
 }
 
 const MypagePostsBottomSheet = ({ isOpen, onClose, mode }: MypagePostsBottomSheetProps) => {
-  const today = new Date();
-
-  const [selectDate, setSelectDate] = useState({
-    year: today.getFullYear(),
-    month: today.getMonth() + 1,
-    day: today.getDate(),
-  });
-
-  const years = useMemo(() => {
-    const startYear = 2025;
-    return Array.from({ length: today.getFullYear() - startYear + 1 }, (_, i) => startYear + i);
-  }, []);
-
-  const months = useMemo(() => Array.from({ length: 12 }, (_, i) => i + 1), []);
-
-  const days = useMemo(() => {
-    const date = new Date(selectDate.year, selectDate.month - 1);
-    const daysCount = getDaysInMonth(date);
-    return Array.from({ length: daysCount }, (_, i) => i + 1);
-  }, [selectDate.year, selectDate.month]);
-
-  const handleDateChange = (type: "year" | "month" | "day", value: number) => {
-    setSelectDate((prev) => {
-      const newDateArray = { ...prev, [type]: value };
-
-      if (type !== "day") {
-        const maxDay = getDaysInMonth(new Date(newDateArray.year, newDateArray.month - 1));
-        if (newDateArray.day > maxDay) {
-          newDateArray.day = maxDay;
-        }
-      }
-      return newDateArray;
-    });
-  };
+  const { years, months, days, selectDate, handleDateChange } = useMakeDate();
 
   return (
     <PopupLayout
