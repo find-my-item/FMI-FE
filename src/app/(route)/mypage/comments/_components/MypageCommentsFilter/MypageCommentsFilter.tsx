4cf9053795c69abafeef2791c93a0193b4973ab2
suhyeon0111 <soo4833@naver.com>
2026-01-18 02:20:17 +0900

    feat(mypage-comments): 최신순, 오래된 순 클릭 시 케밥 메뉴 보이기

--- src/app/(route)/mypage/comments/_components/MypageCommentsFilter/MypageCommentsFilter.tsx
+++ src/app/(route)/mypage/comments/_components/MypageCommentsFilter/MypageCommentsFilter.tsx
@@ -1,46 +1,45 @@
 import { Filter, KebabMenu } from "@/components/common";
 import MypageCommentsBottomSheet from "../MypageCommentsBottomSheet/MypageCommentsBottomSheet";
-import { MYPAGE_COMMENTS_FILTER } from "../../_constants/MYPAGE_COMMENTS_FILTER";
 import { useState } from "react";
 
 const MypageCommentsFilter = () => {
   const [isBottomSheet, setIsBottomSheet] = useState(false);
-  const [isKebabMenu, setIsKebabMenu] = useState(false);
+  const [isKebabMenu, setIsKebabMenu] = useState<{ menu: string; open: boolean }>({
+    menu: "최신순",
+    open: false,
+  });
 
   const MYPAGE_COMMENTS_KEBAB_ITEM = [
-    { text: "최신순", onClick: () => setIsKebabMenu(false) },
-    { text: "오래된 순", onClick: () => setIsKebabMenu(false) },
+    { text: "최신순", onClick: () => setIsKebabMenu({ menu: "최신순", open: false }) },
+    { text: "오래된 순", onClick: () => setIsKebabMenu({ menu: "오래된 순", open: false }) },
   ];
 
-  // TODO(수현): name 타입 안정성 높이기
-  const handleFilterClick = (name: string) => {
-    if (name === "최신순") {
-      setIsKebabMenu(true);
-    } else {
-      setIsBottomSheet(false);
-    }
-  };
-
   return (
-    <section className="flex w-full gap-2 overflow-x-auto px-5 py-[14px]">
+    <section className="flex w-full gap-2 px-5 py-[14px]">
       <h2 className="sr-only">필터링 영역</h2>
 
-      <div className="relative">
-        {MYPAGE_COMMENTS_FILTER.map((item) => (
-          <Filter
-            key={item.name}
-            ariaLabel={item.name}
-            icon={item.icon}
-            onSelected={false}
-            onClick={() => handleFilterClick(item.name)}
-            iconPosition={item.iconPosition}
-          >
-            {item.name}
-          </Filter>
-        ))}
+      <Filter
+        ariaLabel="기간"
+        icon={{ name: "Calendar", size: 16 }}
+        onSelected={false}
+        onClick={() => setIsBottomSheet(true)}
+        iconPosition="leading"
+      >
+        기간
+      </Filter>
 
-        {isKebabMenu && (
-          <div className="absolute top-full">
+      <div className="relative">
+        <Filter
+          ariaLabel={isKebabMenu.menu}
+          icon={{ name: "ArrowDown", size: 16 }}
+          onSelected={false}
+          onClick={() => setIsKebabMenu((prev) => ({ ...prev, open: true }))}
+          iconPosition="trailing"
+        >
+          {isKebabMenu.menu}
+        </Filter>
+        {isKebabMenu.open && (
+          <div className="absolute left-1/2 top-full z-50 mt-2 -translate-x-1/2">
             <KebabMenu items={MYPAGE_COMMENTS_KEBAB_ITEM} />
           </div>
         )}
