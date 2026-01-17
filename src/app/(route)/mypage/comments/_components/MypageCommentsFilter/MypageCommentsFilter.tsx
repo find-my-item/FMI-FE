3e4a2598cdca732abaff91b001e531206e9b2152
suhyeon0111 <soo4833@naver.com>
2026-01-17 17:04:32 +0900

    chore(commit): 중간저장

--- src/app/(route)/mypage/comments/_components/MypageCommentsFilter/MypageCommentsFilter.tsx
+++ src/app/(route)/mypage/comments/_components/MypageCommentsFilter/MypageCommentsFilter.tsx
@@ -2,14 +2,20 @@ import { Filter, KebabMenu } from "@/components/common";
 import MypageCommentsBottomSheet from "../MypageCommentsBottomSheet/MypageCommentsBottomSheet";
 import { MYPAGE_COMMENTS_FILTER } from "../../_constants/MYPAGE_COMMENTS_FILTER";
 import { useState } from "react";
-import { FilterModeType } from "../../../posts/_types/FilterModeType";
 
 const MypageCommentsFilter = () => {
   const [isBottomSheet, setIsBottomSheet] = useState(false);
+  const [isKebabMenu, setIsKebabMenu] = useState(false);
+
+  const MYPAGE_COMMENTS_KEBAB_ITEM = [
+    { text: "최신순", onClick: () => setIsKebabMenu(false) },
+    { text: "오래된 순", onClick: () => setIsKebabMenu(false) },
+  ];
 
   // TODO(수현): name 타입 안정성 높이기
   const handleFilterClick = (name: string) => {
     if (name === "최신순") {
+      setIsKebabMenu(true);
     } else {
       setIsBottomSheet(false);
     }
@@ -18,20 +24,28 @@ const MypageCommentsFilter = () => {
   return (
     <section className="flex w-full gap-2 overflow-x-auto px-5 py-[14px]">
       <h2 className="sr-only">필터링 영역</h2>
-      {MYPAGE_COMMENTS_FILTER.map((item) => (
-        <Filter
-          key={item.name}
-          ariaLabel={item.name}
-          icon={item.icon}
-          onSelected={false}
-          onClick={() => handleFilterClick(item.name)}
-          iconPosition={item.iconPosition}
-        >
-          {item.name}
-        </Filter>
-      ))}
 
-      {/* <KebabMenu items={}/> */}
+      <div className="relative">
+        {MYPAGE_COMMENTS_FILTER.map((item) => (
+          <Filter
+            key={item.name}
+            ariaLabel={item.name}
+            icon={item.icon}
+            onSelected={false}
+            onClick={() => handleFilterClick(item.name)}
+            iconPosition={item.iconPosition}
+          >
+            {item.name}
+          </Filter>
+        ))}
+
+        {isKebabMenu && (
+          <div className="absolute top-full">
+            <KebabMenu items={MYPAGE_COMMENTS_KEBAB_ITEM} />
+          </div>
+        )}
+      </div>
+
       <MypageCommentsBottomSheet onClose={() => setIsBottomSheet(false)} isOpen={isBottomSheet} />
     </section>
   );
