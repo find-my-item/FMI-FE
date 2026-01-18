a1f9b21c62ae7b136f92948d420d5e35407222d6
suhyeon0111 <soo4833@naver.com>
2026-01-18 21:27:57 +0900

    chore(주석): onEnter함수 관련 주석 추가

--- src/app/(route)/mypage/comments/_components/MypageCommentsSearch/MypageCommentsSearch.tsx
+++ src/app/(route)/mypage/comments/_components/MypageCommentsSearch/MypageCommentsSearch.tsx
@@ -4,6 +4,7 @@ const MypageCommentsSearch = () => {
   return (
     <section className="w-full px-5 py-[10px]">
       <h2 className="sr-only">검색 영역</h2>
+      {/* TODO(수현): onEnter 함수 api 연결 시 코드 추가 예정 */}
       <InputSearch name="search" mode="onChange" onEnter={() => {}} />
     </section>
   );
