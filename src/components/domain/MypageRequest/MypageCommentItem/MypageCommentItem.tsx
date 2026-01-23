8b5f2fcf308e70437a92038ba79e60164d244476
suhyeon0111 <soo4833@naver.com>
2026-01-23 22:37:46 +0900

    refactor(MypageCommentItem): 날짜 없을 시 span 태그 제거, nickname은 span태그로 변경

--- src/components/domain/MypageRequest/MypageCommentItem/MypageCommentItem.tsx
+++ src/components/domain/MypageRequest/MypageCommentItem/MypageCommentItem.tsx
@@ -28,6 +28,7 @@ const MypageRequestComment = ({ ...props }: DetailCommentsProps) => {
             {props.status === "admin" && <Chip label="관리자" type="admin" />}
             <h3 className="text-body1-medium text-layout-header-default">{displayName}</h3>
           </span>
+
           {displayDate && (
             <span className="text-body2-regular text-layout-body-default">
               {formatDate(displayDate)}
