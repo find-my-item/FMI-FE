853706e37c94be5bc06b2742a21e0f1423fafbf6
suhyeon0111 <soo4833@naver.com>
2026-01-23 21:09:16 +0900

    rename(MypageEmptyUI): props iconName으로 수정

--- src/components/domain/MypageEmptyUI/MypageEmptyUI.tsx
+++ src/components/domain/MypageEmptyUI/MypageEmptyUI.tsx
@@ -4,12 +4,12 @@ import { IconName } from "@/components/common/Icon/Icon";
 interface MypageEmptyUIProps {
   titleText: string;
   subText: string;
-  IconName: IconName;
+  iconName: IconName;
 }
-const MypageEmptyUI = ({ titleText, subText, IconName }: MypageEmptyUIProps) => {
+const MypageEmptyUI = ({ titleText, subText, iconName }: MypageEmptyUIProps) => {
   return (
     <div className="gap-5 py-20 text-center flex-col-center">
-      <Icon name={IconName} size={70} />
+      <Icon name={iconName} size={70} />
       <h2 className="text-h2-bold text-layout-header-default">아직 {titleText}이 없어요.</h2>
       <p className="text-body2-regular text-layout-body-default">
         아직 {titleText}이 없습니다. <br />
