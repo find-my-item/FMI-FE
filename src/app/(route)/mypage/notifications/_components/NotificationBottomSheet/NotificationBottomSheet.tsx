ea6f424153432693630d41beaf0da445f68dd2eb
suhyeon0111 <soo4833@naver.com>
2026-01-25 16:12:11 +0900

    refactor(notification): bottomSheet div 제거

--- src/app/(route)/mypage/notifications/_components/NotificationBottomSheet/NotificationBottomSheet.tsx
+++ src/app/(route)/mypage/notifications/_components/NotificationBottomSheet/NotificationBottomSheet.tsx
@@ -20,30 +20,30 @@ const NotificationBottomSheet = ({ isOpen, onClose }: NotificationBottomSheetPro
   };
 
   return (
-    <PopupLayout isOpen={isOpen} onClose={onClose}>
-      <div className="w-full px-5 py-10 flex-col-center">
-        <h2 className="text-h2-medium text-layout-header-default">카테고리 키워드</h2>
-        <div className="mt-8 flex w-full flex-wrap gap-x-2 gap-y-3">
-          {CATEGORY_OPTIONS.map((item) => {
-            const isSelected = selectedCategories.includes(item.value);
+    <PopupLayout className="w-full px-5 py-10 flex-col-center" isOpen={isOpen} onClose={onClose}>
+      {/* <div className="w-full px-5 py-10 flex-col-center"> */}
+      <h2 className="text-h2-medium text-layout-header-default">카테고리 키워드</h2>
+      <div className="mt-8 flex w-full flex-wrap gap-x-2 gap-y-3">
+        {CATEGORY_OPTIONS.map((item) => {
+          const isSelected = selectedCategories.includes(item.value);
 
-            return (
-              <Filter
-                key={item.value}
-                ariaLabel={item.label}
-                onSelected={isSelected}
-                onClick={() => handleToggle(item.value)}
-              >
-                {item.label}
-              </Filter>
-            );
-          })}
-        </div>
-
-        <Button className="mt-12 w-full" onClick={onClose}>
-          적용하기
-        </Button>
+          return (
+            <Filter
+              key={item.value}
+              ariaLabel={item.label}
+              onSelected={isSelected}
+              onClick={() => handleToggle(item.value)}
+            >
+              {item.label}
+            </Filter>
+          );
+        })}
       </div>
+
+      <Button className="mt-12 w-full" onClick={onClose}>
+        적용하기
+      </Button>
+      {/* </div> */}
     </PopupLayout>
   );
 };
