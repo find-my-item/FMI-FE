f1610a84298cee7bfa03c9003637c84d4a5cb4b2
suhyeon0111 <soo4833@naver.com>
2026-01-25 17:43:48 +0900

    fix(authApi): 요청 api url이 리프레쉬 토큰 재발급 url라면 재발급 요청 금지

--- src/api/_base/axios/authApi.ts
+++ src/api/_base/axios/authApi.ts
@@ -13,15 +13,16 @@ authApi.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
+    const isAuthError = [401, 403].includes(error.response?.status);
+    const isAlreadyRetried = originalRequest._retry;
+    const isRefreshRequest = originalRequest.url === "/auth/refresh";
+
     // 서버환경에서 실행했을때 에러라는 것을 알려줌
     if (typeof window === "undefined") {
       return Promise.reject(error);
     }
 
-    if (
-      (error.response?.status === 401 || error.response?.status === 403) &&
-      !originalRequest._retry
-    ) {
+    if (isAuthError && !isAlreadyRetried && !isRefreshRequest) {
       originalRequest._retry = true;
 
       try {
