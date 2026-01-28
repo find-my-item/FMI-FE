7f44ea36de1e78abfec07b2e5cbbf18f8c703a88
wlrnjs <wlrnjs5824@gmail.com>
2026-01-28 16:53:02 +0900

    design(PostDetailHeader): 디자인 토큰 적용

--- src/app/(route)/list/[id]/_components/PostDetailHeader/PostDetailHeader.tsx
+++ src/app/(route)/list/[id]/_components/PostDetailHeader/PostDetailHeader.tsx
@@ -23,14 +23,14 @@ const PostDetailHeader = ({ headerData }: PostDetailHeaderType) => {
 
       <section
         aria-label="게시글 작성자 정보"
-        className="flex flex-col items-start justify-center gap-5 border-b border-flatGray-50 p-[20px]"
+        className="flex flex-col items-start justify-center gap-5 border-b border-divider-default p-5"
       >
         <div className="flex items-center justify-start gap-[14px]">
           <ProfileAvatar size={40} src={profileUrl} alt={nickName} priority={true} />
 
           <div className="flex flex-col items-start justify-center">
-            <p className="text-[16px]">{nickName}</p>
-            <div className="text-[14px] leading-[20px] text-flatGray-600">
+            <p className="text-body1-medium text-layout-header-default">{nickName}</p>
+            <div className="text-body2-regular text-layout-body-default">
               <span className="after:mx-2 after:inline-block after:content-['·']">
                 작성글 {userPostCount || 0}
               </span>
@@ -39,7 +39,7 @@ const PostDetailHeader = ({ headerData }: PostDetailHeaderType) => {
           </div>
         </div>
 
-        <Button as={Link} href={`/chat/${postId}`} className="w-full py-[10px]">
+        <Button as={Link} href={`/chat/${postId}`} className="min-h-11 w-full py-[10px]">
           채팅하러 가기
         </Button>
       </section>
