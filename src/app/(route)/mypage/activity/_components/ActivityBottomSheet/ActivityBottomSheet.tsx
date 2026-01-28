2a0425aba00fd1f56feb9d08b564e2ef02b7d233
suhyeon0111 <soo4833@naver.com>
2026-01-28 17:00:23 +0900

    chore(activity): selectBottomSheet 추가

--- src/app/(route)/mypage/activity/_components/ActivityBottomSheet/ActivityBottomSheet.tsx
+++ src/app/(route)/mypage/activity/_components/ActivityBottomSheet/ActivityBottomSheet.tsx
@@ -2,6 +2,7 @@ import { Button, Filter } from "@/components/common";
 import { PopupLayout } from "@/components/domain";
 import DateRangeBottomSheet from "@/components/domain/DateRangeSheet/DateRangeSheet";
 import { ACTIVITY_OPTIONS } from "../../_constants/ACTIVITY_OPTIONS";
+import { SelectBottomSheet } from "../../../_internal";
 
 interface ActivityBottomSheetProps {
   mode: "Date" | "Type";
@@ -14,33 +15,9 @@ const ActivityBottomSheet = ({ mode, isOpen, onClose }: ActivityBottomSheetProps
     return <DateRangeBottomSheet isOpen={isOpen} onClose={onClose} />;
   }
 
-  const handleToggle = (value: any) => {};
-
   if (mode === "Type") {
     return (
-      <PopupLayout className="w-full px-5 py-10 flex-col-center" isOpen={isOpen} onClose={onClose}>
-        <h2 className="text-h2-medium text-layout-header-default">필터</h2>
-        <div className="mt-8 flex w-full flex-wrap gap-x-2 gap-y-3">
-          {ACTIVITY_OPTIONS.map((item) => {
-            // const isSelected = selectedCategories.includes(item.value);
-
-            return (
-              <Filter
-                key={item.value}
-                ariaLabel={item.label}
-                onSelected={false}
-                onClick={() => handleToggle(item.value)}
-              >
-                {item.label}
-              </Filter>
-            );
-          })}
-        </div>
-
-        <Button className="mt-12 w-full" onClick={onClose}>
-          적용하기
-        </Button>
-      </PopupLayout>
+      <SelectBottomSheet isOpen={isOpen} onClose={onClose} title="필터" option={ACTIVITY_OPTIONS} />
     );
   }
 };
