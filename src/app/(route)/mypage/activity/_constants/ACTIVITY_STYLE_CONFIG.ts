cfc8599a67a2ed5111a69d30ad3b6929b22e6020
suhyeon0111 <soo4833@naver.com>
2026-01-29 21:52:32 +0900

    chore(activity): 아이콘 스타일 디자인 토큰 필요 주석 추가

--- src/app/(route)/mypage/activity/_constants/ACTIVITY_STYLE_CONFIG.ts
+++ src/app/(route)/mypage/activity/_constants/ACTIVITY_STYLE_CONFIG.ts
@@ -1,10 +1,11 @@
+// TODO(수현): 디자인 토큰 변환
 export const ACTIVITY_STYLE_CONFIG = {
-  POST: { bgColor: "bg-fill-accent-foundItem", iconName: "Post" },
-  COMMENT: { bgColor: "bg-fill-neutral-strong-default", iconName: "Comment" },
+  POST: { bgColor: "bg-[#4F95FF]", iconName: "Post" },
+  COMMENT: { bgColor: "bg-[#5D5D5D]", iconName: "Comment" },
   FAVORITE: { bgColor: "bg-system-bookmark", iconName: "EmptyStar" },
-  INQUIRY: { bgColor: "bg-fill-neutral-strong-default", iconName: "Inquiry" },
-  INQUIRY_REPLY: { bgColor: "bg-fill-brand-normal-default", iconName: "Inquiry" },
-  REPORT: { bgColor: "bg-fill-neutral-strong-default", iconName: "Report" },
-  REPORT_COMPLETE: { bgColor: "bg-fill-brand-normal-default", iconName: "Report" },
-  ALERT_SETTING: { bgColor: "bg-fill-neutral-strong-default", iconName: "Alert" },
+  INQUIRY: { bgColor: "bg-[#5D5D5D]", iconName: "Inquiry" },
+  INQUIRY_REPLY: { bgColor: "bg-[#0AA874]", iconName: "Inquiry" },
+  REPORT: { bgColor: "bg-[#5D5D5D]", iconName: "Report" },
+  REPORT_COMPLETE: { bgColor: "bg-[#0AA874]", iconName: "Report" },
+  ALERT_SETTING: { bgColor: "bg-[#5D5D5D]", iconName: "Alert" },
 } as const;
