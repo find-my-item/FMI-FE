24e84cad13451c90a53d50b94c073a083ab34daf
wlrnjs <wlrnjs5824@gmail.com>
2026-02-03 23:31:24 +0900

    design(not-found): 404 페이지 디자인 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,28 @@
-import { Icon } from "@/components/common";
 import Link from "next/link";
+import { Button, Icon } from "@/components/common";
 
 const NotFound = () => {
   return (
-    <main className="h-[100dvh] flex-col-center">
+    <main className="min-h-screen flex-col-center">
       <Icon name="NotFound" size={74} />
-      <section className="gap-12 flex-col-center">
-        <header className="gap-3 flex-col-center">
-          <div className="mb-[6px] gap-[10px] flex-col-center">
+
+      <section className="space-y-12">
+        <header className="gap-[6px] flex-col-center">
+          <div className="gap-[10px] py-1 flex-col-center">
             <h1 className="text-title2-medium text-layout-header-default">404</h1>
             <h2 className="text-h2-bold text-layout-header-default">페이지를 찾을 수 없습니다.</h2>
           </div>
+
           <p className="text-center text-body2-regular text-layout-body-default">
             존재하지 않는 주소를 입력했거나 <br />
             요청하신 페이지를 사용할 수 없습니다.
           </p>
         </header>
 
-        <footer className="flex justify-center">
-          <Link
-            href="/"
-            className="rounded-[10px] border border-neutral-normal-default px-5 py-[10px] text-body1-semibold text-neutral-normal-default bg-fill-neutral-normal-default"
-          >
-            이전 페이지로 돌아가기
-          </Link>
+        <footer className="flex-center">
+          <Button as={Link} href="/" variant="outlined" replace className="min-h-11">
+            홈으로 이동하기
+          </Button>
         </footer>
       </section>
     </main>
