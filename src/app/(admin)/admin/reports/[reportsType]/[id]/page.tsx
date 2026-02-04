12ba418b7326c8256329753f14ad9108a9928def
wlrnjs <wlrnjs5824@gmail.com>
2026-02-04 18:38:03 +0900

    feat(admin-reports-detail): 접근성 개선

--- src/app/(admin)/admin/reports/[reportsType]/[id]/page.tsx
+++ src/app/(admin)/admin/reports/[reportsType]/[id]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useParams } from "next/navigation";
-import NotFound from "@/app/not-found";
+import { useParams, notFound } from "next/navigation";
 import { Icon } from "@/components/common";
 import { DetailHeader } from "@/components/layout";
 import { cn } from "@/utils";
@@ -17,7 +16,7 @@ const page = () => {
   const id = params.id;
   console.log("id", id);
 
-  if (reportsType !== "report" && reportsType !== "inquiry") return <NotFound />;
+  if (reportsType !== "report" && reportsType !== "inquiry") return notFound();
 
   return (
     <>
@@ -25,7 +24,7 @@ const page = () => {
       <h1 className="sr-only">신고/문의 상세</h1>
 
       <div className="h-base">
-        <section className="space-y-[14px] px-5 py-[30px]">
+        <section aria-label="신고/문의 내용" className="space-y-[14px] px-5 py-[30px]">
           <div className="flex items-center gap-2">
             <button className="flex items-center gap-1">
               접수 <Icon name="ArrowDown" size={10} />
@@ -41,7 +40,7 @@ const page = () => {
               </h2>
               <div className="flex items-center gap-2 text-body2-regular text-layout-body-default">
                 <span className="block after:mx-2 after:content-['·']">닉네임최대열글자확인</span>
-                <time>2025.10.20</time>
+                <time dateTime="2025-05-06">2025.05.06</time>
               </div>
             </div>
 
@@ -51,31 +50,38 @@ const page = () => {
           </div>
         </section>
 
-        <section>
-          <article
-            className={cn(
-              "flex flex-col gap-2 px-5 py-9",
-              isAdmin ? "bg-fill-neutral-strong-default" : "bg-white"
-            )}
-          >
-            <div className="flex items-center gap-[14px]">
-              <Image
-                src={"/test_list.JPG"}
-                alt="프로필 이미지"
-                width={30}
-                height={30}
-                className="rounded-full"
-              />
-              <div className="flex flex-col gap-[2px]">
-                <p className="text-body1-medium text-layout-header-default">찾아줘 관리자</p>
-                <time className="text-body2-regular text-layout-body-default">2025.05.06</time>
-              </div>
-            </div>
-            <p className="text-body1-regular text-layout-header-default">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, consequuntur
-              consequatur ducimus amet aliquid culpa.
-            </p>
-          </article>
+        <section aria-labelledby="comments-title">
+          <h2 id="comments-title" className="sr-only">
+            댓글
+          </h2>
+          <ul>
+            <li>
+              <article
+                className={cn(
+                  "flex flex-col gap-2 px-5 py-9",
+                  isAdmin ? "bg-fill-neutral-strong-default" : "bg-white"
+                )}
+              >
+                <div className="flex items-center gap-[14px]">
+                  <Image
+                    src={"/test_list.JPG"}
+                    alt="프로필 이미지"
+                    width={30}
+                    height={30}
+                    className="rounded-full"
+                  />
+                  <div className="flex flex-col gap-[2px]">
+                    <p className="text-body1-medium text-layout-header-default">찾아줘 관리자</p>
+                    <time className="text-body2-regular text-layout-body-default">2025.05.06</time>
+                  </div>
+                </div>
+                <p className="text-body1-regular text-layout-header-default">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+                  consequuntur consequatur ducimus amet aliquid culpa.
+                </p>
+              </article>
+            </li>
+          </ul>
         </section>
       </div>
     </>
