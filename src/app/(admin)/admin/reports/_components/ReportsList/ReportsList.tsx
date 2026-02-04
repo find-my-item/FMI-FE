545007f1e23d8eb393cf1a08026392a7b9ad53ab
wlrnjs <wlrnjs5824@gmail.com>
2026-02-04 17:41:31 +0900

    feat(admin-reports): 목업 데이터 기반 리스트 영역 연결 작업

--- src/app/(admin)/admin/reports/_components/ReportsList/ReportsList.tsx
+++ src/app/(admin)/admin/reports/_components/ReportsList/ReportsList.tsx
@@ -1,17 +1,25 @@
-import { Fragment } from "react";
 import AdminReportsItem from "../../../_components/AdminReportsItem/AdminReportsItem";
+import { MOCK_ADMIN_INQUIRY_LIST, MOCK_ADMIN_REPORT_LIST } from "@/mock/data";
+import { toInquiryItemVM, toReportItemVM } from "../../../_utils/toReportsItemVM/toReportsItemVM";
+import { ReportsTabType } from "../../_types/ReportsTabType";
 
-const ReportsList = () => {
+interface ReportsListProps {
+  activeTab: ReportsTabType;
+}
+
+const ReportsList = ({ activeTab }: ReportsListProps) => {
+  const isReport = activeTab === "report";
+
   return (
     <section aria-label="신고/문의 목록">
       <ul className="flex flex-col gap-2">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <Fragment key={index}>
-            <AdminReportsItem />
-            <AdminReportsItem />
-            <AdminReportsItem />
-          </Fragment>
-        ))}
+        {isReport
+          ? MOCK_ADMIN_REPORT_LIST.map((item, index) => (
+              <AdminReportsItem key={index} data={toReportItemVM(item)} />
+            ))
+          : MOCK_ADMIN_INQUIRY_LIST.map((item, index) => (
+              <AdminReportsItem key={index} data={toInquiryItemVM(item)} />
+            ))}
       </ul>
     </section>
   );
--- src/app/(admin)/admin/reports/_components/ReportsView/ReportsView.tsx
+++ src/app/(admin)/admin/reports/_components/ReportsView/ReportsView.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import { Tab } from "@/components/domain";
 import { AdminFilter, AdminSearch } from "../../../_components";
 import { AdminFilterItemType } from "../../../_types";
 import ReportsList from "../ReportsList/ReportsList";
+import { ReportsTabType } from "../../_types/ReportsTabType";
+import { REPORTS_TAB } from "../../_constants/REPORTS_TAB";
 
+// TODO(지권): 추후 필터 기능 추가
 const reportsFilters: AdminFilterItemType[] = [
   {
     label: "상태",
@@ -19,22 +23,21 @@ const reportsFilters: AdminFilterItemType[] = [
 ];
 
 const ReportsView = () => {
+  const [activeTab, setActiveTab] = useState<ReportsTabType>("report");
+
   return (
     <div className="h-base">
       <Tab
-        tabs={[
-          { label: "신고", key: "report" },
-          { label: "문의", key: "inquiry" },
-        ]}
-        selected="report"
-        onValueChange={(key) => console.log(key)}
+        tabs={REPORTS_TAB}
+        selected={activeTab}
+        onValueChange={(key) => setActiveTab(key as ReportsTabType)}
       />
 
       <AdminSearch onEnter={() => {}} />
 
       <AdminFilter filters={reportsFilters} />
 
-      <ReportsList />
+      <ReportsList activeTab={activeTab} />
     </div>
   );
 };
