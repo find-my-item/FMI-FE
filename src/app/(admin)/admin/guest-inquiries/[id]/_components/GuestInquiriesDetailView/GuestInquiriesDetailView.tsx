e3e0fc9a7356b19178ada75a37534927de6095f5
wlrnjs <wlrnjs5824@gmail.com>
2026-02-05 14:36:34 +0900

    feat(admin-guest-inquiries-detail-page): 이메일 복사하기 기능 추가

--- src/app/(admin)/admin/guest-inquiries/[id]/_components/GuestInquiriesDetailView/GuestInquiriesDetailView.tsx
+++ src/app/(admin)/admin/guest-inquiries/[id]/_components/GuestInquiriesDetailView/GuestInquiriesDetailView.tsx
@@ -1,17 +1,34 @@
+"use client";
+
 import { Button } from "@/components/common";
+import { useToast } from "@/context/ToastContext";
 import { MOCK_GUEST_INQUIRY_DETAIL_DATA } from "@/mock/data";
 import { AdminDetailSection } from "@/app/(admin)/admin/_components";
 
 const GuestInquiriesDetailView = () => {
+  const { addToast } = useToast();
+
+  const copyEmail = () => {
+    try {
+      navigator.clipboard.writeText(MOCK_GUEST_INQUIRY_DETAIL_DATA.userName);
+      addToast("이메일을 클립보드에 복사했어요", "success");
+    } catch (error) {
+      console.error(error);
+      addToast("이메일 복사에 실패했어요", "error");
+    }
+  };
+
   return (
     <div className="flex flex-col h-base">
-      <section className="flex-1">
+      <article className="flex-1">
         <AdminDetailSection data={MOCK_GUEST_INQUIRY_DETAIL_DATA} />
-      </section>
+      </article>
 
-      <section className="border-t border-divider-default px-5 pb-8 pt-3">
-        <Button className="min-h-11 w-full">이메일 복사하기</Button>
-      </section>
+      <div className="sticky bottom-0 border-t border-divider-default bg-white px-5 pb-8 pt-3">
+        <Button className="min-h-11 w-full" onClick={copyEmail}>
+          이메일 복사하기
+        </Button>
+      </div>
     </div>
   );
 };
