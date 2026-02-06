8e16f92677cebed15096446d2d1afe92266e943a
wlrnjs <wlrnjs5824@gmail.com>
2026-02-06 21:23:41 +0900

    chore(user): 최신화 커밋

--- src/app/(route)/user/[userId]/_components/TabContents/TabContents.tsx
+++ src/app/(route)/user/[userId]/_components/TabContents/TabContents.tsx
@@ -2,13 +2,13 @@
 
 import { PostListItem } from "@/components/domain";
 import { EmptyState, LoadingState } from "@/components/state";
-import { UserProfileIdDataType } from "@/api/fetch/user";
+import { UserCommentsDataType, UserProfileItem } from "@/api/fetch/user";
 import { UserProfileTabKey } from "../../_types/USER_TABS";
 import { CommentItem } from "../_internal";
 
 interface TabContentsProps {
   selectedTab: UserProfileTabKey;
-  data?: UserProfileIdDataType;
+  data?: UserProfileItem[];
   isLoading: boolean;
 }
 
@@ -16,11 +16,12 @@ const TabContents = ({ selectedTab, data, isLoading }: TabContentsProps) => {
   if (isLoading) return <LoadingState />;
   if (!data) return null;
 
+  console.log(data);
   return (
     <section aria-label="탭 콘텐츠">
       <ul>
         {selectedTab === "posts" &&
-          (data.posts.length === 0 ? (
+          (data.length === 0 ? (
             <li>
               <EmptyState
                 icon={{ iconName: "UserEmptyPost", iconSize: 70 }}
@@ -29,26 +30,28 @@ const TabContents = ({ selectedTab, data, isLoading }: TabContentsProps) => {
               />
             </li>
           ) : (
-            data.posts.map((post) => (
-              <PostListItem post={post} linkState="list" key={post.postId} />
-            ))
+            data.map((post) => <PostListItem post={post} linkState="list" key={post.postId} />)
           ))}
 
         {selectedTab === "comments" &&
-          (data.comments.length === 0 ? (
-            <li>
-              <EmptyState
-                icon={{ iconName: "UserEmptyComment", iconSize: 70 }}
-                title="아직 작성한 댓글이 없어요"
-                description={"아직 작성한 댓글이 없습니다.\n지금 바로 댓글을 남겨보세요!"}
-              />
-            </li>
-          ) : (
-            data.comments.map((comment) => <CommentItem key={comment.commentId} data={comment} />)
-          ))}
+          (() => {
+            const comments = data as UserCommentsDataType[];
+
+            return comments.length === 0 ? (
+              <li>
+                <EmptyState
+                  icon={{ iconName: "UserEmptyComment", iconSize: 70 }}
+                  title="아직 작성한 댓글이 없어요"
+                  description={"아직 작성한 댓글이 없습니다.\n지금 바로 댓글을 남겨보세요!"}
+                />
+              </li>
+            ) : (
+              comments.map((comment) => <CommentItem key={comment.commentId} data={comment} />)
+            );
+          })()}
 
         {selectedTab === "favorites" &&
-          (data.favorites.length === 0 ? (
+          (data.length === 0 ? (
             <li>
               <EmptyState
                 icon={{ iconName: "UserEmptyFavorite", iconSize: 70 }}
@@ -59,9 +62,7 @@ const TabContents = ({ selectedTab, data, isLoading }: TabContentsProps) => {
               />
             </li>
           ) : (
-            data.favorites.map((post) => (
-              <PostListItem post={post} linkState="list" key={post.postId} />
-            ))
+            data.map((post) => <PostListItem post={post} linkState="list" key={post.postId} />)
           ))}
       </ul>
     </section>
