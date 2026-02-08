6ba6e9c7cc0858671e8fa7022e84f0ac23403fc0
jun <znznun@gmail.com>
2026-02-09 05:17:24 +0900

    fix(ChatItem): 구조분해할당 수정

--- src/app/(route)/chat/_components/ChatItem/ChatItem.tsx
+++ src/app/(route)/chat/_components/ChatItem/ChatItem.tsx
@@ -10,22 +10,24 @@ interface ChatItemProps {
 }
 
 const ChatItem = ({ chatRoom }: ChatItemProps) => {
-  const { postInfo, contactUser, lastMessageSentAt, lastMessage, unreadCount, messageType } =
-    chatRoom;
+  const { lastMessageSentAt, lastMessage, unreadCount, messageType } = chatRoom;
+  const { postId, address, thumbnailUrl } = chatRoom.postInfo;
+  const { nickname, profileImageUrl } = chatRoom.contactUser;
+  const { roomId } = chatRoom;
 
   const lastMessageIsImage = lastMessageSentAt && messageType === "IMAGE";
 
   return (
     <Link
-      href={`/chat/${postInfo?.postId}?roomId=${chatRoom.roomId}`}
-      aria-label={`${postInfo?.postId} 채팅방 링크`}
+      href={`/chat/${postId}?roomId=${roomId}`}
+      aria-label={`${postId} 채팅방 링크`}
       className="flex min-h-[113px] w-full items-center gap-3 border-b border-divider-default px-4 py-6 transition-colors hover:bg-flatGray-25"
     >
       <div className="relative h-[58px] w-[58px] shrink-0">
-        {contactUser?.profileImageUrl ? (
+        {profileImageUrl ? (
           <Image
             alt="유저 프로필 이미지"
-            src={contactUser?.profileImageUrl}
+            src={profileImageUrl}
             width={26}
             height={26}
             className="absolute left-0 top-0 z-10 rounded-full border-[1.5px] border-white object-cover"
@@ -41,7 +43,7 @@ const ChatItem = ({ chatRoom }: ChatItemProps) => {
         {/* TODO(형준): 대체 이미지 수정 예정 */}
         <Image
           alt="게시글 썸네일 이미지"
-          src={postInfo?.thumbnailUrl || MOCK_IMAGES[0]}
+          src={thumbnailUrl || MOCK_IMAGES[0]}
           width={50}
           height={50}
           sizes="50px"
@@ -52,7 +54,7 @@ const ChatItem = ({ chatRoom }: ChatItemProps) => {
       <div className="w-full min-w-0 space-y-[2px]">
         <div className="flex items-center justify-between truncate">
           <span className="truncate text-h3-semibold text-layout-header-default">
-            {contactUser?.nickname || "닉네임을 불러오지 못했습니다."}
+            {nickname || "닉네임을 불러오지 못했습니다."}
           </span>
           {unreadCount > 0 && (
             <span className="rounded-full bg-flatGreen-500 px-[5.5px] py-[1.5px] text-caption2-semibold text-white flex-center">
@@ -61,9 +63,7 @@ const ChatItem = ({ chatRoom }: ChatItemProps) => {
           )}
         </div>
         <div className="flex truncate text-caption1-medium text-layout-body-default">
-          <span className="truncate">
-            {postInfo?.address || "위치 정보를 불러오지 못했습니다."}
-          </span>
+          <span className="truncate">{address || "위치 정보를 불러오지 못했습니다."}</span>
           <time className="flex-shrink-0 before:mx-1 before:content-['·']">
             {formatDate(lastMessageSentAt || "시간 정보가 없습니다.")}
           </time>
