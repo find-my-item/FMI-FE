e8181770488db4f209d6de2ec3154f346f946b44
wlrnjs <wlrnjs5824@gmail.com>
2026-02-10 18:33:21 +0900

    design(list-detail): 비슷한 분실물 영역 에러 UI 추가

--- src/app/(route)/list/[id]/_components/_internal/index.ts
+++ src/app/(route)/list/[id]/_components/_internal/index.ts
@@ -1,2 +1,3 @@
+export { default as ErrorSimilarSection } from "./ErrorSimilarSection/ErrorSimilarSection";
 export { default as SimilarItem } from "./SimilarItem/SimilarItem";
 export { default as SimilarItemSkeleton } from "./SimilarItemSkeleton/SimilarItemSkeleton";
