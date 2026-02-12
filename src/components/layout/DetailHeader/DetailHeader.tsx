ff4fce7722912633eaa4346d81b13a2c623dda4f
jun <znznun@gmail.com>
2026-02-12 23:40:33 +0900

    docs(detailHeader): TSDoc 작성

--- src/components/layout/DetailHeader/DetailHeader.tsx
+++ src/components/layout/DetailHeader/DetailHeader.tsx
@@ -9,6 +9,39 @@ interface DetailHeaderProps {
   children?: ReactNode;
 }
 
+/**
+ * @author hyungjun
+ *
+ * @description
+ * 상세 페이지에서 사용하는 헤더 컴포넌트입니다.
+ * 뒤로가기 버튼과 제목을 표시하며, 오른쪽에 추가 액션 버튼들을 배치할 수 있습니다.
+ * sticky 포지션으로 스크롤 시에도 상단에 고정됩니다.
+ *
+ * @param title - 헤더에 표시할 제목입니다. (선택)
+ * @param children - 헤더 오른쪽에 배치할 액션 버튼들입니다. (선택)
+ * `DetailHeaderParts`에서 제공하는 `DetailHeaderSave`, `DetailHeaderSearch`, `DetailHeaderStar`, `DetailHeaderMenu` 등의 컴포넌트를 사용할 수 있습니다.
+ *
+ * @example
+ * ```tsx
+ * import { DetailHeader } from "@/components/layout";
+ * import { DetailHeaderSave } from "@/components/layout/DetailHeader/DetailHeaderParts";
+ *
+ * // 제목만 있는 경우
+ * <DetailHeader title="자주 묻는 질문" />
+ *
+ * // 제목과 저장 버튼이 있는 경우
+ * <DetailHeader title="글쓰기">
+ *   <DetailHeaderSave onClick={handleSave} />
+ * </DetailHeader>
+ *
+ * // 여러 액션 버튼을 함께 사용하는 경우
+ * <DetailHeader title="게시글 상세">
+ *   <DetailHeaderStar isActive />
+ *   <DetailHeaderMenu onClick={handleMenu} />
+ * </DetailHeader>
+ * ```
+ */
+
 const DetailHeader = ({ title = "", children }: DetailHeaderProps) => {
   const router = useRouter();
 
