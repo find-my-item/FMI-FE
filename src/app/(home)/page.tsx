37cdbcb0d708812a168d6056654c4dbc4e4875a7
jun <znznun@gmail.com>
2026-02-15 05:47:48 +0900

    design(main): 메인 Header 퍼블리싱

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,8 +3,8 @@ import { Footer, Header } from "@/components/layout";
 
 const Page = () => {
   return (
-    <div className="h-[100dvh] w-full">
-      <BaseKakaoMap center={{ lat: 37.5665, lng: 126.978 }} level={10} showMarker={false} />
+    <div className="h-[calc(100dvh-4px)] w-full">
+      <BaseKakaoMap center={{ lat: 37.5665, lng: 126.978 }} level={7} showMarker={false} />
       <Header />
       <Footer />
     </div>
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,43 +1,19 @@
-// TODO(지권): 컴포넌트 사용 미확정
-
 "use client";
 
-import { useState } from "react";
-import Link from "next/link";
-import { useHiddenPath } from "@/hooks";
-import SideBar from "./_internal/SideBar";
-import Icon from "@/components/common/Icon/Icon";
-
-/**
- * @author jikwon
- *
- * 상단 header 컴포넌트입니다.
- *
- * useHiddenPath 훅을 사용하여 / 메인 페이지에만 header를 표시합니다.
- *
- * @example
- * ```tsx
- * <Header />
- * ```
- */
+import { Icon } from "@/components/common";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const isHidden = useHiddenPath();
-  if (isHidden) return null;
-
   return (
-    <>
-      <header className="fixed left-1/2 top-0 z-10 flex h-16 w-full max-w-[390px] -translate-x-1/2 items-center justify-between bg-gray-300 p-4">
-        <Link href="/" aria-label="홈으로 이동">
-          <Icon name="Logo" size={40} />
-        </Link>
-        <button onClick={() => setIsOpen(!isOpen)} className="select-none">
-          <Icon name="Menu" title="메뉴 열기" />
-        </button>
-      </header>
-      <SideBar isOpen={isOpen} onClose={() => setIsOpen(false)} />
-    </>
+    <header className="fixed left-1/2 top-0 z-10 w-full max-w-[390px] -translate-x-1/2 px-5 py-[10px]">
+      <div className="relative w-full rounded-[10px] bg-white px-5 py-4">
+        <input
+          type="text"
+          className="w-full pl-8 text-h3-semibold text-flatGray-700 placeholder:text-flatGray-700"
+          placeholder="현재 위치 (위치 정보 허용 시)"
+        />
+        <Icon name="Search" size={20} className="absolute top-[18.5px]" />
+      </div>
+    </header>
   );
 };
 
