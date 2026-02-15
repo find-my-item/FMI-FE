21132b4cf6f61803c2dba0ca1e51c2ef48e83867
jun <znznun@gmail.com>
2026-02-15 16:08:55 +0900

    fix(main): Header, page.tsx 내부로 이동

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,12 +5,14 @@ import { BottomSheet } from "./_components";
 import SideBar from "@/components/layout/Header/_internal/SideBar";
 import { useState } from "react";
 import { Icon } from "@/components/common";
+import { Header } from "@/components/layout";
 
 // TODO(형준): 임시 사이드바 추가, 향후 제거
 const Page = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="h-[calc(100dvh-4px)]">
+      <Header />
       <button onClick={() => setIsOpen(!isOpen)} className="absolute right-[410px] select-none">
         <Icon name="Menu" title="메뉴 열기" />
       </button>
