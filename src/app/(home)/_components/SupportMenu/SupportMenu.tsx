59e87be5b7fe74d129ea843cfeb9dec347daadf7
jun <znznun@gmail.com>
2026-02-15 11:57:41 +0900

    fix(main): SupportMenu key 누락 추가

--- src/app/(home)/_components/SupportMenu/SupportMenu.tsx
+++ src/app/(home)/_components/SupportMenu/SupportMenu.tsx
@@ -16,7 +16,11 @@ const SupportMenu = () => {
   return (
     <div className="w-full">
       {SUPPORT_MENU_ITEMS.map((item) => (
-        <Link href={item.href} className="flex items-center justify-between px-1 py-2">
+        <Link
+          key={item.label}
+          href={item.href}
+          className="flex items-center justify-between px-1 py-2"
+        >
           <span className="text-body1-medium text-layout-header-default">{item.label}</span>
           <Icon name="ArrowRightSmall" size={20} />
         </Link>
