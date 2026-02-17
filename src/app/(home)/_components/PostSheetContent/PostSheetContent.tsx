f3a0f190fbdeecbff7e035895739ab62b040a6d7
jun <znznun@gmail.com>
2026-02-18 08:22:16 +0900

    fix(main): PostSheetContent ul태그로 변경

--- src/app/(home)/_components/PostSheetContent/PostSheetContent.tsx
+++ src/app/(home)/_components/PostSheetContent/PostSheetContent.tsx
@@ -6,11 +6,11 @@ const PostSheetContent = () => {
   return (
     <>
       <HomeFilterSection />
-      <div className="-mx-5 mt-2 space-y-2">
+      <ul className="-mx-5 mt-2 space-y-2">
         {Array.from({ length: 10 }).map((_, index) => (
           <PostListItem key={index} post={MOCK_POST_ITEM} />
         ))}
-      </div>
+      </ul>
     </>
   );
 };
