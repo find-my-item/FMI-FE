7b90ccc21e7fa27c51dd9172d664825d2770ed0b
suhyeon0111 <soo4833@naver.com>
2026-02-19 02:04:54 +0900

    fix(mypage): useServerPrefetchQuery 헤더 쿠키 추가

--- src/app/(route)/mypage/layout.tsx
+++ src/app/(route)/mypage/layout.tsx
@@ -1,14 +1,21 @@
 import useServerPrefetchQuery from "@/api/_base/query/useServerPrefetchQuery";
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { cookies } from "next/headers";
 
 export default async function PostLayout({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
+
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("access_token")?.value;
 
   await useServerPrefetchQuery({
     queryClient,
     queryKey: ["users-me"],
     fetcher: () =>
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+        headers: {
+          Cookie: `access_token=${accessToken}`,
+        },
         next: { revalidate: 0 },
       }).then((res) => res.json()),
   });
