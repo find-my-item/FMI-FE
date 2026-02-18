e9155a40f6ef02ae251494c46c349d0d28b1d36d
wlrnjs <wlrnjs5824@gmail.com>
2026-02-18 21:07:20 +0900

    feat(user): 외부 클릭 시 신고하기 닫기

--- src/app/(route)/user/[userId]/_components/UserProfileDetailHeader/UserProfileDetailHeader.tsx
+++ src/app/(route)/user/[userId]/_components/UserProfileDetailHeader/UserProfileDetailHeader.tsx
@@ -1,34 +1,40 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useParams } from "next/navigation";
 import { ReportModal } from "@/components/domain";
 import { DetailHeader } from "@/components/layout";
 import { HeaderMenu } from "@/components/layout/DetailHeader/DetailHeaderParts";
 import { cn } from "@/utils";
+import { useHandleClickOutside } from "@/app/(route)/chat/hooks";
 
 const UserProfileDetailHeader = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
   const [isReportOpen, setIsReportOpen] = useState(false);
 
   const { userId } = useParams<{ userId: string }>();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  useHandleClickOutside(isMenuOpen, containerRef, dropdownRef, setIsMenuOpen);
 
   return (
-    <div className="relative w-full">
+    <div className="relative" ref={containerRef}>
       <DetailHeader title="프로필">
         <HeaderMenu ariaLabel="더보기 메뉴" onClick={() => setIsMenuOpen(!isMenuOpen)} />
       </DetailHeader>
       {isMenuOpen && (
-        <button
+        <div
+          ref={dropdownRef}
+          role="button"
           className={cn(
-            "glass-card absolute left-[65%] right-0 top-[60%] z-10 mt-2 max-w-[119px]",
-            "text-nowrap rounded-[20px] px-7 py-4 shadow-sm",
-            "border border-white bg-fill-neutral-subtle-default"
+            "absolute left-[65%] top-[90%] z-10 mt-2 cursor-pointer px-7 py-4",
+            "border border-white bg-fill-neutral-subtle-default",
+            "h-[57px] w-[119px] text-nowrap rounded-[20px] shadow-sm"
           )}
           onClick={() => setIsReportOpen(true)}
         >
           <span className="text-h3-medium text-system-warning">신고하기</span>
-        </button>
+        </div>
       )}
 
       <ReportModal
