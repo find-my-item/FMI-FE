2fd012d378139d8d6c3ee8a198db43ac9e56d6d5
wlrnjs <wlrnjs5824@gmail.com>
2026-02-18 18:11:36 +0900

    design(user): 신고하기 디자인 추가

--- src/app/(route)/user/[userId]/_components/UserProfileDetailHeader/UserProfileDetailHeader.tsx
+++ src/app/(route)/user/[userId]/_components/UserProfileDetailHeader/UserProfileDetailHeader.tsx
@@ -1,13 +1,43 @@
 "use client";
 
+import { useState } from "react";
+import { useParams } from "next/navigation";
+import { ReportModal } from "@/components/domain";
 import { DetailHeader } from "@/components/layout";
 import { HeaderMenu } from "@/components/layout/DetailHeader/DetailHeaderParts";
+import { cn } from "@/utils";
 
 const UserProfileDetailHeader = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const [isReportOpen, setIsReportOpen] = useState(false);
+
+  const { userId } = useParams<{ userId: string }>();
+
   return (
-    <DetailHeader title="프로필">
-      <HeaderMenu ariaLabel="더보기 메뉴" />
-    </DetailHeader>
+    <div className="relative w-full">
+      <DetailHeader title="프로필">
+        <HeaderMenu ariaLabel="더보기 메뉴" onClick={() => setIsMenuOpen(!isMenuOpen)} />
+      </DetailHeader>
+      {isMenuOpen && (
+        <button
+          className={cn(
+            "glass-card absolute left-[65%] right-0 top-[60%] z-10 mt-2 max-w-[119px]",
+            "text-nowrap rounded-[20px] px-7 py-4 shadow-sm",
+            "border border-white bg-fill-neutral-subtle-default"
+          )}
+          onClick={() => setIsReportOpen(true)}
+        >
+          <span className="text-h3-medium text-system-warning">신고하기</span>
+        </button>
+      )}
+
+      <ReportModal
+        isOpen={isReportOpen}
+        onClose={() => setIsReportOpen(false)}
+        targetType="USER"
+        targetId={Number(userId)}
+      />
+    </div>
   );
 };
 
