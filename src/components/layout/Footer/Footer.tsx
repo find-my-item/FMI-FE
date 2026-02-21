2b1d999e5fc7341e1f50d19e1653103dbec47123
jun <znznun@gmail.com>
2026-02-21 21:23:16 +0900

    design(main): Footer 여백 추가

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -9,6 +9,7 @@ const Footer = () => {
   const pathname = usePathname();
   const router = useRouter();
   const isActive = (href: FooterLinkHref) => pathname === href && "text-neutral-strong-focused";
+  const isHome = pathname === "/";
   const isHidden = useHiddenPath();
   const [showLoginRequiredNotice, setShowLoginRequiredNotice] = useState(false);
 
@@ -23,19 +24,21 @@ const Footer = () => {
   };
 
   return (
-    <footer className="fixed bottom-0 left-1/2 z-50 w-full max-w-[768px] -translate-x-1/2 overflow-visible border-t-[1.2px] border-divider-default bg-white px-5 pb-[27px] pt-[14px]">
-      <div className="flex justify-between text-caption2-medium text-labelsVibrant-secondary">
-        {FOOTER_LINK.map((link) => (
-          <FooterItem
-            key={link.name}
-            link={link}
-            isActive={isActive}
-            showLoginRequiredNotice={showLoginRequiredNotice}
-            onClick={handleChatClick}
-          />
-        ))}
-      </div>
-    </footer>
+    <div className={!isHome ? "pt-[86.67px]" : ""}>
+      <footer className="fixed bottom-0 left-1/2 z-50 w-full max-w-[768px] -translate-x-1/2 overflow-visible border-t-[1.2px] border-divider-default bg-white px-5 pb-[27px] pt-[14px]">
+        <div className="flex justify-between text-caption2-medium text-labelsVibrant-secondary">
+          {FOOTER_LINK.map((link) => (
+            <FooterItem
+              key={link.name}
+              link={link}
+              isActive={isActive}
+              showLoginRequiredNotice={showLoginRequiredNotice}
+              onClick={handleChatClick}
+            />
+          ))}
+        </div>
+      </footer>
+    </div>
   );
 };
 
