218b23b9c0f8e71ef43f7d061184b66bc8bee8d0
jun <znznun@gmail.com>
2026-02-21 17:37:37 +0900

    design(main): 메인 검색 empty 퍼블리싱

---
+++ src/app/(home)/_components/MainSearchHeader/_internal/SearchEmpty.tsx
@@ -0,0 +1,14 @@
+import { Icon } from "@/components/common";
+
+const SearchEmpty = () => {
+  return (
+    <div className="w-full gap-5 py-3 flex-col-center">
+      <div className="h-[32px] w-[32px] flex-shrink-0 rounded-full bg-fill-neutral-strong-default flex-center">
+        <Icon name="Clock" size={20} />
+      </div>
+      <p className="text-body1-regular text-labelsVibrant-primary">최근 검색한 기록이 없어요.</p>
+    </div>
+  );
+};
+
+export default SearchEmpty;
