15a8a2e7d9719b2811105ca294655d158f740e8a
jun <znznun@gmail.com>
2026-02-21 17:47:00 +0900

    design(main): 검색 결과 로딩 퍼블리싱

---
+++ src/app/(home)/_components/PostSheetContent/_internal/SearchLoading.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "@/components/common";
+
+const SearchLoading = () => {
+  return (
+    <div className="mt-[137px] flex-center">
+      <Icon name="Loading" className="animate-spin" size={30} />
+    </div>
+  );
+};
+
+export default SearchLoading;
