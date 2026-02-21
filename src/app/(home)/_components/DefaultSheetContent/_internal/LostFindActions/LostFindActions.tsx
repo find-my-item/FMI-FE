5a795e20a7dfd48908428a7b8b0f38b2960cb35e
jun <znznun@gmail.com>
2026-02-21 19:20:27 +0900

    design(main): 분실/찾기 내부 svg 따로 분리

---
+++ src/app/(home)/_components/DefaultSheetContent/_internal/LostFindActions/LostFindActions.tsx
@@ -0,0 +1,55 @@
+import { cn } from "@/utils";
+import Image from "next/image";
+import Link from "next/link";
+import LOST_FIND_ACTION_DATA from "./LOST_FIND_ACTION_DATA";
+
+const LostFindActions = () => {
+  return (
+    <div className="flex w-full gap-4">
+      {LOST_FIND_ACTION_DATA.map(
+        ({ type, title, positionImage, markImage, bagImage, messageImage, bgColor }) => {
+          const { src, size } = markImage;
+
+          return (
+            <Link
+              href={`/list?type=${type}`}
+              key={type}
+              aria-label={`${title}로 글 작성`}
+              className="min-w-0 flex-1"
+            >
+              <div className={cn("relative h-[106px] w-full overflow-hidden rounded-2xl", bgColor)}>
+                <Image src={positionImage} alt="lost position" width={50} height={70} priority />
+                <Image
+                  src={messageImage}
+                  alt="lost message"
+                  width={58}
+                  height={52}
+                  className="absolute bottom-3 left-4"
+                  priority
+                />
+                <Image
+                  src={src}
+                  alt="question"
+                  width={size.width}
+                  height={size.height}
+                  className="absolute right-[18px] top-5 z-10"
+                  priority
+                />
+                <Image
+                  src={bagImage}
+                  alt="lost bag"
+                  width={57.69}
+                  height={42.18}
+                  className="absolute bottom-5 right-[14px]"
+                  priority
+                />
+              </div>
+            </Link>
+          );
+        }
+      )}
+    </div>
+  );
+};
+
+export default LostFindActions;
