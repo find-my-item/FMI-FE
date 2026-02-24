4196f82823716c6f6116d114f5129647f67a7fb1
jun <znznun@gmail.com>
2026-02-24 12:58:40 +0900

    design(main): SearchLoading role추가

--- src/app/(home)/_components/PostSheetContent/_internal/SearchLoading.tsx
+++ src/app/(home)/_components/PostSheetContent/_internal/SearchLoading.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@/components/common";
 
 const SearchLoading = () => {
   return (
-    <div className="mt-[137px] flex-center">
+    <div className="mt-[137px] flex-center" role="status">
       <Icon name="Loading" className="animate-spin" size={30} />
     </div>
   );
