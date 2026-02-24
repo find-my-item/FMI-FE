ceaab85368b9b4b64ee95b3d70987382cdc29fc7
jun <znznun@gmail.com>
2026-02-24 20:21:39 +0900

    fix(main): MainSearchLayout 조건식 정리

--- src/app/(home)/_components/MainSearchLayout/MainSearchLayout.tsx
+++ src/app/(home)/_components/MainSearchLayout/MainSearchLayout.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { ReactNode } from "react";
 import { cn } from "@/utils";
 
@@ -9,15 +7,13 @@ interface MainSearchLayoutProps {
 }
 
 const MainSearchLayout = ({ focused, children }: MainSearchLayoutProps) => {
-  const isDropdownOpen = focused;
-
   return (
     <div
       className={cn(
-        isDropdownOpen &&
-          "fixed inset-x-0 bottom-0 top-0 z-[9999] mx-auto max-w-[768px] bg-white px-5 py-3",
-        !focused &&
-          "fixed left-1/2 top-0 z-10 w-full max-w-[768px] -translate-x-1/2 bg-transparent px-5 py-[10px]"
+        "fixed top-0",
+        focused
+          ? "inset-x-0 bottom-0 z-[9999] mx-auto max-w-[768px] bg-white px-5 py-3"
+          : "left-1/2 z-10 w-full max-w-[768px] -translate-x-1/2 bg-transparent px-5 py-[10px]"
       )}
     >
       {children}
