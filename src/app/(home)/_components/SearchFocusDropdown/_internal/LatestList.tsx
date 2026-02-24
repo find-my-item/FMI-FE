7691bdd31d8831b8c04286418a165c36e340a623
jun <znznun@gmail.com>
2026-02-24 17:29:42 +0900

    fix(searchFocusDropdown): 내부 요소 tabIndex, button태그 변경

--- src/app/(home)/_components/SearchFocusDropdown/_internal/LatestList.tsx
+++ src/app/(home)/_components/SearchFocusDropdown/_internal/LatestList.tsx
@@ -5,28 +5,30 @@ const LatestList = () => {
   return (
     <ul>
       {Array.from({ length: 5 }).map((_, index) => (
-        <li
-          key={index}
-          className={cn(
-            "flex w-full cursor-pointer items-center justify-between gap-3 border-b border-labelsVibrant-quaternary py-4 transition-colors",
-            "[&:is(:hover,:focus)]:bg-fill-neutral-strong-default"
-          )}
-          tabIndex={0}
-        >
-          <div className="flex min-w-0 items-center gap-3">
-            <div className="h-[32px] w-[32px] flex-shrink-0 rounded-full bg-fill-neutral-strong-default flex-center">
-              <Icon name="Clock" size={20} />
+        <li key={index}>
+          <div
+            tabIndex={0}
+            aria-label="최근 검색어 클릭"
+            className={cn(
+              "flex w-full cursor-pointer items-center justify-between gap-3 border-b border-labelsVibrant-quaternary py-4 transition-colors",
+              "[&:is(:hover,:focus)]:bg-fill-neutral-strong-default"
+            )}
+          >
+            <div className="flex min-w-0 items-center gap-3">
+              <div className="h-[32px] w-[32px] flex-shrink-0 rounded-full bg-fill-neutral-strong-default flex-center">
+                <Icon name="Clock" size={20} />
+              </div>
+              <p className="truncate text-body1-regular text-labelsVibrant-primary">
+                최근 검색어 목록 최근 검색어 목록 최근 검색어 목록 최근 검색어 목록 최근 검색어 목록
+                최근 검색어 목록 최근 검색어 목록 최근 검색어 목록
+              </p>
             </div>
-            <p className="truncate text-body1-regular text-labelsVibrant-primary">
-              최근 검색어 목록 최근 검색어 목록 최근 검색어 목록 최근 검색어 목록 최근 검색어 목록
-              최근 검색어 목록 최근 검색어 목록 최근 검색어 목록
-            </p>
-          </div>
-          <div className="flex items-center gap-[9px]">
-            <time className="text-body1-regular text-labelsVibrant-primary">01.01</time>
-            <button type="button" aria-label="최근 검색어 삭제">
-              <Icon name="XSecond" size={20} />
-            </button>
+            <div className="flex items-center gap-[9px]">
+              <time className="text-body1-regular text-labelsVibrant-primary">01.01</time>
+              <button type="button" aria-label="최근 검색어 삭제">
+                <Icon name="XSecond" size={20} />
+              </button>
+            </div>
           </div>
         </li>
       ))}
