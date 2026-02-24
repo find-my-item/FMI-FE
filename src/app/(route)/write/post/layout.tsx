17d2e89de55f4332f8f833fc7d1840004ccf6961
wlrnjs <wlrnjs5824@gmail.com>
2026-02-25 01:54:30 +0900

    feat(write): 임시저장 모달 추가

--- src/app/(route)/write/post/layout.tsx
+++ src/app/(route)/write/post/layout.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from "react";
 import { PostWriteFormProvider } from "./location/_components";
 
-// TODO(지권): 지역 선택 페이지에서 글쓰기 페이지 이동 시 모달 표시 제한
-// TODO(지권): 글쓰기 페이지 우상단 임시저장 버튼 클릭 시 모달 (기획 미정)
+// TODO(지권): 글쓰기 페이지 우상단 임시저장 버튼 클릭 시 모달 (기획 미확정)
 // TODO(지권): 이미지 미등록 모달 표시
 // TODO(지권): 지도 페이지 디자인 변경사항 반영 (위치 검색, 지도 범위 선택 디자인)
 
