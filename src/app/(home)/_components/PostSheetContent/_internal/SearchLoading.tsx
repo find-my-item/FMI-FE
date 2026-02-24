e42401953487b7e58b817a3b0e9d2c0b2f173f30
jun <znznun@gmail.com>
2026-02-24 13:01:37 +0900

    design(main): SearchLoading mt제거, h-full 추가

--- src/app/(home)/_components/PostSheetContent/_internal/SearchLoading.tsx
+++ src/app/(home)/_components/PostSheetContent/_internal/SearchLoading.tsx
@@ -2,7 +2,7 @@ import { Icon } from "@/components/common";
 
 const SearchLoading = () => {
   return (
-    <div className="mt-[137px] flex-center" role="status">
+    <div className="h-full flex-center" role="status">
       <Icon name="Loading" className="animate-spin" size={30} />
     </div>
   );
