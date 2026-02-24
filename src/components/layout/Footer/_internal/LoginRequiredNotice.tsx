5118650ad1d07d281da881f7043fb1e8fafbde38
jun <znznun@gmail.com>
2026-02-24 12:41:01 +0900

    design(main): LoginRequiredNotice role 추가

--- src/components/layout/Footer/_internal/LoginRequiredNotice.tsx
+++ src/components/layout/Footer/_internal/LoginRequiredNotice.tsx
@@ -1,6 +1,9 @@
 const LoginRequiredNotice = () => {
   return (
-    <div className="animate-float absolute left-1/2 top-[-42px] z-[9999] -translate-x-1/2 select-none">
+    <div
+      role="alert"
+      className="animate-float absolute left-1/2 top-[-42px] z-[9999] -translate-x-1/2 select-none"
+    >
       <div className="relative rounded-2xl p-[10px] shadow-sm bg-fill-brand-subtle-default_2">
         <p className="whitespace-nowrap text-caption2-semibold text-brand-strongUseThis-hover">
           로그인 후 이용하실 수 있어요
