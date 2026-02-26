035a7c92e1a8bfd43c58f009619074bce6c1365a
jun <znznun@gmail.com>
2026-02-26 18:31:21 +0900

    refactor(alertEmpty): 공통 컴포넌트 Emptystate로 대체

--- src/app/(route)/alert/_components/index.ts
+++ src/app/(route)/alert/_components/index.ts
@@ -1,3 +1,2 @@
 export { default as AlertCategory } from "./AlertCategory/AlertCategory";
 export { default as AlertView } from "./AlertView/AlertView";
-export { default as AlertEmpty } from "./AlertEmpty/AlertEmpty";
