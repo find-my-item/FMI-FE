7770e7aa4adf7c55054d1e5cdf4a7976f47d5cbe
jun <znznun@gmail.com>
2026-02-26 18:13:38 +0900

    design(alert): 알림 empty 퍼블리싱

--- src/app/(route)/alert/_components/index.ts
+++ src/app/(route)/alert/_components/index.ts
@@ -1,2 +1,3 @@
 export { default as AlertCategory } from "./AlertCategory/AlertCategory";
 export { default as AlertView } from "./AlertView/AlertView";
+export { default as AlertEmpty } from "./AlertEmpty/AlertEmpty";
