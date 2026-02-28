7d51f879070e113e367ae973d40f56aa90dc4095
jun <znznun@gmail.com>
2026-02-28 21:20:40 +0900

    refactor(notice): 필터 별도 컴포넌트로 분리

--- src/app/(route)/notice/_components/NoticeView/NoticeView.tsx
+++ src/app/(route)/notice/_components/NoticeView/NoticeView.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import NoticeList from "../NoticeList/NoticeList";
+import { InputSearch } from "@/components/common";
+import NoticeFilter from "../NoticeFilter/NoticeFilter";
 
 const NoticeView = () => {
-  return <NoticeList />;
+  return (
+    <>
+      <div className="px-5 py-[10px]">
+        <InputSearch
+          name="noticeSearch"
+          mode="onChange"
+          onEnter={() => {}}
+          placeholder="제목, 내용을 입력해 주세요."
+        />
+      </div>
+      <NoticeFilter />
+      <NoticeList />
+    </>
+  );
 };
 
 export default NoticeView;
--- src/app/(route)/notice/_components/index.ts
+++ src/app/(route)/notice/_components/index.ts
@@ -2,3 +2,4 @@ export { default as NoticeChip } from "./NoticeChip/NoticeChip";
 export { default as NoticeDetailHeader } from "./NoticeDetailHeader/NoticeDetailHeader";
 export { default as NoticeView } from "./NoticeView/NoticeView";
 export { default as NoticeList } from "./NoticeList/NoticeList";
+export { default as NoticeFilter } from "./NoticeFilter/NoticeFilter";
