ea8c2e43dcf144c8d406e8d5e8087aaa3c96bf3b
jun <znznun@gmail.com>
2026-03-03 03:17:31 +0900

    design(notice): 공지사항 상세 메뉴 토글 퍼블리싱

--- src/app/(route)/notice/[id]/_components/NoticeDetailHeader/NoticeDetailHeader.tsx
+++ src/app/(route)/notice/[id]/_components/NoticeDetailHeader/NoticeDetailHeader.tsx
@@ -1,12 +1,38 @@
+"use client";
+
+import { KebabMenu } from "@/components/common";
 import { DetailHeader } from "@/components/layout";
-import { HeaderShare, HeaderStar } from "@/components/layout/DetailHeader/DetailHeaderParts";
+import {
+  HeaderMenu,
+  HeaderShare,
+  HeaderStar,
+} from "@/components/layout/DetailHeader/DetailHeaderParts";
+import { useClickOutside } from "@/hooks";
+import { useState } from "react";
 
 const NoticeDetailHeader = () => {
+  const [isKebabMenuOpen, setIsKebabMenuOpen] = useState(false);
+  const kebabMenuRef = useClickOutside(() => setIsKebabMenuOpen(false));
+
   return (
-    <DetailHeader>
-      <HeaderStar isActive={false} />
-      <HeaderShare />
-    </DetailHeader>
+    <div className="relative" ref={kebabMenuRef}>
+      <DetailHeader>
+        <HeaderStar isActive={false} />
+        <HeaderShare />
+        <HeaderMenu onClick={() => setIsKebabMenuOpen((prev) => !prev)} />
+      </DetailHeader>
+
+      {isKebabMenuOpen && (
+        <div className="absolute right-5 top-[54px] z-40">
+          <KebabMenu
+            items={[
+              { text: "게시글 수정하기", icon: { name: "Edit" }, onClick: () => {} },
+              { text: "게시글 삭제하기", icon: { name: "Trash" }, onClick: () => {} },
+            ]}
+          />
+        </div>
+      )}
+    </div>
   );
 };
 
