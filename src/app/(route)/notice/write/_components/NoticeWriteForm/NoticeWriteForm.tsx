4db8b8304f09775efe608557a3ee5832562cb398
jun <znznun@gmail.com>
2026-03-03 03:08:17 +0900

    design(notice): 글 작성 버튼 퍼블리싱

--- src/app/(route)/notice/write/_components/NoticeWriteForm/NoticeWriteForm.tsx
+++ src/app/(route)/notice/write/_components/NoticeWriteForm/NoticeWriteForm.tsx
@@ -2,6 +2,7 @@ import { UseFormReturn } from "react-hook-form";
 import TitleInput from "./_internal/TitleInput";
 import ContentInput from "./_internal/ContentInput";
 import ImageInput from "./_internal/ImageInput";
+import { ActionSection } from "@/app/(route)/write/post/_components/_internal";
 
 const NoticeWriteForm = ({ methods }: { methods: UseFormReturn }) => {
   return (
@@ -9,6 +10,9 @@ const NoticeWriteForm = ({ methods }: { methods: UseFormReturn }) => {
       <TitleInput />
       <ContentInput />
       <ImageInput />
+      <div className="fixed bottom-0 w-full max-w-[764px] border-t border-divider-default bg-white">
+        <ActionSection disabled={false} />
+      </div>
     </form>
   );
 };
