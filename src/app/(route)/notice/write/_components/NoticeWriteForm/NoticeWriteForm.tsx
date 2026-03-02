7a3336f98bff93db69b34937f6e0444ef755810f
jun <znznun@gmail.com>
2026-03-03 03:01:49 +0900

    design(notice): 공지사항 글쓰기 이미지 input 퍼블리싱

--- src/app/(route)/notice/write/_components/NoticeWriteForm/NoticeWriteForm.tsx
+++ src/app/(route)/notice/write/_components/NoticeWriteForm/NoticeWriteForm.tsx
@@ -1,12 +1,14 @@
 import { UseFormReturn } from "react-hook-form";
 import TitleInput from "./_internal/TitleInput";
 import ContentInput from "./_internal/ContentInput";
+import ImageInput from "./_internal/ImageInput";
 
 const NoticeWriteForm = ({ methods }: { methods: UseFormReturn }) => {
   return (
     <form onSubmit={methods.handleSubmit(() => {})}>
       <TitleInput />
       <ContentInput />
+      <ImageInput />
     </form>
   );
 };
---
+++ src/app/(route)/notice/write/_components/NoticeWriteForm/_internal/ImageInput.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useRef } from "react";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import { Icon } from "@/components/common";
+import { ImagePreviewList } from "@/app/(route)/write/post/_components/_internal";
+import { useToast } from "@/context/ToastContext";
+import { PostWriteFormValues } from "@/app/(route)/write/post/_types/PostWriteType";
+
+const ImageInput = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { addToast } = useToast();
+
+  const { control } = useFormContext<PostWriteFormValues>();
+
+  const { fields, append, remove, move } = useFieldArray({ control, name: "images" });
+
+  const openImagePicker = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleImageChange = () => {
+    const files = fileInputRef.current?.files;
+    if (!files) return;
+
+    const fileArray = Array.from(files).filter((file) => file.type.startsWith("image/"));
+
+    const remainCount = 5 - fields.length;
+
+    if (remainCount <= 0 || fileArray.length > remainCount) {
+      addToast("이미지는 최대 5장만 첨부할 수 있어요.", "warning");
+      return;
+    }
+
+    fileArray.slice(0, remainCount).forEach((file) => {
+      append({
+        file,
+        previewUrl: URL.createObjectURL(file),
+      });
+    });
+  };
+
+  return (
+    <section className="flex flex-col items-start justify-center gap-4 px-5 py-6">
+      <input
+        type="file"
+        accept="image/png, image/jpeg, image/jpg"
+        className="hidden"
+        multiple
+        ref={fileInputRef}
+        onChange={handleImageChange}
+      />
+
+      <div className="hide-scrollbar flex flex-nowrap items-center gap-4 overflow-x-scroll">
+        <button
+          type="button"
+          aria-label="이미지 업로드"
+          onClick={openImagePicker}
+          className="size-[104px] shrink-0 rounded-[6px] bg-flatGray-25 flex-col-center"
+        >
+          <Icon name="Camera" size={32} className="text-neutralInversed-strong-default" />
+          <span className="select-none text-caption1-regular text-flatGray-400">
+            ({fields.length}/5)
+          </span>
+        </button>
+        <ImagePreviewList images={fields} onRemove={remove} onMove={move} />
+      </div>
+      <span className="text-caption1-regular text-neutral-normal-placeholder">
+        *사진은 최대 5장 첨부가 가능합니다. (선택)
+      </span>
+    </section>
+  );
+};
+
+export default ImageInput;
