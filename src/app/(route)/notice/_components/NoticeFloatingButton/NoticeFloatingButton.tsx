1f0fa615eda6cc4421f74a9928f3a43316b3db2a
jun <znznun@gmail.com>
2026-03-04 02:37:07 +0900

    refactor(notice): 공지사항 작성 관리자 내부 폴더로 이동

--- src/app/(route)/notice/_components/NoticeFloatingButton/NoticeFloatingButton.tsx
+++ src/app/(route)/notice/_components/NoticeFloatingButton/NoticeFloatingButton.tsx
@@ -6,8 +6,8 @@ const NoticeFloatingButton = () => {
   return (
     <div className="fixed-button-position">
       <Link
-        href="/notice/write"
-        aria-label="공지사항 글쓰기 페이지 이동"
+        href="/admin/notice/write"
+        aria-label="공지사항 관리자 글쓰기 페이지 이동"
         className={cn(
           "h-[70px] w-[70px] rounded-full p-3 transition-colors duration-150 flex-center",
           "glass-card bg-opacity-70 bg-fill-brand-normal-default",
