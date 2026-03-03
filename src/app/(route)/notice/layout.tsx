03ab9353c2b282d9c3cadd630ddf81a4bbc6b7cd
jun <znznun@gmail.com>
2026-03-04 02:26:03 +0900

    fix(notice): 공지사항 리스트 layout의 DetailHeader, 플로팅 버튼 page 내부로 이동

--- src/app/(route)/notice/layout.tsx
+++ src/app/(route)/notice/layout.tsx
@@ -1,16 +1,7 @@
-import { DetailHeader } from "@/components/layout";
 import { ReactNode } from "react";
-import { NoticeFloatingButton } from "./_components";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="h-base">
-      <DetailHeader title="공지사항" />
-      <h1 className="sr-only">공지사항 목록</h1>
-      {children}
-      <NoticeFloatingButton />
-    </div>
-  );
+  return <div className="h-base">{children}</div>;
 };
 
 export default Layout;
--- src/app/(route)/notice/page.tsx
+++ src/app/(route)/notice/page.tsx
@@ -1,7 +1,15 @@
-import { NoticeView } from "./_components";
+import { DetailHeader } from "@/components/layout";
+import { NoticeFloatingButton, NoticeView } from "./_components";
 
 const Notice = () => {
-  return <NoticeView />;
+  return (
+    <>
+      <DetailHeader title="공지사항" />
+      <h1 className="sr-only">공지사항 목록</h1>
+      <NoticeView />
+      <NoticeFloatingButton />
+    </>
+  );
 };
 
 export default Notice;
