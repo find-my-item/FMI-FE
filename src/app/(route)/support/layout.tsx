094fc8a1878ee38568f309dfd5e88391f706fc6b
jun <znznun@gmail.com>
2026-03-05 04:33:49 +0900

    design(support): 1:1 문의하기 플로팅 버튼 구현

--- src/app/(route)/support/layout.tsx
+++ src/app/(route)/support/layout.tsx
@@ -1,11 +1,14 @@
 import { DetailHeader } from "@/components/layout";
 import { ReactNode } from "react";
+import { FloatingInquiryButton } from "./_components";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <DetailHeader title="자주 묻는 질문 (FAQ)" />
+      <h1 className="sr-only">자주 묻는 질문 (FAQ)</h1>
       {children}
+      <FloatingInquiryButton />
     </>
   );
 };
