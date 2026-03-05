06e2360347cfb1fd425a3419d99e22191829b096
jun <znznun@gmail.com>
2026-03-06 05:17:31 +0900

    feat(support): Suspense 적용(임시)

--- src/app/(route)/support/page.tsx
+++ src/app/(route)/support/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import { SupportFaqAccordion, SupportSearchForm, SupportTab } from "./_components";
 
 const page = () => {
   return (
     <div className="space-y-[10px] h-base">
-      <SupportSearchForm />
-      <SupportTab />
-      <SupportFaqAccordion />
+      {/* TODO(형준): 기능 개발 시점에서 Suspense 세분화 필요 */}
+      <Suspense fallback="">
+        <SupportSearchForm />
+        <SupportTab />
+        <SupportFaqAccordion />
+      </Suspense>
     </div>
   );
 };
