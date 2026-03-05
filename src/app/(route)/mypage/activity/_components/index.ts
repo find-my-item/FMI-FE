42bd752a482b3554e92dab019ba97f5b5678a216
suhyeon0111 <soo4833@naver.com>
2026-03-05 19:37:10 +0900

    chore(activity): 바텀 시트 삭제

--- src/app/(route)/mypage/activity/_components/index.ts
+++ src/app/(route)/mypage/activity/_components/index.ts
@@ -1,2 +1,1 @@
 export { default as ActivityFilterSection } from "./ActivityFilterSection/ActivityFilterSection";
-export { default as ActivityBottomSheet } from "./ActivityBottomSheet/ActivityBottomSheet";
