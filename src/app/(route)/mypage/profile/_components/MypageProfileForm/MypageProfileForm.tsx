98adee48b37db6bdd031e4e8ffd355811fc1037d
suhyeon0111 <soo4833@naver.com>
2026-03-10 02:39:19 +0900

    design(mypage): 퍼블리싱 수정

--- src/app/(route)/mypage/profile/_components/MypageProfileForm/MypageProfileForm.tsx
+++ src/app/(route)/mypage/profile/_components/MypageProfileForm/MypageProfileForm.tsx
@@ -25,6 +25,7 @@ const MypageProfileForm = ({ user }: MypageProfileFormProps) => {
   // 모달 제어
   const [openModal, setOpenModal] = useState(false);
 
+  // 이미지 관련 처리
   const { handleChangeImg, handleButtonClick, previewImgUrl, resetImage, fileInputRef } =
     useChangeImg({
       setOpenMenu,
@@ -32,6 +33,7 @@ const MypageProfileForm = ({ user }: MypageProfileFormProps) => {
       onImageChange: (file) => setValue("profileImg", file, { shouldDirty: true }),
     });
 
+  // 최종 버튼 제출 함수
   const { handleSubmitMypageProfile } = useProfileFormSubmit({
     preNickname: nickname,
     preProfileImg: profileImg,
@@ -40,73 +42,71 @@ const MypageProfileForm = ({ user }: MypageProfileFormProps) => {
 
   return (
     <form className="flex w-full flex-col h-base">
-      <div className="flex justify-center py-[30px]">
-        <div className="relative h-[80px] w-[80px]">
-          <ProfileAvatar size={80} src={previewImgUrl} alt="프로필" priority={true} />
-          {/* TODO(수현): 디자인 토큰 변경 요청 해놓은 상태로 등록 시 추후 변경 */}
-          <button
-            className="absolute left-[52px] top-[52px] h-[28px] w-[28px] rounded-full bg-fill-neutral-strong-default flex-center"
-            aria-label="프로필 이미지 변경 버튼"
-            onClick={() => setOpenMenu((prev) => !prev)}
-            type="button"
-          >
-            <Icon name="CameraBorder" size={16} />
-          </button>
+      <div className="flex-1">
+        <div className="flex justify-center py-[30px]">
+          <div className="relative h-[80px] w-[80px]">
+            <ProfileAvatar size={80} src={previewImgUrl} alt="프로필" priority={true} />
+            {/* TODO(수현): 디자인 토큰 변경 요청 해놓은 상태로 등록 시 추후 변경 */}
+            <button
+              className="absolute left-[52px] top-[52px] h-[28px] w-[28px] rounded-full bg-fill-neutral-strong-default flex-center"
+              aria-label="프로필 이미지 변경 버튼"
+              onClick={() => setOpenMenu((prev) => !prev)}
+              type="button"
+            >
+              <Icon name="CameraBorder" size={16} />
+            </button>
 
-          {/* 메뉴 */}
-          {openMenu && (
-            <KebabMenu
-              items={[
-                { text: "내 앨범에서 선택", onClick: handleButtonClick, type: "button" },
-                {
-                  text: "프로필 이미지 삭제",
-                  textColor: "text-system-warning",
-                  onClick: resetImage,
-                  type: "button",
-                },
-              ]}
+            {/* 메뉴 */}
+            {openMenu && (
+              <KebabMenu
+                items={[
+                  { text: "내 앨범에서 선택", onClick: handleButtonClick, type: "button" },
+                  {
+                    text: "프로필 이미지 삭제",
+                    textColor: "text-system-warning",
+                    onClick: resetImage,
+                    type: "button",
+                  },
+                ]}
+              />
+            )}
+
+            <input
+              type="file"
+              accept="image/png, image/jpeg, image/jpg"
+              className="hidden"
+              ref={fileInputRef}
+              onChange={handleChangeImg}
             />
-          )}
+          </div>
+        </div>
 
-          <input
-            type="file"
-            accept="image/png, image/jpeg, image/jpg"
-            className="hidden"
-            ref={fileInputRef}
-            onChange={handleChangeImg}
+        {/* 닉네임 input */}
+        <div className="flex w-full flex-col gap-5 p-5">
+          <InputText
+            inputOption={{
+              name: "nickname",
+              // TODO(수현): 기존 닉네임이 placeholder로 들어갈 예정
+              placeholder: nickname,
+              maxLength: 10,
+              validation: {
+                required: true,
+                maxLength: 10,
+              },
+            }}
+            label="닉네임"
+            btnOption={{
+              btnLabel: "중복 확인",
+              onClick: () => {
+                handleClickNickname("nickname");
+              },
+            }}
+            caption={{ rule: "2~10자, 특수문자/금칙어 제한" }}
           />
         </div>
-      </div>
 
-      {/* 닉네임 input */}
-      <div className="flex w-full flex-col gap-5 p-5">
-        <InputText
-          inputOption={{
-            name: "nickname",
-            // TODO(수현): 기존 닉네임이 placeholder로 들어갈 예정
-            placeholder: nickname,
-            maxLength: 10,
-            validation: {
-              required: true,
-              maxLength: 10,
-            },
-          }}
-          label="닉네임"
-          btnOption={{
-            btnLabel: "중복 확인",
-            onClick: () => {
-              handleClickNickname("nickname");
-            },
-          }}
-          caption={{ rule: "2~10자, 특수문자/금칙어 제한" }}
-        />
+        <MypageProfileModal isOpen={openModal} onClose={() => setOpenModal(false)} />
       </div>
-
-      {/* TODO(수현): 삭제할 버튼 */}
-      <Button type="button" onClick={() => setOpenModal(true)}>
-        테스트 버튼
-      </Button>
-      <MypageProfileModal isOpen={openModal} onClose={() => setOpenModal(false)} />
 
       <FooterButton
         type="button"
