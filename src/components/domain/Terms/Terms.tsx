bf5e3a8584f1049b2c3e5d4dccfdce4c2268283a
suhyeon0111 <soo4833@naver.com>
2026-03-11 02:18:02 +0900

    refactor(sign-up): 상세 약관 선택 함수 부모로 이동

---
+++ src/components/domain/Terms/Terms.tsx
@@ -0,0 +1,30 @@
+import { DetailHeader } from "@/components/layout";
+import { AGREE_CONFIG } from "../../../app/(route)/sign-up/_constants/AGREE_CONFIG";
+import { FooterButton } from "@/components/domain";
+
+interface TermsProps {
+  termName: string;
+  onAgree?: () => void;
+  showButton: boolean;
+}
+
+const Terms = ({ termName, onAgree, showButton }: TermsProps) => {
+  const term = AGREE_CONFIG[termName as keyof typeof AGREE_CONFIG];
+
+  if (!term) {
+    return null;
+  }
+
+  return (
+    <>
+      <DetailHeader title={term.title} />
+      <div className="whitespace-pre-wrap px-4 pb-[calc(88px+24px)] pt-6 text-body2-regular text-layout-body-default h-base">
+        {term.content}
+      </div>
+
+      {showButton && <FooterButton onClick={onAgree}>동의</FooterButton>}
+    </>
+  );
+};
+
+export default Terms;
