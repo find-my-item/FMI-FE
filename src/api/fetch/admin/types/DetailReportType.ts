867ee7a408858b9ad819a8cfd0cd24acc495273f
wlrnjs <wlrnjs5824@gmail.com>
2026-03-10 14:43:42 +0900

    feat(admin-reports): 상세페이지 API 분리 및 useReportsDetailQuery 구현

---
+++ src/api/fetch/admin/types/DetailReportType.ts
@@ -0,0 +1,16 @@
+import { ApiBaseResponseType } from "@/api/_base/types/ApiBaseResponseType";
+import { ReportTargetType, ReportType, ReportStatus } from "@/types";
+
+export interface GetDetailReportResponse extends ApiBaseResponseType<AdminDetailReport> {}
+
+export interface AdminDetailReport {
+  reportId: number;
+  targetType: ReportTargetType;
+  targetId: number;
+  reportType: ReportType;
+  reason: string;
+  status: ReportStatus;
+  adminNote: string;
+  createdAt: string;
+  resolvedAt: string;
+}
