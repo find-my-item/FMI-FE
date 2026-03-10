c4ca55613ae2e3f7739b44a800ee5adeb5c5733a
jun <znznun@gmail.com>
2026-03-10 16:16:44 +0900

    feat(noticeDetail): 공지사항 상세 API 연동

--- src/app/(route)/notice/[id]/_components/NoticeDetailView/_internal/NoticeDetailContent.tsx
+++ src/app/(route)/notice/[id]/_components/NoticeDetailView/_internal/NoticeDetailContent.tsx
@@ -1,4 +1,7 @@
+import { NoticeDetail } from "@/api/fetch/notice";
 import { Icon } from "@/components/common";
+import { formatDate } from "@/utils";
+import Image from "next/image";
 
 const NoticeNewBadge = () => {
   return (
@@ -8,36 +11,45 @@ const NoticeNewBadge = () => {
   );
 };
 
-const NoticeDetailContent = () => {
+const NoticeDetailContent = ({ noticeDetail }: { noticeDetail?: NoticeDetail }) => {
+  if (!noticeDetail) return null;
+  const { title, content, viewCount, likeCount, authorName, isNew, createdAt, images } =
+    noticeDetail;
+
   return (
     <section className="space-y-3 px-5 py-[30px]">
-      <NoticeNewBadge />
+      {isNew && <NoticeNewBadge />}
 
       <div className="space-y-6">
         <div className="space-y-1">
-          <h1 className="text-h2-bold text-layout-header-default">
-            여기에 공지사항 제목이 들어갑니다.
-          </h1>
+          <h1 className="text-h2-bold text-layout-header-default">{title}</h1>
           <div className="text-body2-regular text-layout-body-default">
-            <time className="after:mx-2 after:content-['·']">2025.10.15</time>
-            <span>관리자</span>
+            <time className="after:mx-2 after:content-['·']">{formatDate(createdAt)}</time>
+            <span>{authorName}</span>
           </div>
         </div>
-
-        <p className="text-body1-regular text-layout-header-default">
-          여기에 공지사항 내용이 들어갑니다. 이 글은 텍스트가 들어가는 느낌을 확인하기 위해 임의로
-          작성하는 글입니다. 여기에 공지사항 내용이 들어갑니다. 이 글은 텍스트가 들어가는 느낌을
-          확인하기 위해 임의로 작성하는 글입니다.
-        </p>
+        <p className="text-body1-regular text-layout-header-default">{content}</p>
+        {/* TODO(형준): 이미지 임시 스타일 적용 상태, 피그마 디자인 추가 시 변경 필요 */}
+        {images &&
+          images.map((image) => (
+            <div key={image} className="relative aspect-square w-full">
+              <Image
+                src={image}
+                alt="공지사항 상세 이미지"
+                fill
+                className="rounded-2xl object-contain"
+              />
+            </div>
+          ))}
 
         <div className="flex gap-3 text-body2-regular text-neutral-strong-placeholder">
           <button aria-label="좋아요 버튼" className="flex items-center gap-1">
             <Icon name="Like" size={16} className="text-border-divider-default" />
-            <span>추천 12</span>
+            <span>추천 {likeCount}</span>
           </button>
           <div className="flex items-center gap-1">
             <Icon name="Eye" size={16} className="text-border-divider-default" />
-            <span>조회 24</span>
+            <span>조회 {viewCount}</span>
           </div>
         </div>
       </div>
