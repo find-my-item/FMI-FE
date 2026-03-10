0f638bfd19505bf3e8d721f879ef853210707d27
suhyeon0111 <soo4833@naver.com>
2026-03-11 02:40:53 +0900

    fix(terms): 빌드에러 수정

--- src/app/(route)/terms/[termName]/page.tsx
+++ src/app/(route)/terms/[termName]/page.tsx
@@ -1,28 +1,32 @@
 import { Terms } from "@/components/domain";
 import { ErrorView } from "@/components/state";
 
-export default async function Page({ params }: { params: { termName: string } }) {
+interface PageProps {
+  params: Promise<{ termName: string }>;
+}
+
+export default async function Page({ params }: PageProps) {
   const { termName } = await params;
 
   if (termName === "privacy") {
     return <Terms termName="privacyPolicyAgreed" />;
   }
 
-  if (termName === "marketing") {
-    return <Terms termName="marketingConsent" />;
-  } else {
-    return (
-      <ErrorView
-        iconName="NotFound"
-        code="404"
-        title="페이지를 찾을 수 없습니다."
-        description={
-          <>
-            존재하지 않는 주소를 입력했거나 <br />
-            요청하신 페이지를 사용할 수 없습니다.
-          </>
-        }
-      />
-    );
+  if (termName === "marketingConsent") {
+    return <Terms termName="marketingConsentAgreed" />;
   }
+
+  return (
+    <ErrorView
+      iconName="NotFound"
+      code="404"
+      title="페이지를 찾을 수 없습니다."
+      description={
+        <>
+          존재하지 않는 주소를 입력했거나 <br />
+          요청하신 페이지를 사용할 수 없습니다.
+        </>
+      }
+    />
+  );
 }
