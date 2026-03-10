bf0e6933d367fbcff85e86ee4ef84f5fe905d534
jun <znznun@gmail.com>
2026-03-10 15:46:00 +0900

    feat(noticeDetail): 공지사항 상세 패칭훅 자것ㅇ

--- src/api/fetch/notice/index.ts
+++ src/api/fetch/notice/index.ts
@@ -2,3 +2,4 @@ export * from "./types/NoticesType";
 export * from "./types/NoticeDetailType";
 
 export { useGetNotices } from "./api/useGetNotices";
+export { useGetNoticeDetail } from "./api/useGetNoticeDetail";
