337535ff70177ce713736ac6d73ad600d8eada90
suhyeon0111 <soo4833@naver.com>
2026-03-12 01:01:47 +0900

    refactor(terms): NotFound로 대체

--- src/app/(route)/terms/[termName]/page.tsx
+++ src/app/(route)/terms/[termName]/page.tsx
@@ -1,3 +1,4 @@
+import NotFound from "@/app/not-found";
 import { Terms } from "@/components/domain";
 import { ErrorView } from "@/components/state";
 
@@ -14,19 +15,5 @@ export default async function Page({ params }: PageProps) {
 
   if (termName === "marketingConsent") {
     return <Terms termName="marketingConsentAgreed" />;
-  }
-
-  return (
-    <ErrorView
-      iconName="NotFound"
-      code="404"
-      title="페이지를 찾을 수 없습니다."
-      description={
-        <>
-          존재하지 않는 주소를 입력했거나 <br />
-          요청하신 페이지를 사용할 수 없습니다.
-        </>
-      }
-    />
-  );
+  } else return <NotFound />;
 }
