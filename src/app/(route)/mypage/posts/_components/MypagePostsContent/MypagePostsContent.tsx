f33e6b20f46f13ba2895b301f49a49acb00f3c69
suhyeon0111 <soo4833@naver.com>
2026-03-15 01:32:41 +0900

    refactor(posts): suspense, errorBoundary 제거 - isLoading, isError로 분기처리

--- src/app/(route)/mypage/posts/_components/MypagePostsContent/MypagePostsContent.tsx
+++ src/app/(route)/mypage/posts/_components/MypagePostsContent/MypagePostsContent.tsx
@@ -7,12 +7,17 @@ import {
   StatusFilterValue,
 } from "@/components/domain/FilterSectionBottomSheet/_types/types";
 import MypagePostsList from "../MypagePostsList/MypagePostsList";
+import { LoadingState } from "@/components/state";
+import { useToast } from "@/context/ToastContext";
 
 const MypagePostsContent = () => {
   const { status, findStatus, category, sort, startDate, endDate } = useFilterParams();
+  const { addToast } = useToast();
 
   const {
     data: PostsData,
+    isLoading,
+    isError,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
@@ -24,6 +29,9 @@ const MypagePostsContent = () => {
     endDate,
     sortType: sort ?? "LATEST",
   });
+
+  if (isLoading) return <LoadingState />;
+  if (isError) addToast("목록을 불러오는데 실패했어요", "error");
 
   return (
     <>
--- src/app/(route)/mypage/posts/page.tsx
+++ src/app/(route)/mypage/posts/page.tsx
@@ -3,9 +3,6 @@
 import { DetailHeader } from "@/components/layout";
 import { MypagePostsContent } from "./_components";
 import { FilterSection, MypageSearch } from "@/components/domain";
-import { ErrorBoundary } from "@/app/ErrorBoundary";
-import { LoadingState } from "@/components/state";
-import { Suspense } from "react";
 
 const page = () => {
   return (
@@ -17,11 +14,7 @@ const page = () => {
 
         <FilterSection pageType="MY_POSTS" />
 
-        <ErrorBoundary toastMessage="목록을 불러올 수 없어요. 다시 시도해 주세요.">
-          <Suspense fallback={<LoadingState />}>
-            <MypagePostsContent />
-          </Suspense>
-        </ErrorBoundary>
+        <MypagePostsContent />
       </div>
     </>
   );
