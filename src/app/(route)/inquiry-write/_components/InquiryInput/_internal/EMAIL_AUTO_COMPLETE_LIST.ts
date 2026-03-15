32fd26042fd16924adf743c2808e9cc3caabe04d
jun <znznun@gmail.com>
2026-03-15 14:52:43 +0900

    fix(inquiryWrite): 자동완성 이메일 리스트 수정

--- src/app/(route)/inquiry-write/_components/InquiryInput/_internal/EMAIL_AUTO_COMPLETE_LIST.ts
+++ src/app/(route)/inquiry-write/_components/InquiryInput/_internal/EMAIL_AUTO_COMPLETE_LIST.ts
@@ -1,12 +1,8 @@
 export const EMAIL_AUTO_COMPLETE_LIST = [
   "naver.com",
-  "gmail.com",
   "daum.net",
   "kakao.com",
+  "gmail.com",
   "hanmail.net",
-  "nate.com",
-  "outlook.com",
-  "hotmail.com",
   "icloud.com",
-  "yahoo.com",
 ];
