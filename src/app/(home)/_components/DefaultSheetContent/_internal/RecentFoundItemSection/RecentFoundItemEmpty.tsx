7f73eb822294295583655e7848118bd5d51fcf9b
jun <znznun@gmail.com>
2026-03-15 22:40:36 +0900

    design(main): 최근 발견된 분실물 empty 퍼블리싱

---
+++ src/app/(home)/_components/DefaultSheetContent/_internal/RecentFoundItemSection/RecentFoundItemEmpty.tsx
@@ -0,0 +1,12 @@
+import { Icon } from "@/components/common";
+
+const RecentFoundItemEmpty = () => {
+  return (
+    <div className="h-[118px] w-full gap-2 py-[14px] flex-col-center">
+      <Icon name="EmptyPostList" size={90} />
+      <p className="text-body2-medium text-layout-body-default">아직 발견된 분실물이 없어요</p>
+    </div>
+  );
+};
+
+export default RecentFoundItemEmpty;
