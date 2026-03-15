5f5e5bf938ddfd1824622db9084ecc92f6f2732c
jun <znznun@gmail.com>
2026-03-15 21:37:37 +0900

    design(main): 최근 발견된 분실물로 텍스트 수정

--- src/app/(home)/_components/DefaultSheetContent/_internal/RecentFoundItemSection.tsx
+++ src/app/(home)/_components/DefaultSheetContent/_internal/RecentFoundItemSection.tsx
@@ -25,7 +25,7 @@ const RecentFoundItemSection = () => {
     <section className="space-y-2">
       <h2 className="space-x-1 py-2 pl-1 text-h3-semibold">
         <span className="text-brand-normal-default">OO동</span>
-        <span className="text-neutral-strong-hover">습득된 분실물</span>
+        <span className="text-neutral-strong-hover">최근 발견된 분실물</span>
       </h2>
       <div ref={scrollRef} onMouseDown={onMouseDown} className="-mx-5 flex gap-4 px-5 no-scrollbar">
         {Array.from({ length: 10 }).map((_, index) => (
