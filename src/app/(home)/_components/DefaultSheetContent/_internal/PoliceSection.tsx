c675f85d4223eb5cbf9d3163d5a6a845bfc58711
jun <znznun@gmail.com>
2026-03-16 17:05:24 +0900

    design(main): 경찰청 분실/발견 했어요 카드 반응형 추가

--- src/app/(home)/_components/DefaultSheetContent/_internal/PoliceSection.tsx
+++ src/app/(home)/_components/DefaultSheetContent/_internal/PoliceSection.tsx
@@ -19,18 +19,18 @@ const POLICE_ITEMS = [
 const PoliceSection = () => {
   return (
     <section className="space-y-4">
-      <div className="flex items-center justify-between gap-[15px] rounded-2xl px-3 py-4 bg-fill-brand-subtle-default_2">
+      <div className="flex items-center gap-10 rounded-2xl px-3 py-4 bg-fill-brand-subtle-default_2">
         <div className="flex flex-col gap-[10px] px-3 py-[10px]">
           <span className="whitespace-pre text-body2-semibold text-brand-normal-default">{`경찰청 분실물도\n찾아줘!에서 확인해요`}</span>
           <Image src="/main/police24-icon.svg" alt="경찰민원24로고" width={77} height={21} />
         </div>
 
-        <div className="flex items-center gap-3">
+        <div className="flex w-full items-center gap-3">
           {POLICE_ITEMS.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="group box-border h-14 w-[60px] overflow-hidden rounded-2xl border border-brand-normal-disabled bg-white px-[14px] py-3 flex-col-center"
+              className="group box-border h-14 w-full max-w-[255.5px] overflow-hidden rounded-2xl border border-brand-normal-disabled bg-white px-[14px] py-3 flex-col-center"
             >
               <div className="flex flex-col items-center text-center text-caption1-medium transition-colors">
                 <span className="text-neutralInversed-normal-focused group-hover:text-caption1-semibold group-hover:text-flatGreen-500">
