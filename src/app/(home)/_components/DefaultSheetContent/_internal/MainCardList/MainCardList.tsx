bf20efad3b947c17b5fa6f8ec9673087ff3b98c2
jun <znznun@gmail.com>
2026-03-16 16:46:04 +0900

    design(main): 바텀시트 카드 컴포넌트 분리(최근/경찰청 호환)

---
+++ src/app/(home)/_components/DefaultSheetContent/_internal/MainCardList/MainCardList.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Icon } from "@/components/common";
+import { useHorizontalDragScroll } from "@/hooks";
+import Link from "next/link";
+import { MainCardMode } from "./MainCardModeType";
+import PublicChip from "./PublicChip";
+import PublicMoreViewCard from "./PublicMoreViewCard";
+import Image from "next/image";
+
+interface MainCardProps {
+  showChip?: boolean;
+}
+
+const MainCard = ({ showChip = false }: MainCardProps) => {
+  return (
+    <Link href="#" className="relative rounded-2xl border-[0.7px] border-divider-default">
+      <div className="h-[120px] w-[123px] rounded-2xl bg-fill-neutralInversed-normal-preesed">
+        <div className="relative flex h-full w-full justify-center">
+          {/* TODO(형준): 이미지 없을 때 Icon, 있을 때 Image 사용 */}
+          {/* <Image
+            src="https://i.namu.wiki/i/HXXV8jiIlE0t9EoBXsyfG-ltpGEqnc620PzNjD6G_aYP74irttmRysvhQlYX0lEVSWFEt3icEL0RU6R_jE1URg.webp"
+            alt="thumbnail"
+            fill
+            className="rounded-2xl object-cover"
+          /> */}
+          <Icon name="LogoCharacter" size={65} />
+          {showChip && <PublicChip />}
+        </div>
+      </div>
+      <div className="absolute bottom-0 right-0 flex w-full flex-col gap-1 rounded-b-2xl bg-white px-3 py-[6px]">
+        <span className="truncate text-caption1-semibold text-layout-header-default">
+          습득물 제목 습득물 제목 습득물 제목
+        </span>
+        <time className="text-caption2-regular text-layout-body-default">2026-00-00</time>
+      </div>
+    </Link>
+  );
+};
+
+const MainCardList = ({ mode = "recent" }: MainCardMode) => {
+  const { ref: scrollRef, onMouseDown } = useHorizontalDragScroll();
+
+  const isPublicMode = mode === "public";
+
+  return (
+    <div ref={scrollRef} onMouseDown={onMouseDown} className="-mx-5 flex gap-4 px-5 no-scrollbar">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <MainCard key={index} showChip={isPublicMode} />
+      ))}
+      {isPublicMode && <PublicMoreViewCard />}
+    </div>
+  );
+};
+
+export default MainCardList;
--- src/app/(home)/_components/DefaultSheetContent/_internal/RecentFoundItemSection/RecentFoundItemSection.tsx
+++ src/app/(home)/_components/DefaultSheetContent/_internal/RecentFoundItemSection/RecentFoundItemSection.tsx
@@ -1,49 +1,15 @@
 "use client";
 
-import { Icon } from "@/components/common";
-import { useHorizontalDragScroll } from "@/hooks";
-import Image from "next/image";
-import Link from "next/link";
-
-const RecentFoundItem = () => {
-  return (
-    <Link href="#" className="relative rounded-2xl border-[0.7px] border-divider-default">
-      <div className="h-[120px] w-[123px] rounded-2xl bg-fill-neutralInversed-normal-preesed">
-        <div className="relative flex h-full w-full justify-center">
-          <Icon name="LogoCharacter" size={65} />
-          {/* TODO(형준): 이미지 예시 임시 주석처리 */}
-          {/* <Image
-            src="https://i.namu.wiki/i/HXXV8jiIlE0t9EoBXsyfG-ltpGEqnc620PzNjD6G_aYP74irttmRysvhQlYX0lEVSWFEt3icEL0RU6R_jE1URg.webp"
-            alt="thumbnail"
-            fill
-            className="rounded-2xl object-cover"
-          /> */}
-        </div>
-      </div>
-      <div className="absolute bottom-0 right-0 flex w-full flex-col gap-1 rounded-b-2xl bg-white px-3 py-[6px]">
-        <span className="truncate text-caption1-semibold text-layout-header-default">
-          습득물 제목 습득물 제목 습득물 제목
-        </span>
-        <time className="text-caption2-regular text-layout-body-default">2026-00-00</time>
-      </div>
-    </Link>
-  );
-};
+import MainCardList from "../MainCardList/MainCardList";
 
 const RecentFoundItemSection = () => {
-  const { ref: scrollRef, onMouseDown } = useHorizontalDragScroll();
-
   return (
     <section className="space-y-2">
       <h2 className="space-x-1 py-2 pl-1 text-h3-semibold">
         <span className="text-brand-normal-default">OO동</span>
         <span className="text-neutral-strong-hover">최근 발견된 분실물</span>
       </h2>
-      <div ref={scrollRef} onMouseDown={onMouseDown} className="-mx-5 flex gap-4 px-5 no-scrollbar">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <RecentFoundItem key={index} />
-        ))}
-      </div>
+      <MainCardList />
     </section>
   );
 };
