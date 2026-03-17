f41eaa65268f9438d77aaf00a47155bbdbf49fdf
jun <znznun@gmail.com>
2026-03-17 16:58:27 +0900

    refactor(alert): 알림 DetailHeader 클라이언트 컴포넌트로 layout에서 분리

--- src/app/(route)/alert/layout.tsx
+++ src/app/(route)/alert/layout.tsx
@@ -1,21 +1,13 @@
-"use client";
-
-import { DetailHeader } from "@/components/layout";
-import { HeaderSetting } from "@/components/layout/DetailHeader/DetailHeaderParts";
-import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
+import { AlertDetailHeader } from "./_components";
 
 const layout = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
-
   return (
-    <main className="flex w-full flex-col h-base">
+    <div className="flex w-full flex-col h-base">
+      <AlertDetailHeader />
       <h1 className="sr-only">알림 페이지</h1>
-      <DetailHeader title="알림">
-        <HeaderSetting onClick={() => router.push("/mypage/notifications")} />
-      </DetailHeader>
       {children}
-    </main>
+    </div>
   );
 };
 
