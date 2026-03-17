cf36deacc3d041739f3e3f6ec11ba15d5270b626
jun <znznun@gmail.com>
2026-03-17 16:15:20 +0900

    refactor(noticeEdit): 공지사항 수정페이지 DetailHeader layout으로 이동

--- src/app/(admin)/admin/notice/write/[id]/page.tsx
+++ src/app/(admin)/admin/notice/write/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { use } from "react";
-import { DetailHeader } from "@/components/layout";
 import { useGetNoticeDetail } from "@/api/fetch/notice";
 import { NoticeEditFormProvider } from "./_components/NoticeEditFormProvider/NoticeEditFormProvider";
 
@@ -12,8 +11,6 @@ const Page = ({ params }: { params: Promise<{ id: string }> }) => {
 
   return (
     <>
-      <DetailHeader title="공지사항 수정" />
-      <h1 className="sr-only">공지사항 수정 페이지</h1>
       {!isLoading && noticeDetail?.result && (
         <NoticeEditFormProvider initialData={noticeDetail.result} noticeId={noticeId} />
       )}
