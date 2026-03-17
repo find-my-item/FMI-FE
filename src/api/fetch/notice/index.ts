16cac847ad7bc9ab1e9dba0dddbeb1bfd962cd2e
jun <znznun@gmail.com>
2026-03-17 21:05:32 +0900

    feat(noticeShare): 공지사항 메타데이터 api 훅 구현

--- src/api/fetch/notice/index.ts
+++ src/api/fetch/notice/index.ts
@@ -1,5 +1,7 @@
 export * from "./types/NoticesType";
 export * from "./types/NoticeDetailType";
+export * from "./types/NoticeMetaDataType";
 
 export { useGetNotices } from "./api/useGetNotices";
 export { useGetNoticeDetail } from "./api/useGetNoticeDetail";
+export { useGetNoticeMetaData } from "./api/useGetNoticeMetaData";
