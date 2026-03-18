1e05636222f6247f4ef44625613983963928efd4
wlrnjs <wlrnjs5824@gmail.com>
2026-03-18 20:46:03 +0900

    feat(metadata): 알림, 서비스 소개 페이지 메타데이터 추가

--- src/app/(route)/hello/layout.tsx
+++ src/app/(route)/hello/layout.tsx
@@ -1,6 +1,14 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
 import { DetailHeader } from "@/components/layout";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+export const metadata: Metadata = {
+  title: "서비스 소개",
+  description:
+    "가장 빠르고 쉬운 분실물 센터. 경찰청 분실물부터 내 주변 분실물까지, 지도에서 바로 확인해보세요!",
+};
+
+const layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <DetailHeader />
