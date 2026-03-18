7bcc3b8f70e17dec44314345240a59bcd0a5c890
jun <znznun@gmail.com>
2026-03-18 21:51:47 +0900

    refactor(inquiryWrite): _components/_internal 내부 컴포넌트 폴더로 감싸기

--- src/app/(route)/inquiry-write/_components/InquiryWriteForm/_internal/index.ts
+++ src/app/(route)/inquiry-write/_components/InquiryWriteForm/_internal/index.ts
@@ -1,4 +1,3 @@
-export { default as InquiryCategoryButton } from "./InquiryCategoryButton";
-export { default as InquiryTextarea } from "./InquiryTextarea";
-export { default as InquiryInput } from "./InquiryInput";
-export { default as useInquiryWrite } from "../../../_hooks/useInquiryWrite";
+export { default as InquiryCategoryButton } from "./InquiryCategoryButton/InquiryCategoryButton";
+export { default as InquiryTextarea } from "./InquiryTextarea/InquiryTextarea";
+export { default as InquiryInput } from "./InquiryInput/InquiryInput";
