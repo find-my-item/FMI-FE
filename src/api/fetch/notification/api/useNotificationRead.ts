86d5817da3657afc93086d8aad3a7529fde6fa7a
jun <znznun@gmail.com>
2026-03-18 16:55:43 +0900

    fix(alert): 알림 읽음 에러 시 토스트

--- src/api/fetch/notification/api/useNotificationRead.ts
+++ src/api/fetch/notification/api/useNotificationRead.ts
@@ -1,12 +1,17 @@
 import useAppMutation from "@/api/_base/query/useAppMutation";
+import { useToast } from "@/context/ToastContext";
 import { useQueryClient } from "@tanstack/react-query";
 
 const useNotificationRead = () => {
   const queryClient = useQueryClient();
+  const { addToast } = useToast();
 
   return useAppMutation("auth", "/notifications/read-batch", "put", {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["notificationList"] });
+    },
+    onError: () => {
+      addToast("알림을 읽지 못했어요", "error");
     },
   });
 };
