cd2eec081482f09a841352cefee9ad6218a08874
suhyeon0111 <soo4833@naver.com>
2026-03-18 21:01:32 +0900

    refactor(notice): 공지 상세 리팩토링

--- src/app/(route)/notice/[id]/page.tsx
+++ src/app/(route)/notice/[id]/page.tsx
@@ -12,11 +12,11 @@ export async function generateMetadata({ params }: NoticeDetailProps): Promise<M
     next: { revalidate: 60 },
   }).then((res) => res.json());
 
-  const title = `${notice?.result?.title} ?? "문의 상세"`;
-  const description = notice?.result?.description ?? "문의 상세 내용";
-  const thumbnailUrl =
-    notice?.result?.thumbnailUrl ??
-    "https://fmi-project-s3-bucket.s3.ap-northeast-2.amazonaws.com/9e619169-f_default-share.png";
+  const {
+    title = "공지 상세",
+    description = "공지 상세 내용",
+    thumbnailUrl = "https://fmi-project-s3-bucket.s3.ap-northeast-2.amazonaws.com/9e619169-f_default-share.png",
+  } = notice?.result ?? {};
 
   return {
     title,
@@ -28,8 +28,6 @@ export async function generateMetadata({ params }: NoticeDetailProps): Promise<M
       images: [
         {
           url: thumbnailUrl,
-          width: 1200,
-          height: 630,
           alt: title,
         },
       ],
@@ -37,7 +35,6 @@ export async function generateMetadata({ params }: NoticeDetailProps): Promise<M
     twitter: {
       title,
       description,
-      card: "summary_large_image",
       images: [thumbnailUrl],
     },
   };
