0cb859b1308d747e7b6520f5dd58bbd15823803d
wlrnjs <wlrnjs5824@gmail.com>
2026-03-20 21:58:08 +0900

    feat(public): 검색 메타데이터 추가 및 sanitizeKeyword 유틸 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,3 +24,4 @@ export {
   filterSelectionState,
 } from "./deriveFilterParams/deriveFilterParams";
 export { retryBackoffController } from "./retryBackoffController/retryBackoffController";
+export { sanitizeKeyword } from "./sanitizeKeyword/sanitizeKeyword";
