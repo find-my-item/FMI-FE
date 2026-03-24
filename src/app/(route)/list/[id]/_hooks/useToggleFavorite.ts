993e2438cc36ecb459e9e8f1c94584a57758163f
wlrnjs <wlrnjs5824@gmail.com>
2026-03-24 12:46:00 +0900

    fix(list-detail): 불필요한prop 제거, 비로그인 신고/차단/즐겨찾기 제한

--- src/app/(route)/list/[id]/_hooks/useToggleFavorite.ts
+++ src/app/(route)/list/[id]/_hooks/useToggleFavorite.ts
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { throttle } from "lodash";
+import { useGetUsersMe } from "@/api/fetch/user";
 import { useDeletePostFavorites, usePostFavorites } from "@/api/fetch/post";
 
 interface ToggleFavoriteProps {
@@ -9,13 +10,16 @@ interface ToggleFavoriteProps {
 export const useToggleFavorite = ({ postId }: ToggleFavoriteProps) => {
   const { mutate: postFavorite, isPending: postPending } = usePostFavorites(postId);
   const { mutate: deleteFavorite, isPending: deletePending } = useDeletePostFavorites(postId);
+  const { data: me } = useGetUsersMe();
 
   const handleToggleFavorite = useMemo(
     () =>
       throttle((favoriteStatus: boolean) => {
+        if (!me) return;
+
         favoriteStatus ? deleteFavorite({ postId }) : postFavorite({ postId });
       }, 300),
-    [postId, postFavorite, deleteFavorite]
+    [postId, postFavorite, deleteFavorite, me]
   );
 
   return {
