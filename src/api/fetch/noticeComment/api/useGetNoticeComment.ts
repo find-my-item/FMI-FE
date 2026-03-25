a307a5ad4143780fb5a41ad4cca93268c80f4654
jun <znznun@gmail.com>
2026-03-25 18:52:31 +0900

    feat(noticeComment): 공지사항 댓글 조회 패칭 훅 구현

---
+++ src/api/fetch/noticeComment/api/useGetNoticeComment.ts
@@ -0,0 +1,54 @@
+"use client";
+
+import useAppInfiniteQuery from "@/api/_base/query/useAppInfiniteQuery";
+import { InfiniteData, keepPreviousData } from "@tanstack/react-query";
+import { GetNoticeCommentsData, GetNoticeCommentsResponse } from "../types/GetNoticeComments";
+
+interface UseGetNoticeCommentParams {
+  noticeId: number;
+  enabled: boolean;
+  size?: number;
+}
+
+const DEFAULT_NOTICE_COMMENT_SIZE = 10;
+
+export const useGetNoticeComment = ({
+  noticeId,
+  enabled,
+  size = DEFAULT_NOTICE_COMMENT_SIZE,
+}: UseGetNoticeCommentParams) => {
+  return useAppInfiniteQuery<GetNoticeCommentsResponse, unknown, GetNoticeCommentsData>(
+    "auth",
+    ["notice-comments", noticeId],
+    `/notices/${noticeId}/comments?size=${size}`,
+    {
+      enabled: !!noticeId && enabled,
+      pageParamName: "cursor",
+      initialPageParam: undefined,
+      getNextPageParam: (lastPage) =>
+        lastPage.result.hasNext ? lastPage.result.cursor : undefined,
+      select: (data: InfiniteData<GetNoticeCommentsResponse>) => {
+        const allRawComments = data.pages.flatMap((page) => page.result?.comments ?? []);
+        const lastPage = data.pages[data.pages.length - 1];
+        const lastResult = lastPage?.result;
+
+        const comments = allRawComments.map(({ like, ...rest }) => ({
+          ...rest,
+          isLike: like,
+        }));
+
+        const hasNext = lastResult?.hasNext ?? false;
+        const cursor = lastResult?.cursor ?? 0;
+
+        return {
+          comments,
+          hasNext,
+          cursor,
+          totalCommentCount: comments.length,
+          remainingCount: hasNext ? size : 0,
+        };
+      },
+      placeholderData: keepPreviousData,
+    }
+  );
+};
---
+++ src/api/fetch/noticeComment/index.ts
@@ -0,0 +1,3 @@
+export * from "./types/GetNoticeComments";
+
+export { useGetNoticeComment } from "./api/useGetNoticeComment";
