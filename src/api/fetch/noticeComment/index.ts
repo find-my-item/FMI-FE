b74856aa156e6747998ac04dd54496a26b2d3498
jun <znznun@gmail.com>
2026-03-25 19:07:02 +0900

    feat(noticeComment): 공지사항 댓글 삭제 패칭 훅 구현

--- src/api/fetch/noticeComment/index.ts
+++ src/api/fetch/noticeComment/index.ts
@@ -3,3 +3,6 @@ export * from "./types/PostNoticeComments";
 
 export { useGetNoticeComment } from "./api/useGetNoticeComment";
 export { usePostNoticeComment } from "./api/usePostNoticeComment";
+
+export { useDeleteNoticeComment } from "./api/useDeleteNoticeComment";
+export * from "./types/DeleteNoticeCommentType";
