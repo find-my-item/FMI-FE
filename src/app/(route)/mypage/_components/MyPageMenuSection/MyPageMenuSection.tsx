7b307d18df2d0b8137dc823638e4ae1c7ed9ebfc
suhyeon0111 <soo4833@naver.com>
2026-03-27 04:03:28 +0900

    fix(mypage): 비로그인 상태일때 구분선 제거

--- src/app/(route)/mypage/_components/MyPageMenuSection/MyPageMenuSection.tsx
+++ src/app/(route)/mypage/_components/MyPageMenuSection/MyPageMenuSection.tsx
@@ -36,7 +36,8 @@ const MyPageMenuSection = ({
           </Fragment>
         ))}
       </div>
-      <hr className="mx-5 max-w-full border border-divider-default_3" />
+      {isUserLogin && <hr className="mx-5 max-w-full border border-divider-default_3" />}
+
       {isUserLogin && menu.title === "서비스 정책" && (
         <div className="w-full px-5 py-6">
           <button
