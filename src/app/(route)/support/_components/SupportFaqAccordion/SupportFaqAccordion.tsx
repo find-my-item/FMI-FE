fa484359b9f8de193274ce3de662b55ca26e6d54
jun <znznun@gmail.com>
2026-03-29 04:19:01 +0900

    fix(qa-support): motion.div 아코디언 태그에 key 추가

--- src/app/(route)/support/_components/SupportFaqAccordion/SupportFaqAccordion.tsx
+++ src/app/(route)/support/_components/SupportFaqAccordion/SupportFaqAccordion.tsx
@@ -61,6 +61,7 @@ const SupportFaqAccordionItem = ({ item, isExpanded, onToggle }: SupportFaqAccor
       <AnimatePresence>
         {isExpanded && (
           <motion.div
+            key="content"
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "auto" }}
             exit={{ opacity: 0, height: 0 }}
