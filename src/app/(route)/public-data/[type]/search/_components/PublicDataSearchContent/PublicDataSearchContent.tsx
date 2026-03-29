f5723526a8458583801c6ceba03a8b728adecf8d
wlrnjs <wlrnjs5824@gmail.com>
2026-03-29 22:54:59 +0900

    feat(public): 최근 검색 기능 추가

--- src/app/(route)/public-data/[type]/search/_components/PublicDataSearchContent/PublicDataSearchContent.tsx
+++ src/app/(route)/public-data/[type]/search/_components/PublicDataSearchContent/PublicDataSearchContent.tsx
@@ -5,6 +5,8 @@ import { InputSearch } from "@/components/common";
 import PublicDataSearchList from "../PublicDataSearchList/PublicDataSearchList";
 import { Tab } from "@/components/domain";
 import { usePublicDataTabQuery } from "@/app/(route)/public-data/_hooks/usePublicDataTabQuery/usePublicDataTabQuery";
+import PublicDataBeforeSearch from "../PublicDataBeforeSearch/PublicDataBeforeSearch";
+import useRecentSearch from "../../_hooks/useRecentSearch/useRecentSearch";
 
 const PublicDataSearchContent = () => {
   const router = useRouter();
@@ -12,15 +14,17 @@ const PublicDataSearchContent = () => {
   const paramsConfig = useParams();
   const type = paramsConfig.type === "found" ? "found" : "lost";
   const { activeTab, handleTabChange, PUBLIC_LIST_TABS } = usePublicDataTabQuery();
+  const params = new URLSearchParams(searchParams.toString());
+  const { recentSearches, addSearch, removeSearch } = useRecentSearch();
 
   const handlePublicDataSearch = (newKeyword: string) => {
-    const params = new URLSearchParams(searchParams.toString());
     if (newKeyword === params.get("keyword")) return;
 
     if (!newKeyword) {
       params.delete("keyword");
     } else {
       params.set("keyword", newKeyword);
+      addSearch(newKeyword);
     }
 
     router.replace(`/public-data/${type}/search?${params.toString()}`);
@@ -38,14 +42,24 @@ const PublicDataSearchContent = () => {
         />
       </section>
 
-      <Tab
-        tabs={PUBLIC_LIST_TABS}
-        selected={activeTab}
-        onValueChange={(key) => handleTabChange(key)}
-        className="sticky left-0 top-[56px]"
-      />
+      {params.get("keyword") && (
+        <Tab
+          tabs={PUBLIC_LIST_TABS}
+          selected={activeTab}
+          onValueChange={(key) => handleTabChange(key)}
+          className="sticky left-0 top-[56px]"
+        />
+      )}
 
-      <PublicDataSearchList />
+      {params.get("keyword") ? (
+        <PublicDataSearchList />
+      ) : (
+        <PublicDataBeforeSearch
+          recentSearches={recentSearches}
+          removeSearch={removeSearch}
+          onSearch={handlePublicDataSearch}
+        />
+      )}
     </>
   );
 };
