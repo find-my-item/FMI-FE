a44d49b2546618dcba25372d7db39815dd7a1e16
suhyeon0111 <soo4833@naver.com>
2026-04-01 03:22:59 +0900

    fix(signup): 가입 완료 스로틀링

--- src/app/(route)/sign-up/_hooks/useSignUpSubmit.ts
+++ src/app/(route)/sign-up/_hooks/useSignUpSubmit.ts
@@ -5,6 +5,8 @@ import { SIGNUP_ERROR_MESSAGE } from "../_constants/SIGNUP_ERROR_MESSAGE";
 import { useErrorToast } from "@/hooks/domain";
 import { usePermissionStore } from "@/store";
 import { useQueryClient } from "@tanstack/react-query";
+import { throttle } from "lodash";
+import { useMemo } from "react";
 
 export const useSignUpSubmit = () => {
   const router = useRouter();
@@ -14,23 +16,30 @@ export const useSignUpSubmit = () => {
   const { setFirstSignUp } = usePermissionStore();
   const queryClient = useQueryClient();
 
-  const submitSignUp = (data: ApiSignUpType) => {
-    SignUpMutate(data, {
-      onSuccess: () => {
-        setFirstSignUp(true);
-        queryClient.clear();
-
-        router.replace("/");
-        addToast("회원가입이 완료되었어요.", "success");
-      },
-      onError: (error) => {
-        const errorCode = error?.response?.data.code;
-        if (errorCode) {
-          handlerApiError(SIGNUP_ERROR_MESSAGE, errorCode);
-        }
-      },
-    });
-  };
+  const submitSignUp = useMemo(
+    () =>
+      throttle(
+        (data: ApiSignUpType) => {
+          SignUpMutate(data, {
+            onSuccess: () => {
+              setFirstSignUp(true);
+              queryClient.clear();
+              router.replace("/");
+              addToast("회원가입이 완료되었어요.", "success");
+            },
+            onError: (error) => {
+              const errorCode = error?.response?.data?.code;
+              if (errorCode) {
+                handlerApiError(SIGNUP_ERROR_MESSAGE, errorCode);
+              }
+            },
+          });
+        },
+        3000,
+        { leading: true, trailing: false }
+      ),
+    [SignUpMutate, setFirstSignUp, queryClient, router, addToast, handlerApiError]
+  );
 
   return {
     submitSignUp,
